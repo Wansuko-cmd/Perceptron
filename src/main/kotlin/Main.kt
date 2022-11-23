9e2870644c800f46ea8dd4885b9e85cfed55c493
Wansuko-cmd <63241922+Wansuko-cmd@users.noreply.github.com>
2022-11-24 00:58:44 +0900

    [add] Irisの学習を行う部分を追加

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,32 @@
+import dataset.datasets
+import layer.Layer
+import layer.step
+
 fun main() {
-    println("Hello World")
+    val model = (1..1000).fold(
+        Layer.create(input = 4, center = 50, output = 3, rate = 0.01),
+    ) { model, index ->
+        println("epoc: $index")
+        datasets.fold(model) { acc, element ->
+            acc.train(
+                value = listOf(
+                    element.petalLength,
+                    element.petalWidth,
+                    element.sepalLength,
+                    element.sepalWidth,
+                ),
+                label = element.label,
+            )
+        }
+    }
+    datasets.count { data ->
+        model.forward(
+            value = listOf(
+                data.petalLength,
+                data.petalWidth,
+                data.sepalLength,
+                data.sepalWidth,
+            ),
+        ).map { it.sum().step().toInt() }[data.label] == 1
+    }.let { println(it.toDouble() / datasets.size.toDouble()) }
 }
