6c09c898f21b1c7c2d91ca513287d288326b72e9
Wansuko-cmd <63241922+Wansuko-cmd@users.noreply.github.com>
2022-11-24 01:01:45 +0900

    [add] コマンドラインから実行できる機能を追加

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     kotlin("jvm") version "1.7.10"
+    application
 }
 
 group = "me.wansu"
@@ -13,6 +14,10 @@ repositories {
 
 dependencies {
     testImplementation(kotlin("test"))
+}
+
+application {
+    mainClass.set("MainKt")
 }
 
 tasks.test {
