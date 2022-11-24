e7cba70aeb595edbce6059e2d75d9ef084c59051
Wansuko-cmd <63241922+Wansuko-cmd@users.noreply.github.com>
2022-11-25 03:07:45 +0900

    [tmp] 誤差逆伝播法を実装

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,32 +1,38 @@
 import dataset.datasets
+import kotlin.random.Random
 import layer.Layer
+import layer.random
 
 fun main() {
-    val (train, test) = datasets.shuffled().chunked(120)
-    val model = (1..1000).fold(
-        Layer.create(input = 4, center = 50, output = 3, rate = 0.01),
-    ) { model, index ->
-        println("epoc: $index")
-        train.fold(model) { acc, element ->
-            acc.train(
-                value = listOf(
-                    element.petalLength,
-                    element.petalWidth,
-                    element.sepalLength,
-                    element.sepalWidth,
-                ),
-                label = element.label,
-            )
+//    val (train, test) = datasets.shuffled().chunked(120)
+    val (train, test) = datasets to datasets
+    (1..100).maxBy {
+        random = Random(it)
+        val model = (1..50).fold(
+            Layer.create(input = 4, center = 50, output = 3, rate = 0.01),
+        ) { model, index ->
+            println("epoc: $index")
+            train.fold(model) { acc, element ->
+                acc.fd(
+                    input = listOf(
+                        element.petalLength,
+                        element.petalWidth,
+                        element.sepalLength,
+                        element.sepalWidth,
+                    ),
+                    label = element.label,
+                )
+            }
         }
+        test.count { data ->
+            model.forward(
+                value = listOf(
+                    data.petalLength,
+                    data.petalWidth,
+                    data.sepalLength,
+                    data.sepalWidth,
+                ),
+            ) == data.label
+        }.also { println(it.toDouble() / test.size.toDouble()) }
     }
-    test.count { data ->
-        model.forward(
-            value = listOf(
-                data.petalLength,
-                data.petalWidth,
-                data.sepalLength,
-                data.sepalWidth,
-            ),
-        ) == data.label
-    }.let { println(it.toDouble() / test.size.toDouble()) }
 }
--- src/main/kotlin/Test.kt
+++ src/main/kotlin/Test.kt
@@ -1,29 +1,46 @@
-import layer.Node
+
+import layer.step
 
 sealed interface N {
     val delta: Double
+    val id: String
 
     class OutputN(
         val v: Double,
         val y: Double,
         val t: Double,
+        override val id: String,
     ) : N {
         override val delta: Double = (y - t) * (1 - v) * v
+        override fun fixWeight(): N = this
+        override fun toList(): List<List<Pair<Double, N>>> = listOf(listOf())
     }
 
     class NormalN(
         val v: Double,
-        val nodes: List<Pair<N, Double>>,
+        val y: Double,
+        val nodes: List<Pair<Double, N>>,
+        override val id: String,
     ) : N {
+        override fun toString() = nodes.toString()
         override val delta: Double =
-            (1 - v) * v * nodes.sumOf { (node, weight) -> node.delta * weight }
+            v.step() * nodes.sumOf { (weight, node) -> node.delta * weight }
+
+        override fun fixWeight(): N =
+            nodes
+                .map { (weight, node) -> (weight - node.delta * y) to node }
+                .let { NormalN(v, y, it, id) }
+
+        override fun toList(): List<List<Pair<Double, N>>> =
+            nodes.flatMap { (weight, node) -> node.toList().map {  listOf((weight to node)) + it } }
     }
+
+    fun fixWeight(): N
+
+    fun toList(): List<List<Pair<Double, N>>>
 }
 
-class L(
-    val nodes: List<N>
-) {
-    fun study() {
-        nodes.map { it.delta }
-    }
+sealed interface TestN {
+    object OutputN : TestN
+    class NormalN(val nodes: List<Pair<TestN, Double>>) : TestN
 }
