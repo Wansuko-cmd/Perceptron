2eff07b7b7f4b771f00311293aca32f469cea299
Wansuko-cmd <63241922+Wansuko-cmd@users.noreply.github.com>
2022-11-25 15:50:20 +0900

    [refactor] Main関数をリファクタリング

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,67 +1,11 @@
-import dataset.IrisDataset
-import dataset.datasets
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.async
+import common.checkAverage
+import common.searchGoodSeed
 import kotlinx.coroutines.runBlocking
-import kotlinx.coroutines.withContext
-import layer.Layer
-import kotlin.random.Random
-
-const val SEED = 12
-val random = Random(SEED)
 
 fun main(): Unit = runBlocking {
-    val (train, test) = datasets.shuffled().chunked(120)
-
-    val a = checkAverage(12)
-    val b = checkAverage(32)
-    println("a: $a, b: $b")
-//    createModel(train, test, 32)
-//    (30..100)
-//        .map { async { createModel(train, test, it) to it } }
-//        .maxBy { it.await().first }
-//        .also { println(it.await().second) }
+    searchGoodSeed(from = 200, to = 300, epoc = 50).also(::println)
 }
 
-suspend fun checkAverage(seed: Int) = withContext(Dispatchers.Default) {
-    return@withContext (1..20).map {
-        val (train, test) = datasets.shuffled().chunked(120)
-        async { createModel(train, test, seed) to test.size }
-    }
-        .map { it.await() }
-        .fold(0 to 0) { acc, (correct, size) -> acc.first + correct to acc.second + size }
-        .let { it.first.toDouble() / it.second.toDouble() }.also { println("Average: $it") }
-}
-
-suspend fun createModel(
-    train: List<IrisDataset>,
-    test: List<IrisDataset>,
-    seed: Int,
-): Int = withContext(Dispatchers.Default) {
-    val model = (1..50).fold(
-        Layer.create(input = 4, center = listOf(5, 8), output = 3, rate = 0.01, random = Random(seed)),
-    ) { model, index ->
-        println("epoc: $index")
-        train.fold(model) { acc, element ->
-            acc.train(
-                input = listOf(
-                    element.petalLength,
-                    element.petalWidth,
-                    element.sepalLength,
-                    element.sepalWidth,
-                ),
-                label = element.label,
-            )
-        }
-    }
-    return@withContext test.count { data ->
-        model.forward(
-            input = listOf(
-                data.petalLength,
-                data.petalWidth,
-                data.sepalLength,
-                data.sepalWidth,
-            ),
-        ) == data.label
-    }.also { println(it.toDouble() / test.size.toDouble()) }
-}
+/**
+ * Seed値 例: 12, 32
+ */
---
+++ src/main/kotlin/common/ModelUtils.kt
@@ -0,0 +1,67 @@
+package common
+
+import dataset.IrisDataset
+import dataset.datasets
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.async
+import kotlinx.coroutines.withContext
+import layer.Layer
+import kotlin.random.Random
+
+suspend fun createModel(
+    train: List<IrisDataset>,
+    test: List<IrisDataset>,
+    epoc: Int,
+    seed: Int? = null,
+): Int = withContext(Dispatchers.Default) {
+    val model = (1..epoc).fold(
+        Layer.create(
+            input = 4,
+            center = listOf(7, 8),
+            output = 3,
+            rate = 0.01,
+            random = seed?.let { Random(it) } ?: Random,
+        ),
+    ) { model, index ->
+        println("epoc: $index")
+        train.fold(model) { acc, element ->
+            acc.train(
+                input = listOf(
+                    element.petalLength,
+                    element.petalWidth,
+                    element.sepalLength,
+                    element.sepalWidth,
+                ),
+                label = element.label,
+            )
+        }
+    }
+    return@withContext test.count { data ->
+        model.forward(
+            input = listOf(
+                data.petalLength,
+                data.petalWidth,
+                data.sepalLength,
+                data.sepalWidth,
+            ),
+        ) == data.label
+    }.also { println(it.toDouble() / test.size.toDouble()) }
+}
+
+suspend fun checkAverage(seed: Int, count: Int, epoc: Int) = withContext(Dispatchers.Default) {
+    return@withContext (1..count).map {
+        val (train, test) = datasets.shuffled().chunked(120)
+        async { createModel(train = train, test = test, epoc = epoc, seed = seed) to test.size }
+    }
+        .map { it.await() }
+        .fold(0 to 0) { acc, (correct, size) -> acc.first + correct to acc.second + size }
+        .let { it.first.toDouble() / it.second.toDouble() }.also { println("Average: $it") }
+}
+
+suspend fun searchGoodSeed(from: Int, to: Int, epoc: Int): Int = withContext(Dispatchers.Default) {
+    val (train, test) = datasets.shuffled().chunked(120)
+    (from..to)
+        .map { async { createModel(train = train, test = test, epoc = epoc, seed = it) to it } }
+        .maxBy { it.await().first }.await().second
+        .also { println("Best Seed: $it") }
+}
