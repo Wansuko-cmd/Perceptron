5498d59e1589ba6b1a5e35075429dc6a50e775d5
Wansuko-cmd <63241922+Wansuko-cmd@users.noreply.github.com>
2022-11-25 21:38:14 +0900

    [add] Mnistに対応

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,10 +1,37 @@
 import common.checkAverage
-import common.searchGoodSeed
 import kotlinx.coroutines.runBlocking
+import kotlin.system.measureTimeMillis
 
 fun main(): Unit = runBlocking {
-    searchGoodSeed(from = 200, to = 300, epoc = 50).also(::println)
+    measureTimeMillis { checkAverage(12, 10, 50) }.also { println(it) }
+//    val (train, test) = MnistDataset.read().chunked(5000)
+//    val model = (1..10).fold(
+//        Layer.create(
+//            input = train.first().imageSize * train.first().imageSize,
+//            center = listOf(32),
+//            output = 10,
+//            rate = 0.03
+//        ),
+//    ) { model, index ->
+//        println("epoc: $index")
+//        train.fold(model) { acc, element ->
+//            println(element)
+//            acc.train(
+//                input = element.pixels,
+//                label = element.label,
+//            )
+//        }
+//    }
+//    train.count { data ->
+//        model.forward(
+//            input = data.pixels,
+//        ).also { println("$it, except: ${it.maxIndex()} label: ${data.label}") }.maxIndex() == data.label
+//    }.also { println(it.toDouble() / train.size.toDouble()) }
 }
+
+/**
+ *
+ */
 
 /**
  * Seed値 例: 12, 32
--- src/main/kotlin/dataset/mnist/MnistDataset.kt
+++ src/main/kotlin/dataset/mnist/MnistDataset.kt
@@ -1,13 +1,50 @@
-package dataset
+package dataset.mnist
 
 import java.io.DataInputStream
+import java.nio.file.Files
+import java.nio.file.Paths
 import java.util.zip.GZIPInputStream
 
-class MnistDataset(
-    val byte: List<Int>,
+data class MnistDataset(
+    val pixels: List<Double>,
+    val label: Int,
+    val imageSize: Int,
 ) {
+    override fun toString(): String =
+        pixels
+            .chunked(imageSize)
+            .joinToString("\n") { row ->
+                row.joinToString { column ->
+                    if (column > 0) "■" else "□"
+                }.replace(",", "")
+            }
+
     companion object {
-        fun read() {
+        private const val LABEL_PATH = "src/main/resources/train-labels-idx1-ubyte.gz"
+        private const val IMAGE_PATH = "src/main/resources/train-images-idx3-ubyte.gz"
+        private const val PIXEL_DEPTH = 255
+
+        fun read(): List<MnistDataset> {
+            val labelPath = Paths.get(LABEL_PATH)
+            val imagePath = Paths.get(IMAGE_PATH)
+            val labelStream = DataInputStream(GZIPInputStream(Files.newInputStream(labelPath)))
+            val imageStream = DataInputStream(GZIPInputStream(Files.newInputStream(imagePath)))
+            labelStream.skip(4)
+            imageStream.skip(4)
+            val labelSize = labelStream.readInt()
+            val imageSize = imageStream.readInt()
+            val imageHeight = imageStream.readInt()
+            val imageWidth = imageStream.readInt()
+            val labels = (1..labelSize).map { labelStream.readUnsignedByte() }
+            val images = (1..imageSize)
+                .map {
+                    (1..imageHeight * imageWidth)
+                        .map { imageStream.readUnsignedByte() }
+                        .filterIndexed { index, _ -> index % 2 == 0 }
+                        .map { it.toDouble() - (PIXEL_DEPTH / 2.0) }
+                        .map { it / PIXEL_DEPTH }
+                }.filterIndexed { index, _ -> index % 2 == 0 }
+            return labels.zip(images) { label, image -> MnistDataset(image, label, imageWidth / 2) }
         }
     }
 }
