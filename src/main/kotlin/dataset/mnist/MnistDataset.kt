dd2b2b835ef0529a831c912df99a08a47e163c97
Wansuko-cmd <63241922+Wansuko-cmd@users.noreply.github.com>
2022-11-25 17:35:43 +0900

    [add] Mnist読み取り機能を追加

---
+++ src/main/kotlin/dataset/mnist/MnistDataset.kt
@@ -0,0 +1,13 @@
+package dataset
+
+import java.io.DataInputStream
+import java.util.zip.GZIPInputStream
+
+class MnistDataset(
+    val byte: List<Int>,
+) {
+    companion object {
+        fun read() {
+        }
+    }
+}
