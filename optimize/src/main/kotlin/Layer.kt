58b885a0c70fb1c619c86028b205d99102a3b06e
Wansuko-cmd <63241922+Wansuko-cmd@users.noreply.github.com>
2022-11-28 02:17:23 +0900

    [add] 高速版を実装

---
+++ optimize/src/main/kotlin/Layer.kt
@@ -0,0 +1,93 @@
+import common.mapDownIndexed
+import common.maxIndex
+import common.relu
+import common.sigmoid
+import common.step
+import kotlin.random.Random
+
+val random = Random
+
+class Network(
+    private val layers: List<Int>,
+    private val weights: MutableMap<WParam, Double>,
+) {
+    fun expect(input: List<Double>): Int {
+        val output = forward(input)
+        return (0 until layers.last()).map { output[layers.size - 1 to it]!! }.maxIndex()
+    }
+
+    fun train(input: List<Double>, label: Int) {
+        val output = forward(input)
+        backward(output, calcDelta(output, label))
+    }
+
+    private fun forward(input: List<Double>): Map<Pair<Int, Int>, Double> {
+        val output = mutableMapOf<Pair<Int, Int>, Double>()
+        (0 until layers.first()).forEach { output[0 to it] = input[it] }
+        layers
+            .windowed(2) { (before, after) -> before to after }
+            .mapIndexed { index, (before, after) ->
+                (0 until after).forEach { a ->
+                    if (index == layers.size - 2) {
+                        output[index + 1 to a] =
+                            sigmoid((0 until before).sumOf { b -> output[index to b]!! * weights[WParam(index, b, a)]!! })
+                    } else {
+                        output[index + 1 to a] =
+                            relu((0 until before).sumOf { b -> output[index to b]!! * weights[WParam(index, b, a)]!! })
+                    }
+                }
+            }
+        return output
+    }
+
+    private fun backward(output: Map<Pair<Int, Int>, Double>, delta: Map<Pair<Int, Int>, Double>) {
+        layers
+            .windowed(2) { (before, after) -> before to after }
+            .mapIndexed { index, (before, after) ->
+                (0 until before).forEach { b ->
+                    (0 until after).forEach { a ->
+                        weights[WParam(index, b, a)] =
+                            weights[WParam(index, b, a)]!! - 0.01 * delta[index + 1 to a]!! * output[index to b]!!
+                    }
+                }
+            }
+    }
+
+    private fun calcDelta(output: Map<Pair<Int, Int>, Double>, label: Int): Map<Pair<Int, Int>, Double> {
+        val teacher = MutableList(layers.last()) { 0.0 }
+        teacher[label] = 1.0
+        val delta = mutableMapOf<Pair<Int, Int>, Double>()
+        (0 until layers.last()).forEach {
+            val y = output[layers.size - 1 to it]
+            delta[layers.size - 1 to it] = (y!! - teacher[it]) * (1 - y) * y
+        }
+        layers
+            .windowed(2) { (before, after) -> before to after }
+            .mapDownIndexed { index, (before, after) ->
+                (0 until before).forEach { b ->
+                    delta[index to b] = step(output[index to b]!!) *
+                        (0 until after).sumOf { a -> delta[index + 1 to a]!! * weights[WParam(index, b, a)]!! }
+                }
+            }
+        return delta
+    }
+
+    companion object {
+        fun create(layers: List<Int>): Network {
+            val weights: MutableMap<WParam, Double> = mutableMapOf()
+            layers
+                .windowed(2) { (before, after) -> before to after }
+                .mapIndexed { index, (before, after) ->
+                    (0 until before).forEach { b ->
+                        (0 until after).forEach { a ->
+                            weights[WParam(index, b, a)] = random.nextDouble(from = -1.0, until = 1.0)
+                        }
+                    }
+                }
+
+            return Network(layers, weights)
+        }
+    }
+}
+
+data class WParam(val layer: Int, val from: Int, val to: Int)
--- optimize/src/main/kotlin/Main.kt
+++ optimize/src/main/kotlin/Main.kt
@@ -1,5 +1,30 @@
-import dataset.iris.IrisLabel
+import dataset.iris.datasets
 
 fun main() {
-    println("Hello")
+    val network = Network.create(listOf(4, 50, 3))
+    val (train, test) = datasets.shuffled().chunked(120)
+    (1..1000).forEach { epoc ->
+        println("epoc: $epoc")
+        train.forEach { data ->
+            network.train(
+                input = listOf(
+                    data.petalLength,
+                    data.petalWidth,
+                    data.sepalLength,
+                    data.sepalWidth,
+                ),
+                label = data.label,
+            )
+        }
+    }
+    test.count { data ->
+        network.expect(
+            input = listOf(
+                data.petalLength,
+                data.petalWidth,
+                data.sepalLength,
+                data.sepalWidth,
+            ),
+        ).also { println("Except: $it, label: ${data.label}") } == data.label
+    }.also { println(it.toDouble() / test.size.toDouble()) }
 }
