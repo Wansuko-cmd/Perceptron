ca26a3a8892d88fbf5f2edbee9622542d6c7d0eb
Wansuko-cmd <63241922+Wansuko-cmd@users.noreply.github.com>
2022-11-28 12:15:55 +0900

    [add] 非同期処理を追加

--- optimize/src/main/kotlin/Main.kt
+++ optimize/src/main/kotlin/Main.kt
@@ -1,29 +1,38 @@
 
 import common.checkAverage
 import dataset.mnist.MnistDataset
-import kotlinx.coroutines.runBlocking
 import kotlin.random.Random
 import kotlin.system.measureTimeMillis
+import kotlinx.coroutines.async
+import kotlinx.coroutines.runBlocking
 
 fun main(): Unit = runBlocking {
 //    println("Score: ${checkAverage(0, 100, 500)}")
 //    println("Score: ${checkAverage(0, 100, 50)}")
-    val (train, test) = MnistDataset.read().chunked(20000)
-    val network = Network.create(listOf(train.first().imageSize * train.first().imageSize, 32, 64, 512, 10), Random, 0.03)
-    (1..5).forEach { epoc ->
-        println("epoc: $epoc")
-        measureTimeMillis {
-            train.forEach { data ->
-                network.train(
-                    input = data.pixels,
-                    label = data.label,
-                )
+    val (train, test) = MnistDataset.read().chunked(2000)
+    (1..1000).map {
+        async {
+            val network =
+                Network.create(listOf(train.first().imageSize * train.first().imageSize, 32, 64, 512, 10), Random, 0.03)
+            (1..2).forEach { epoc ->
+                println("epoc: $epoc")
+                train.forEach { data ->
+                    network.train(
+                        input = data.pixels,
+                        label = data.label,
+                    )
+                }
             }
-        }.let { println(it) }
+            test.count { data ->
+                network.expect(
+                    input = data.pixels,
+                ) == data.label
+            } to it
+        }
     }
-    test.count { data ->
-        network.expect(
-            input = data.pixels,
-        ) == data.label
-    }.also { println(it.toDouble() / test.size.toDouble()) }
+        .map { it.await() }
+        .sortedBy { it.first }
+        .take(10)
+        .also { println(it.joinToString("\n") {  (score, seed) -> "Seed: $seed, Score: ${score.toDouble() / test.size}" }) }
+        .map { it.second }
 }
