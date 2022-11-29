b75c82ee9b019b1952426b3dc49dddf5c6d759b0
Wansuko-cmd <63241922+Wansuko-cmd@users.noreply.github.com>
2022-11-29 14:25:07 +0900

    [tmp] 動くconvを追加

--- optimize/src/main/kotlin/Main.kt
+++ optimize/src/main/kotlin/Main.kt
@@ -3,32 +3,35 @@
 import common.relu
 import common.sigmoid
 import dataset.iris.datasets
+import dataset.mnist.MnistDataset
 import kotlinx.coroutines.runBlocking
 import network.DevNetwork
 import network.LayerConfig
+import network.LayerType
 import kotlin.random.Random
 
 fun main(): Unit = runBlocking {
     val network = DevNetwork.create(
         listOf(
-            LayerConfig(4, ::relu),
-            LayerConfig(50, ::relu),
-            LayerConfig(3, ::sigmoid),
+            LayerConfig(4, ::relu, LayerType.MatMul),
+            LayerConfig(50, ::relu, LayerType.MatMul),
+            LayerConfig(3, ::sigmoid, LayerType.MatMul),
         ),
         Random(1652),
-        0.01,
+        0.01
     )
     val (train, test) = datasets.shuffled().chunked(120)
-    (1..1000).forEach { _ ->
+    (1..1000).forEach { epoc ->
+        println("epoc: $epoc")
         train.forEach { data ->
             network.train(
                 input = listOf(
                     data.petalLength,
                     data.petalWidth,
-                    data.sepalLength,
                     data.sepalWidth,
+                    data.sepalLength,
                 ),
-                label = data.label,
+                label = data.label
             )
         }
     }
@@ -37,46 +40,34 @@ fun main(): Unit = runBlocking {
             input = listOf(
                 data.petalLength,
                 data.petalWidth,
-                data.sepalLength,
                 data.sepalWidth,
+                data.sepalLength,
             ),
         ) == data.label
     }.let { println(it.toDouble() / test.size) }
-
-//    val input = listOf(
-//        listOf(0, 1, 2, 3),
-//        listOf(1, 2, 3, 0),
-//        listOf(2, 3, 0, 1),
-//        listOf(3, 0, 1, 2),
-//    ).map { it.map { it.toDouble() } }
-//    val kernel = listOf(
-//        listOf(0, 1, 2),
-//        listOf(1, 2, 0),
-//        listOf(2, 0, 1),
-//    ).map { it.map { it.toDouble() } }
-//    println(input.conv(kernel))
-
-//    searchGoodSeed(0, 10000, 1000)
-//        .map { checkAverage(it, 50, 1000) to it }
-//        .sortedByDescending { it.first }
-//        .forEach { (score, seed) -> println("Seed: $seed, Score: $score") }
-//    println(measureTimeMillis { println("Score: ${checkAverage(0, 100, 10000)}") })
-//    println("Score: ${checkAverage(0, 100, 50)}")
-//    val (train, test) = MnistDataset.read().chunked(20000)
-//    val network =
-//        network.Network.create(listOf(train.first().imageSize * train.first().imageSize, 392, 98, 49, 10), Random, 0.01)
-//    (1..50).forEach { epoc ->
+//    val (train, test) = MnistDataset.read().chunked(200)
+//    val network = DevNetwork.create(
+//        listOf(
+//            LayerConfig(1, ::relu, LayerType.Conv),
+//            LayerConfig(32, ::relu, LayerType.Conv),
+//            LayerConfig(30, ::relu, LayerType.MatMul),
+//            LayerConfig(10, ::sigmoid, LayerType.MatMul),
+//        ),
+//        Random(1652),
+//        0.01,
+//    )
+//    (1..5).forEach { epoc ->
 //        println("epoc: $epoc")
 //        train.forEach { data ->
-//            network.train(
-//                input = data.pixels,
+//            network.trains(
+//                input = listOf(data.pixels.chunked(train.first().imageSize)),
 //                label = data.label,
 //            )
 //        }
 //    }
 //    test.count { data ->
-//        network.expect(
-//            input = data.pixels,
+//        network.expects(
+//            input = listOf(data.pixels.chunked(train.first().imageSize)),
 //        ) == data.label
-//    }.let { println("${it.toDouble() / test.size}") }
+//    }.let { println(it.toDouble() / test.size) }
 }
