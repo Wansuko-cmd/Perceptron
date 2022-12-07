c5c7485299e3aea968285ddc0b85144526972550
Wansuko-cmd <63241922+Wansuko-cmd@users.noreply.github.com>
2022-12-08 02:45:27 +0900

    [add] Mnistを追加

--- practical/src/main/kotlin/Main.kt
+++ practical/src/main/kotlin/Main.kt
@@ -1,7 +1,8 @@
 
+import dataset.mnist.createMnistModel
 import dataset.wine.createWineModel
 import kotlin.system.measureTimeMillis
 
 fun main() {
-    measureTimeMillis { (0..10).forEach { _ -> createWineModel(epoc = 1000, 1) } }.also { println(it) }
+    measureTimeMillis { (0..10).forEach { _ -> createMnistModel(epoc = 10, 1) } }.also { println(it) }
 }
