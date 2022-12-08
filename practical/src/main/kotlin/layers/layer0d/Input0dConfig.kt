2a259fc1e4126f89c2ec99a43bac440030eee0cd
Wansuko-cmd <63241922+Wansuko-cmd@users.noreply.github.com>
2022-12-08 18:35:52 +0900

    [fix] Input0dConfigを修正

--- practical/src/main/kotlin/layers/layer0d/Input0dConfig.kt
+++ practical/src/main/kotlin/layers/layer0d/Input0dConfig.kt
@@ -2,32 +2,34 @@ package layers.layer0d
 
 import exception.DomainException
 import layers.IOType
+import layers.LayerConfig
 import layers.LayerType
+import kotlin.random.Random
 
-data class Input0dConfig(val size: Int) {
-    fun toLayoutConfig() =
-        Layer0dConfig(
-            numOfNeuron = size,
-            activationFunction = { throw DomainException.UnreachableCodeException() },
-            type = object : LayerType {
-                override fun forward(
-                    input: IOType,
-                    output: IOType,
-                    weight: Array<IOType>,
-                    activationFunction: (Double) -> Double,
-                ) = throw DomainException.UnreachableCodeException()
-                override fun calcDelta(
-                    delta: Array<Double>,
-                    output: IOType,
-                    afterDelta: Array<Double>,
-                    afterWeight: Array<IOType>,
-                ) = throw DomainException.UnreachableCodeException()
-                override fun backward(
-                    weight: Array<IOType>,
-                    delta: Array<Double>,
-                    input: IOType,
-                    rate: Double,
-                ) = throw DomainException.UnreachableCodeException()
-            },
-        )
+data class Input0dConfig(val size: Int) : LayerConfig<IOType.IOType0d> {
+    override val numOfNeuron: Int = size
+    override val numOfOutput: Int = size
+    override val activationFunction: (Double) -> Double = { throw DomainException.UnreachableCodeException() }
+    override val type: LayerType = object : LayerType {
+        override fun forward(
+            input: IOType,
+            output: IOType,
+            weight: Array<IOType>,
+            activationFunction: (Double) -> Double,
+        ) = throw DomainException.UnreachableCodeException()
+        override fun calcDelta(
+            delta: Array<Double>,
+            output: IOType,
+            afterDelta: Array<Double>,
+            afterWeight: Array<IOType>,
+        ) = throw DomainException.UnreachableCodeException()
+        override fun backward(
+            weight: Array<IOType>,
+            delta: Array<Double>,
+            input: IOType,
+            rate: Double,
+        ) = throw DomainException.UnreachableCodeException()
+    }
+    override fun createWeight(random: Random) = throw DomainException.UnreachableCodeException()
+    override fun createOutput() = IOType.IOType0d(Array(numOfNeuron) { 0.0 })
 }
