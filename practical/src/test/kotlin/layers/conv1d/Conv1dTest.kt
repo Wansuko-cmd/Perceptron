e86f7956a68afb8b3981f987fa9b2ac72ab31d90
Wansuko-cmd <63241922+Wansuko-cmd@users.noreply.github.com>
2022-12-09 23:19:36 +0900

    [fix] deConv1dの実装を高速化

--- practical/src/test/kotlin/layers/conv1d/Conv1dTest.kt
+++ practical/src/test/kotlin/layers/conv1d/Conv1dTest.kt
@@ -1,6 +1,9 @@
 package layers.conv1d
 
 import com.google.common.truth.Truth.assertThat
+import layers.layer1d.conv1d
+import layers.layer1d.deConv1d
+import kotlin.system.measureNanoTime
 import kotlin.test.Test
 
 class Conv1dTest {
@@ -11,5 +14,14 @@ class Conv1dTest {
         val output = Array(input.size - kernel.size + 1) { 0.0 }
         input.conv1d(kernel, output) { it }
         assertThat(output).isEqualTo(arrayOf(4.0, 1.0, -2.0))
+    }
+
+    @Test
+    fun deConv1d() {
+        val input = arrayOf(2.0, 1.0, 0.0, -1.0)
+        val kernel = arrayOf(1.0, 2.0)
+        val output = Array(5) { 0.0 }
+        input.deConv1d(kernel, output)
+        assertThat(output).isEqualTo(arrayOf(4.0, 4.0, 1.0, -2.0, -1.0))
     }
 }
