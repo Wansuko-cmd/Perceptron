387f012a9db455702ef0956b96ba04bbe6f26faf
Wansuko-cmd <63241922+Wansuko-cmd@users.noreply.github.com>
2023-01-10 01:28:11 +0900

    [add] Tensorクラスを追加

---
+++ tensor/src/main/kotlin/tensor/Tensor.kt
@@ -0,0 +1,25 @@
+package tensor
+
+abstract class Tensor(
+    private val before: List<Tensor>,
+    val output: Double,
+) {
+    var grad: Double = 0.0
+    abstract fun calcGrad()
+
+    fun backward() {
+        grad = 1.0
+        calcGrad()
+        backwardBefore()
+    }
+
+    private fun backwardBefore() {
+        before.forEach { it.calcGrad() }
+        before.forEach { it.backwardBefore() }
+    }
+
+    fun gradZero() {
+        grad = 0.0
+        before.forEach { it.gradZero() }
+    }
+}
