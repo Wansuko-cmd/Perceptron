5e18ee79218f928907f6ad88033dab93e144ba7f
Wansuko-cmd <63241922+Wansuko-cmd@users.noreply.github.com>
2023-01-10 01:30:53 +0900

    [add] 計算用Tensorを追加

---
+++ tensor/src/main/kotlin/tensor/Sin.kt
@@ -0,0 +1,14 @@
+package tensor
+
+import kotlin.math.cos
+import kotlin.math.sin
+
+class Sin(
+    private val before: Tensor,
+) : Tensor(before = listOf(before), output = sin(before.output)) {
+    override fun calcGrad() {
+        before.grad += grad * cos(before.output)
+    }
+}
+
+fun sin(tensor: Tensor) = Sin(tensor)
