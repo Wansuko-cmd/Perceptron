3c069d6799c5c96577830ae979764776ac9ca94d
Wansuko-cmd <63241922+Wansuko-cmd@users.noreply.github.com>
2025-07-27 14:50:18 +0900

    [update] ライブラリをアップデート

--- practical/build.gradle.kts
+++ practical/build.gradle.kts
@@ -1,24 +1,13 @@
-import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
-
 plugins {
-    kotlin("jvm") version "1.7.10"
-    application
+    kotlin("jvm") version "2.1.20"
 }
 
 dependencies {
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.4")
+    implementation(libs.coroutine)
     testImplementation(libs.bundles.test)
     testImplementation(kotlin("test"))
 }
 
-application {
-    mainClass.set("MainKt")
-}
-
 tasks.test {
     useJUnit()
-}
-
-tasks.withType<KotlinCompile>() {
-    kotlinOptions.jvmTarget = "1.8"
 }
