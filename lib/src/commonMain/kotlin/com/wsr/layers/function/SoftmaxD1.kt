acf85e721d777e67ad22d23de145bce0bd97bbd2
Wansuko-cmd <63241922+Wansuko-cmd@users.noreply.github.com>
2025-07-30 00:27:14 +0900

    [add] SoftmaxD1を追加

---
+++ lib/src/commonMain/kotlin/com/wsr/layers/function/SoftmaxD1.kt
@@ -0,0 +1,27 @@
+package com.wsr.layers.function
+
+import com.wsr.Network
+import com.wsr.common.IOTypeD1
+import com.wsr.layers.Layer
+import kotlin.math.exp
+
+class SoftmaxD1 internal constructor(
+    override val numOfInput: Int,
+    override val numOfOutput: Int,
+) : Layer {
+    override fun expect(input: IOTypeD1): IOTypeD1 = input
+
+    override fun train(
+        input: IOTypeD1,
+        delta: (IOTypeD1) -> IOTypeD1,
+    ): IOTypeD1 {
+        val max = input.max()
+        val exp = input.map { exp(it - max) }
+        val sum = exp.sum()
+        val output = Array(numOfOutput) { exp[it] / sum }
+        return delta(output)
+    }
+}
+
+fun Network.Builder.softmaxD1() =
+    addLayer(SoftmaxD1(numOfInput = numOfInput, numOfOutput = numOfInput))
