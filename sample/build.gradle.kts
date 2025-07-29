5cd2cb8813c732488b83124ff3e871923670dca8
Wansuko-cmd <63241922+Wansuko-cmd@users.noreply.github.com>
2025-07-30 00:33:43 +0900

    [add] Mainにlibを用いたiris用モデルを追加

--- sample/build.gradle.kts
+++ sample/build.gradle.kts
@@ -3,6 +3,8 @@ plugins {
 }
 
 dependencies {
+    implementation(project(":lib"))
+
     implementation(libs.coroutine)
     testImplementation(libs.bundles.test)
     testImplementation(kotlin("test"))
--- sample/src/main/kotlin/Main.kt
+++ sample/src/main/kotlin/Main.kt
@@ -1,2 +1,40 @@
+import com.wsr.Network
+import com.wsr.layers.affine.affineD1
+import com.wsr.layers.bias.biasD1
+import com.wsr.layers.function.reluD1
+import com.wsr.layers.function.softmaxD1
+import dataset.iris.irisDatasets
+
+private const val EPOC = 1000
+
 fun main() {
+    val (train, test) = irisDatasets.shuffled() to irisDatasets.shuffled()
+    val network = Network.Builder(numOfInput = 4, rate = 0.01)
+        .affineD1(50).biasD1().reluD1()
+        .affineD1(3).softmaxD1()
+        .build()
+
+    (1..EPOC).forEach { epoc ->
+        train.forEach { data ->
+            network.train(
+                input = arrayOf(
+                    data.petalLength,
+                    data.petalWidth,
+                    data.sepalLength,
+                    data.sepalWidth,
+                ),
+                label = Array(3) { if (data.label == it) 1.0 else 0.0 },
+            )
+        }
+    }
+    test.count { data ->
+        network.expect(
+            input = arrayOf(
+                data.petalLength,
+                data.petalWidth,
+                data.sepalLength,
+                data.sepalWidth,
+            ),
+        ).toList().maxIndex() == data.label
+    }.let { println(it.toDouble() / test.size.toDouble()) }
 }
