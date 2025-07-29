2907a235004c46263232f3280cf123fdd59ca82a
Wansuko-cmd <63241922+Wansuko-cmd@users.noreply.github.com>
2025-07-30 00:26:02 +0900

    [add] SigmoidD1を追加

---
+++ lib/src/commonMain/kotlin/com/wsr/layers/function/SigmoidD1.kt
@@ -0,0 +1,25 @@
+package com.wsr.layers.function
+
+import com.wsr.Network
+import com.wsr.common.IOTypeD1
+import com.wsr.layers.Layer
+import kotlin.math.exp
+
+class SigmoidD1 internal constructor(
+    override val numOfInput: Int,
+    override val numOfOutput: Int,
+) : Layer {
+    override fun expect(input: IOTypeD1): IOTypeD1 = input
+
+    override fun train(
+        input: IOTypeD1,
+        delta: (IOTypeD1) -> IOTypeD1,
+    ): IOTypeD1 {
+        val output = Array(numOfInput) { 1 / (1 + exp(-input[it])) }
+        val delta = delta(output)
+        return Array(numOfOutput) { delta[it] * (1 - delta[it]) }
+    }
+}
+
+fun Network.Builder.sigmoidD1() =
+    addLayer(SigmoidD1(numOfInput = numOfInput, numOfOutput = numOfInput))
