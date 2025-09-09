61a0fe557805951b2dda76f7aaf4ec3b43550e1c
Wansuko-cmd <63241922+Wansuko-cmd@users.noreply.github.com>
2025-09-09 22:06:56 +0900

    [add] Layerにバッチ版の関数を追加

--- lib/src/commonMain/kotlin/com/wsr/layers/Layer.kt
+++ lib/src/commonMain/kotlin/com/wsr/layers/Layer.kt
@@ -9,6 +9,9 @@ sealed interface Layer {
     fun expect(input: IOType): IOType
     fun train(input: IOType, calcDelta: (IOType) -> IOType): IOType
 
+    fun expect(input: List<IOType>): List<IOType>
+    fun train(input: List<IOType>, calcDelta: (List<IOType>) -> List<IOType>): List<IOType>
+
     @Serializable
     abstract class D1 : Layer {
         abstract val outputSize: Int
@@ -16,12 +19,25 @@ sealed interface Layer {
         protected abstract fun expect(input: IOType.D1): IOType.D1
         protected abstract fun train(input: IOType.D1, calcDelta: (IOType.D1) -> IOType.D1): IOType.D1
 
+        protected abstract fun expect(input: List<IOType.D1>): List<IOType.D1>
+        protected abstract fun train(
+            input: List<IOType.D1>,
+            calcDelta: (List<IOType.D1>) -> List<IOType.D1>,
+        ): List<IOType.D1>
+
         override fun expect(input: IOType): IOType = expect(input = input as IOType.D1)
 
         override fun train(input: IOType, calcDelta: (IOType) -> IOType): IOType =
             train(
                 input = input as IOType.D1,
                 calcDelta = { input: IOType.D1 -> calcDelta(input) as IOType.D1 },
+            )
+
+        override fun expect(input: List<IOType>): List<IOType> = expect(input = input as List<IOType.D1>)
+        override fun train(input: List<IOType>, calcDelta: (List<IOType>) -> List<IOType>): List<IOType> =
+            train(
+                input = input as List<IOType.D1>,
+                calcDelta = { input: List<IOType.D1> -> calcDelta(input) as List<IOType.D1> }
             )
     }
 
@@ -33,11 +49,21 @@ sealed interface Layer {
         protected abstract fun expect(input: IOType.D2): IOType.D2
         protected abstract fun train(input: IOType.D2, calcDelta: (IOType.D2) -> IOType.D2): IOType.D2
 
+        protected abstract fun expect(input: List<IOType.D2>): List<IOType.D2>
+        protected abstract fun train(input: List<IOType.D2>, calcDelta: (List<IOType.D2>) -> List<IOType.D2>): List<IOType.D2>
+
         override fun expect(input: IOType): IOType = expect(input = input as IOType.D2)
         override fun train(input: IOType, calcDelta: (IOType) -> IOType): IOType =
             train(
                 input = input as IOType.D2,
                 calcDelta = { input: IOType.D2 -> calcDelta(input) as IOType.D2 },
+            )
+
+        override fun expect(input: List<IOType>): List<IOType> = expect(input = input as List<IOType.D2>)
+        override fun train(input: List<IOType>, calcDelta: (List<IOType>) -> List<IOType>): List<IOType> =
+            train(
+                input = input as List<IOType.D2>,
+                calcDelta = { input: List<IOType.D2> -> calcDelta(input) as List<IOType.D2> },
             )
     }
 
