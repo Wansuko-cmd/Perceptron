7f7ff2e111b25561de7582cb3ab2df40dbdf5cd1
Wansuko-cmd <63241922+Wansuko-cmd@users.noreply.github.com>
2025-09-16 23:27:19 +0900

    [add] SoftmaxWithLoss層を追加

---
+++ lib/src/commonMain/kotlin/com/wsr/output/SoftmaxWithLoss.kt
@@ -0,0 +1,21 @@
+package com.wsr.output
+
+import com.wsr.IOType
+import com.wsr.d1.minus
+import kotlin.math.exp
+import kotlinx.serialization.Serializable
+
+@Serializable
+internal class SoftmaxWithLoss internal constructor(val outputSize: Int) : Output.D1() {
+    override fun expect(input: List<IOType.D1>): List<IOType.D1> = input
+
+    override fun train(input: List<IOType.D1>, label: List<IOType.D1>): List<IOType.D1> {
+        val output = input.map { (value) ->
+            val max = value.max()
+            val exp = value.map { exp(it - max) }
+            val sum = exp.sum()
+            IOType.d1(outputSize) { exp[it] / sum }
+        }
+        return List(input.size) { i -> output[i] - label[i] }
+    }
+}
