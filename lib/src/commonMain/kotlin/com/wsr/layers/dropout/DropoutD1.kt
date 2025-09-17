4608eedf9908ca70fd225c18a5f7d3eee4c74324
Wansuko-cmd <63241922+Wansuko-cmd@users.noreply.github.com>
2025-09-18 00:17:16 +0900

    [add] dropout層を追加

---
+++ lib/src/commonMain/kotlin/com/wsr/layers/dropout/DropoutD1.kt
@@ -0,0 +1,37 @@
+package com.wsr.layers.dropout
+
+import com.wsr.IOType
+import com.wsr.NetworkBuilder
+import com.wsr.d1.times
+import com.wsr.layers.Layer
+import kotlin.random.Random
+import kotlinx.serialization.Serializable
+
+@Serializable
+class DropoutD1 internal constructor(
+    override val outputSize: Int,
+    private val ratio: Double,
+    private val seed: Int? = null,
+) : Layer.D1() {
+    private val random by lazy { seed?.let { Random(it) } ?: Random }
+
+    override fun expect(input: List<IOType.D1>): List<IOType.D1> = ratio * input
+
+    override fun train(
+        input: List<IOType.D1>,
+        calcDelta: (List<IOType.D1>) -> List<IOType.D1>,
+    ): List<IOType.D1> {
+        val mask = IOType.d1(outputSize) { if (random.nextDouble(0.0, 1.0) <= ratio) 1.0 else 0.0 }
+        val output = input.map { input -> IOType.d1(outputSize) { input[it] * mask[it] } }
+        val delta = calcDelta(output)
+        return delta.map { delta -> IOType.d1(outputSize) { delta[it] * mask[it] } }
+    }
+}
+
+fun <T : IOType> NetworkBuilder.D1<T>.dropout(ratio: Double, seed: Int? = null) = addLayer(
+    layer = DropoutD1(
+        outputSize = inputSize,
+        ratio = ratio,
+        seed = seed,
+    ),
+)
