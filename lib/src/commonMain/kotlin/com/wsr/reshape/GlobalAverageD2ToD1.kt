2970d6dd8d4da027afd03ebcaeb59561e4ccff3f
Wansuko-cmd <63241922+Wansuko-cmd@users.noreply.github.com>
2025-09-28 19:19:22 +0900

    [add] GlobalAverageの実装を追加

---
+++ lib/src/commonMain/kotlin/com/wsr/reshape/GlobalAverageD2ToD1.kt
@@ -0,0 +1,35 @@
+package com.wsr.reshape
+
+import com.wsr.IOType
+import com.wsr.NetworkBuilder
+import com.wsr.d1.average
+import com.wsr.d1.div
+import kotlinx.serialization.Serializable
+
+@Serializable
+internal class GlobalAverageD2ToD1(
+    private val inputX: Int,
+    private val inputY: Int,
+) : Reshape.D2ToD1() {
+    override val outputSize: Int = inputX
+
+    override fun expect(input: List<IOType.D2>): List<IOType.D1> = input.map { input ->
+        IOType.d1(inputX) { input[it].average() }
+    }
+
+    override fun train(
+        input: List<IOType.D2>,
+        calcDelta: (List<IOType.D1>) -> List<IOType.D1>,
+    ): List<IOType.D2> {
+        val output = input.map { input -> IOType.d1(outputSize) { input[it].average() } }
+        val delta = calcDelta(output)
+        return List(input.size) {
+            val delta = delta[it] / inputY.toDouble()
+            IOType.d2(inputX, inputY) { x, _ -> delta[x] }
+        }
+    }
+}
+
+fun <T : IOType> NetworkBuilder.D2<T>.globalAverageToD1() = addReshape(
+    reshape = GlobalAverageD2ToD1(inputX, inputY),
+)
