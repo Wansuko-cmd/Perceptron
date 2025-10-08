591bd926c62bc4411d8e8d37eac98122ecf43c9f
Wansuko-cmd <63241922+Wansuko-cmd@users.noreply.github.com>
2025-10-08 22:04:10 +0900

    [add] ddotの定義を追加

--- io-type/blas/src/commonMain/kotlin/com/wsr/BLAS.kt
+++ io-type/blas/src/commonMain/kotlin/com/wsr/BLAS.kt
@@ -4,4 +4,24 @@ expect object BLAS : IBLAS
 
 interface IBLAS {
     val isNative: Boolean get() = false
+
+    fun ddot(
+        n: Int,
+        x: DoubleArray,
+        incX: Int,
+        y: DoubleArray,
+        incY: Int,
+    ): Double {
+        var result = 0.0
+        var xi = 0
+        var yi = 0
+
+        repeat(n) {
+            result += x[xi] * y[yi]
+            xi += incX
+            yi += incY
+        }
+
+        return result
+    }
 }
