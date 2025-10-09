8a157ebf2deec1a2f3d0883fdc7b728c38bd1274
Wansuko-cmd <63241922+Wansuko-cmd@users.noreply.github.com>
2025-10-09 21:55:23 +0900

    [add] JBLASを読み込む機構を追加

--- io-type/blas/src/jvmMain/kotlin/com/wsr/LoaderExt.kt
+++ io-type/blas/src/jvmMain/kotlin/com/wsr/LoaderExt.kt
@@ -1,6 +1,22 @@
 package com.wsr
 
+import java.nio.file.Files
+import java.nio.file.StandardCopyOption
+
+private const val name = "JBLAS"
+
 internal fun loadJBLAS(): JBLAS? {
-    // TODO(Wansuko-cmd) Cライブラリの読み込み
-    return null
+    val resource = System.mapLibraryName(name)
+    val result = BLAS::class.java
+        .classLoader
+        .getResourceAsStream(resource)
+        ?.use { inputStream ->
+            val path = Files
+                .createTempDirectory(name)
+                .also { it.toFile().deleteOnExit() }
+                .resolve(resource)
+            Files.copy(inputStream, path, StandardCopyOption.REPLACE_EXISTING)
+            System.load(path.toString())
+        }
+    return if (result != null) JBLAS() else null
 }
