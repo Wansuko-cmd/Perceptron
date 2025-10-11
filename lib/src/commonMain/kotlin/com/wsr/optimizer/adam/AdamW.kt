9fb17cf30f28f0f90d2ec1423da75320e1a505cb
Wansuko-cmd <63241922+Wansuko-cmd@users.noreply.github.com>
2025-10-11 15:55:08 +0900

    [add] AdamWを追加

---
+++ lib/src/commonMain/kotlin/com/wsr/optimizer/adam/AdamW.kt
@@ -0,0 +1,125 @@
+package com.wsr.optimizer.adam
+
+import com.wsr.IOType
+import com.wsr.operator.div
+import com.wsr.operator.minus
+import com.wsr.operator.plus
+import com.wsr.operator.times
+import com.wsr.optimizer.Optimizer
+import com.wsr.pow.pow
+import com.wsr.sqrt.sqrt
+import kotlinx.serialization.Serializable
+import kotlin.math.pow
+
+private const val E = 1e-8
+
+@Serializable
+data class AdamW(
+    private val rate: Double,
+    private val momentum: Double = 0.9,
+    private val rms: Double = 0.999,
+    private val decay: Double = 0.01,
+) :
+    Optimizer {
+    override fun d1(size: Int): Optimizer.D1 = AdamWD1(
+        rate = rate,
+        momentum = momentum,
+        rms = rms,
+        decay = decay,
+        shape = listOf(size),
+    )
+
+    override fun d2(x: Int, y: Int): Optimizer.D2 = AdamWD2(
+        rate = rate,
+        momentum = momentum,
+        rms = rms,
+        decay = decay,
+        shape = listOf(x, y),
+    )
+
+    override fun d3(x: Int, y: Int, z: Int): Optimizer.D3 = AdamWD3(
+        rate = rate,
+        momentum = momentum,
+        rms = rms,
+        decay = decay,
+        shape = listOf(x, y, z),
+    )
+}
+
+@Serializable
+internal data class AdamWD1(
+    private val rate: Double,
+    private val momentum: Double,
+    private val rms: Double,
+    private val decay: Double,
+    private val shape: List<Int>,
+) : Optimizer.D1 {
+    private var m: IOType.D1 = IOType.d1(shape)
+    private var v: IOType.D1 = IOType.d1(shape)
+    private var t: Int = 0
+
+    override fun adapt(weight: IOType.D1, dw: IOType.D1): IOType.D1 {
+        t += 1
+
+        m = momentum * m + (1 - momentum) * dw
+        v = rms * v + (1 - rms) * dw.pow(2)
+
+        val mHat = m / (1.0 - momentum.pow(t.toDouble()))
+        val vHat = v / (1.0 - rms.pow(t.toDouble()))
+
+        val e = IOType.d1(dw.shape) { E }
+        return (1 - decay) * weight - rate / (vHat.sqrt() + e) * mHat
+    }
+}
+
+@Serializable
+internal data class AdamWD2(
+    private val rate: Double,
+    private val momentum: Double,
+    private val rms: Double,
+    private val decay: Double,
+    private val shape: List<Int>,
+) : Optimizer.D2 {
+    private var m: IOType.D2 = IOType.d2(shape)
+    private var v: IOType.D2 = IOType.d2(shape)
+    private var t: Int = 0
+
+    override fun adapt(weight: IOType.D2, dw: IOType.D2): IOType.D2 {
+        t += 1
+
+        m = momentum * m + (1 - momentum) * dw
+        v = rms * v + (1 - rms) * dw.pow(2)
+
+        val mHat = m / (1.0 - momentum.pow(t.toDouble()))
+        val vHat = v / (1.0 - rms.pow(t.toDouble()))
+
+        val e = IOType.d2(dw.shape) { _, _ -> E }
+        return (1 - decay) * weight - rate / (vHat.sqrt() + e) * mHat
+    }
+}
+
+@Serializable
+internal data class AdamWD3(
+    private val rate: Double,
+    private val momentum: Double,
+    private val rms: Double,
+    private val decay: Double,
+    private val shape: List<Int>,
+) : Optimizer.D3 {
+    private var m: IOType.D3 = IOType.d3(shape)
+    private var v: IOType.D3 = IOType.d3(shape)
+    private var t: Int = 0
+
+    override fun adapt(weight: IOType.D3, dw: IOType.D3): IOType.D3 {
+        t += 1
+
+        m = momentum * m + (1 - momentum) * dw
+        v = rms * v + (1 - rms) * dw.pow(2)
+
+        val mHat = m / (1.0 - momentum.pow(t.toDouble()))
+        val vHat = v / (1.0 - rms.pow(t.toDouble()))
+
+        val e = IOType.d3(dw.shape) { _, _, _ -> E }
+        return (1 - decay) * weight - rate / (vHat.sqrt() + e) * mHat
+    }
+}
