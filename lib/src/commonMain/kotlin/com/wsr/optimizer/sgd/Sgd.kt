8047e0f6e960efbe9c91d513b417f559372897be
Wansuko-cmd <63241922+Wansuko-cmd@users.noreply.github.com>
2025-10-11 11:58:06 +0900

    [fix] Serializeできるように修正

--- lib/src/commonMain/kotlin/com/wsr/optimizer/sgd/Sgd.kt
+++ lib/src/commonMain/kotlin/com/wsr/optimizer/sgd/Sgd.kt
@@ -3,7 +3,9 @@ package com.wsr.optimizer.sgd
 import com.wsr.IOType
 import com.wsr.operator.times
 import com.wsr.optimizer.Optimizer
+import kotlinx.serialization.Serializable
 
+@Serializable
 data class Sgd(private val rate: Double) : Optimizer {
     override fun d1(): Optimizer.D1 = SgdD1(rate)
 
@@ -12,14 +14,17 @@ data class Sgd(private val rate: Double) : Optimizer {
     override fun d3(): Optimizer.D3 = SgdD3(rate)
 }
 
+@Serializable
 data class SgdD1(private val rate: Double) : Optimizer.D1 {
     override fun adapt(dw: IOType.D1): IOType.D1 = rate * dw
 }
 
+@Serializable
 data class SgdD2(private val rate: Double) : Optimizer.D2 {
     override fun adapt(dw: IOType.D2): IOType.D2 = rate * dw
 }
 
+@Serializable
 data class SgdD3(private val rate: Double) : Optimizer.D3 {
     override fun adapt(dw: IOType.D3): IOType.D3 = rate * dw
 }
