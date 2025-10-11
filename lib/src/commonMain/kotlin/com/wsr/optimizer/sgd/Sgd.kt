737e9919636253dab37ca00ba71a6507b89acb55
Wansuko-cmd <63241922+Wansuko-cmd@users.noreply.github.com>
2025-10-11 11:40:22 +0900

    [add] Sgdを追加

---
+++ lib/src/commonMain/kotlin/com/wsr/optimizer/sgd/Sgd.kt
@@ -0,0 +1,25 @@
+package com.wsr.optimizer.sgd
+
+import com.wsr.IOType
+import com.wsr.operator.times
+import com.wsr.optimizer.Optimizer
+
+data class Sgd(private val rate: Double) : Optimizer {
+    override fun d1(): Optimizer.D1 = SgdD1(rate)
+
+    override fun d2(): Optimizer.D2 = SgdD2(rate)
+
+    override fun d3(): Optimizer.D3 = SgdD3(rate)
+}
+
+data class SgdD1(private val rate: Double) : Optimizer.D1 {
+    override fun adapt(dw: IOType.D1): IOType.D1 = rate * dw
+}
+
+data class SgdD2(private val rate: Double) : Optimizer.D2 {
+    override fun adapt(dw: IOType.D2): IOType.D2 = rate * dw
+}
+
+data class SgdD3(private val rate: Double) : Optimizer.D3 {
+    override fun adapt(dw: IOType.D3): IOType.D3 = rate * dw
+}
