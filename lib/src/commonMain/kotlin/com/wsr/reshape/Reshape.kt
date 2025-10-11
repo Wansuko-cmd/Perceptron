2c4ad4d21bda32d7d0112b1810c61672899a46cc
Wansuko-cmd <63241922+Wansuko-cmd@users.noreply.github.com>
2025-10-11 20:27:49 +0900

    [add] ReshapeにD3ToD1を追加

--- lib/src/commonMain/kotlin/com/wsr/reshape/Reshape.kt
+++ lib/src/commonMain/kotlin/com/wsr/reshape/Reshape.kt
@@ -44,4 +44,23 @@ sealed interface Reshape : Layer {
             calcDelta = { input: List<IOType.D2> -> calcDelta(input) as List<IOType.D2> },
         )
     }
+
+    @Serializable
+    abstract class D3ToD1 : Reshape {
+        abstract val outputSize: Int
+
+        protected abstract fun expect(input: List<IOType.D3>): List<IOType.D1>
+
+        protected abstract fun train(
+            input: List<IOType.D3>,
+            calcDelta: (List<IOType.D1>) -> List<IOType.D1>,
+        ): List<IOType.D3>
+
+        final override fun _expect(input: List<IOType>): List<IOType> = expect(input = input as List<IOType.D3>)
+
+        final override fun _train(input: List<IOType>, calcDelta: (List<IOType>) -> List<IOType>): List<IOType> = train(
+            input = input as List<IOType.D3>,
+            calcDelta = { input: List<IOType.D1> -> calcDelta(input) as List<IOType.D1> },
+        )
+    }
 }
