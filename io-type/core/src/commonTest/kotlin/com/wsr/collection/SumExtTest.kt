e573f63fe9daa96f2f2b2985e5275f2cb3a55bf8
Wansuko-cmd <63241922+Wansuko-cmd@users.noreply.github.com>
2025-10-13 12:01:47 +0900

    wip

--- io-type/core/src/commonTest/kotlin/com/wsr/collection/SumExtTest.kt
+++ io-type/core/src/commonTest/kotlin/com/wsr/collection/SumExtTest.kt
@@ -1,16 +1,15 @@
 @file:Suppress("NonAsciiCharacters")
 
-package com.wsr.sum
+package com.wsr.collection
 
 import com.wsr.IOType
-import com.wsr.collection.sum
 import kotlin.test.Test
 import kotlin.test.assertEquals
 
 class SumExtTest {
     @Test
     fun `D1のsum=全要素の合計`() {
-        val a = IOType.d1(listOf(1.0, 2.0, 3.0, 4.0))
+        val a = IOType.Companion.d1(listOf(1.0, 2.0, 3.0, 4.0))
         val result = a.sum()
         assertEquals(
             expected = 10.0,
@@ -22,20 +21,20 @@ class SumExtTest {
     fun `List_D1のsum=各インデックスごとの合計を持つD1`() {
         val list =
             listOf(
-                IOType.d1(listOf(1.0, 2.0, 3.0)),
-                IOType.d1(listOf(4.0, 5.0, 6.0)),
-                IOType.d1(listOf(7.0, 8.0, 9.0)),
+                IOType.Companion.d1(listOf(1.0, 2.0, 3.0)),
+                IOType.Companion.d1(listOf(4.0, 5.0, 6.0)),
+                IOType.Companion.d1(listOf(7.0, 8.0, 9.0)),
             )
         val result = list.sum()
         assertEquals(
-            expected = IOType.d1(listOf(12.0, 15.0, 18.0)),
+            expected = IOType.Companion.d1(listOf(12.0, 15.0, 18.0)),
             actual = result,
         )
     }
 
     @Test
     fun `D2のsum=全要素の合計`() {
-        val a = IOType.d2(2, 3) { x, y -> (x * 3 + y + 1).toDouble() }
+        val a = IOType.Companion.d2(2, 3) { x, y -> (x * 3 + y + 1).toDouble() }
         val result = a.sum()
         assertEquals(
             expected = 21.0,
@@ -48,10 +47,10 @@ class SumExtTest {
         // [[1, 2],
         //  [3, 4],
         //  [5, 6]]
-        val a = IOType.d2(3, 2) { x, y -> (x * 2 + y + 1).toDouble() }
+        val a = IOType.Companion.d2(3, 2) { x, y -> (x * 2 + y + 1).toDouble() }
         val result = a.sum(axis = 0)
         assertEquals(
-            expected = IOType.d1(listOf(9.0, 12.0)),
+            expected = IOType.Companion.d1(listOf(9.0, 12.0)),
             actual = result,
         )
     }
@@ -61,10 +60,10 @@ class SumExtTest {
         // [[1, 2],
         //  [3, 4],
         //  [5, 6]]
-        val a = IOType.d2(3, 2) { x, y -> (x * 2 + y + 1).toDouble() }
+        val a = IOType.Companion.d2(3, 2) { x, y -> (x * 2 + y + 1).toDouble() }
         val result = a.sum(axis = 1)
         assertEquals(
-            expected = IOType.d1(listOf(3.0, 7.0, 11.0)),
+            expected = IOType.Companion.d1(listOf(3.0, 7.0, 11.0)),
             actual = result,
         )
     }
