b564bee132832e106be050ae05d6d5691c82f4f5
Wansuko-cmd <63241922+Wansuko-cmd@users.noreply.github.com>
2025-10-25 02:29:32 +0900

    [add] SkipD2を追加

---
+++ lib/src/commonMain/kotlin/com/wsr/process/skip/SkipD2.kt
@@ -0,0 +1,75 @@
+@file:Suppress("UNCHECKED_CAST")
+
+package com.wsr.process.skip
+
+import com.wsr.IOType
+import com.wsr.NetworkBuilder
+import com.wsr.operator.plus
+import com.wsr.process.Process
+import kotlinx.serialization.Serializable
+
+private typealias CALC_DELTA_D2 = (List<IOType.D2>) -> List<IOType.D2>
+
+@Serializable
+class SkipD2 internal constructor(
+    private val layers: List<Process.D2> = emptyList(),
+    override val outputX: Int,
+    override val outputY: Int,
+) : Process.D2() {
+    override fun expect(input: List<IOType.D2>): List<IOType.D2> {
+        return input + layers.fold(input) { acc, layer -> layer._expect(acc) as List<IOType.D2> }
+    }
+
+    private val trainChain: ((List<IOType.D2>) -> List<IOType.D2>) -> CALC_DELTA_D2 =
+        layers.foldRight(
+            initial = { final: CALC_DELTA_D2 -> final },
+        ) { layer, acc ->
+            { final ->
+                { input ->
+                    layer._train(input) { acc(final)(it as List<IOType.D2>) } as List<IOType.D2>
+                }
+            }
+        }
+
+    override fun train(
+        input: List<IOType.D2>,
+        calcDelta: (List<IOType.D2>) -> List<IOType.D2>,
+    ): List<IOType.D2> {
+        var skipDelta: List<IOType.D2> = emptyList()
+
+        val final: CALC_DELTA_D2 = { acc ->
+            val output = input + acc
+            calcDelta(output).also { skipDelta = it }
+        }
+        val mainDelta = trainChain(final)(input)
+
+        return mainDelta + skipDelta
+    }
+}
+
+fun <T : IOType> NetworkBuilder.D2<T>.skip(
+    builder: NetworkBuilder.D2<T>.() -> NetworkBuilder.D2<T>,
+): NetworkBuilder.D2<T> {
+    val layers = builder().layers
+        .drop(layers.size)
+        .filterIsInstance<Process.D2>()
+
+    val last = layers.last()
+    check(inputX == last.outputX && inputY == last.outputY) {
+        """
+            invalid layers.
+            inputX: $inputX
+            inputY: $inputY
+            outputX: ${last.outputX}
+            outputY: ${last.outputY}
+        """.trimIndent()
+    }
+
+    return addProcess(
+        process = SkipD2(
+            outputX = inputX,
+            outputY = inputY,
+            layers = layers,
+        ),
+    )
+}
