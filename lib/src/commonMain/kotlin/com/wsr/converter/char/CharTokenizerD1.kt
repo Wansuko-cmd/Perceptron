514345db1e5e00d2bcc92cd31de9d42be45f7891
Wansuko-cmd <63241922+Wansuko-cmd@users.noreply.github.com>
2025-10-26 23:36:37 +0900

    [add] ChatTokenizerD1を追加

---
+++ lib/src/commonMain/kotlin/com/wsr/converter/char/CharTokenizerD1.kt
@@ -0,0 +1,45 @@
+package com.wsr.converter.char
+
+import com.wsr.IOType
+import com.wsr.NetworkBuilder
+import com.wsr.converter.Converter
+import com.wsr.optimizer.Optimizer
+import kotlinx.serialization.Serializable
+
+@Serializable
+class CharTokenizerD1 internal constructor(
+    override val outputSize: Int,
+) : Converter.D1<String>() {
+    override fun encode(input: List<String>): List<IOType.D1> = input.map { text ->
+        IOType.D1(
+            value = DoubleArray(outputSize) { index ->
+                text.getOrNull(index)
+                    ?.let { charToId[it] }
+                    ?: 0.0
+            },
+        )
+    }
+
+    override fun decode(input: List<IOType.D1>): List<String> = input.map { input ->
+        input.value
+            .map { chars.getOrNull(it.toInt()) }
+            .filterNotNull()
+            .joinToString("")
+    }
+
+    companion object {
+        private val chars = " abcdefghijklmnopqrstuvwxyz.,!?".toList()
+        private val charToId = chars.mapIndexed { index, char -> char to index.toDouble() }.toMap()
+        val vocabSize = chars.size
+    }
+}
+
+fun NetworkBuilder.Companion.charTokenizerD1(
+    maxInputSize: Int,
+    optimizer: Optimizer,
+    seed: Int? = null,
+) = inputD1(
+    converter = CharTokenizerD1(maxInputSize),
+    optimizer = optimizer,
+    seed = seed,
+)
