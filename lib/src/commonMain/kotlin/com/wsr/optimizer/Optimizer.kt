f72e04991055abd2a949b91c32e8d13dbad3a51a
Wansuko-cmd <63241922+Wansuko-cmd@users.noreply.github.com>
2025-10-29 00:25:22 +0900

    [add] Optimizer内に実装を追加

--- lib/src/commonMain/kotlin/com/wsr/optimizer/Optimizer.kt
+++ lib/src/commonMain/kotlin/com/wsr/optimizer/Optimizer.kt
@@ -1,7 +1,11 @@
 package com.wsr.optimizer
 
 import com.wsr.IOType
+import com.wsr.collection.sum
+import com.wsr.operator.times
+import com.wsr.power.pow
 import kotlinx.serialization.Serializable
+import kotlin.math.sqrt
 
 interface Optimizer {
     fun d1(size: Int): D1
@@ -9,17 +13,50 @@ interface Optimizer {
     fun d3(x: Int, y: Int, z: Int): D3
 
     @Serializable
-    abstract class D1 {
-        abstract fun adapt(weight: IOType.D1, dw: IOType.D1): IOType.D1
+    abstract class D1(private val maxNorm: Double = Double.MAX_VALUE) {
+        protected abstract fun adapt(weight: IOType.D1, dw: IOType.D1): IOType.D1
+
+        fun adapt(weight: IOType.D1, dw: IOType.D1, enableClip: Boolean = true): IOType.D1 {
+            val norm = sqrt(dw.pow(2).sum())
+            val scaled = if (norm > maxNorm && enableClip) {
+                val scale = maxNorm / norm
+                dw * scale
+            } else {
+                dw
+            }
+            return adapt(weight, scaled)
+        }
     }
 
     @Serializable
-    abstract class D2 {
-        abstract fun adapt(weight: IOType.D2, dw: IOType.D2): IOType.D2
+    abstract class D2(private val maxNorm: Double = Double.MAX_VALUE) {
+        protected abstract fun adapt(weight: IOType.D2, dw: IOType.D2): IOType.D2
+
+        fun adapt(weight: IOType.D2, dw: IOType.D2, enableClip: Boolean = true): IOType.D2 {
+            val norm = sqrt(dw.pow(2).sum())
+            val scaled = if (norm > maxNorm && enableClip) {
+                val scale = maxNorm / norm
+                dw * scale
+            } else {
+                dw
+            }
+            return adapt(weight, scaled)
+        }
     }
 
     @Serializable
-    abstract class D3 {
-        abstract fun adapt(weight: IOType.D3, dw: IOType.D3): IOType.D3
+    abstract class D3(private val maxNorm: Double = Double.MAX_VALUE) {
+        protected abstract fun adapt(weight: IOType.D3, dw: IOType.D3): IOType.D3
+
+        fun adapt(weight: IOType.D3, dw: IOType.D3, enableClip: Boolean = true): IOType.D3 {
+            val norm = sqrt(dw.pow(2).sum())
+            val scaled = if (norm > maxNorm && enableClip) {
+                val scale = maxNorm / norm
+                dw * scale
+            } else {
+                dw
+            }
+            return adapt(weight, scaled)
+        }
     }
 }
