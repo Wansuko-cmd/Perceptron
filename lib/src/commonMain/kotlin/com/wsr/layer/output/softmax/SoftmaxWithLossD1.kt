9c72e929b133c5d5e384e169816a97ccf1f308a5
Wansuko-cmd <63241922+Wansuko-cmd@users.noreply.github.com>
2025-11-02 15:06:43 +0900

    [add] temperatureを追加

--- lib/src/commonMain/kotlin/com/wsr/layer/output/softmax/SoftmaxWithLossD1.kt
+++ lib/src/commonMain/kotlin/com/wsr/layer/output/softmax/SoftmaxWithLossD1.kt
@@ -6,31 +6,26 @@ import com.wsr.converter.Converter
 import com.wsr.layer.output.Output
 import com.wsr.operator.div
 import com.wsr.operator.minus
-import kotlinx.serialization.Serializable
 import kotlin.math.exp
+import kotlinx.serialization.Serializable
 
 @Serializable
-internal class SoftmaxWithLossD1 internal constructor(
-    val outputSize: Int,
-    val temperature: Double,
-) : Output.D1() {
+internal class SoftmaxWithLossD1 internal constructor(val outputSize: Int, val temperature: Double) : Output.D1() {
     override fun expect(input: List<IOType.D1>): List<IOType.D1> = input.also { println(it) }
 
     override fun train(input: List<IOType.D1>, label: List<IOType.D1>): List<IOType.D1> {
         val input = input / temperature
         val output = input.map { (value) ->
-                val max = value.max()
-                val exp = value.map { exp(it - max) }
-                val sum = exp.sum()
-                IOType.d1(outputSize) { exp[it] / sum }
-            }
+            val max = value.max()
+            val exp = value.map { exp(it - max) }
+            val sum = exp.sum()
+            IOType.d1(outputSize) { exp[it] / sum }
+        }
         return List(input.size) { i -> output[i] - label[i] }
     }
 }
 
-fun <T> NetworkBuilder.D1<T>.softmaxWithLoss(
-    temperature: Double = 1.0,
-) = addOutput(
+fun <T> NetworkBuilder.D1<T>.softmaxWithLoss(temperature: Double = 1.0) = addOutput(
     output = SoftmaxWithLossD1(
         outputSize = inputSize,
         temperature = temperature,
