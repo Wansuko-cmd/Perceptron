0d25705bf70d5f1eaf175c0ce15a609b1501a268
Wansuko-cmd <63241922+Wansuko-cmd@users.noreply.github.com>
2025-11-02 18:17:13 +0900

    [add] Randomを追加

---
+++ lib/src/commonMain/kotlin/com/wsr/initializer/Random.kt
@@ -0,0 +1,43 @@
+package com.wsr.initializer
+
+import com.wsr.IOType
+import kotlinx.serialization.Serializable
+import kotlin.random.Random
+
+@Serializable
+class Random(
+    private val seed: Int? = null,
+    private val from: Double = -1.0,
+    private val until: Double = 1.0,
+) : WeightInitializer {
+    private val random by lazy { seed?.let { Random(it) } ?: Random }
+    override fun d1(
+        input: List<Int>,
+        output: List<Int>,
+        size: Int,
+    ): IOType.D1 = IOType.d1(size) { random.nextDouble(from, until) }
+
+    override fun d2(
+        input: List<Int>,
+        output: List<Int>,
+        x: Int,
+        y: Int,
+    ): IOType.D2 = IOType.d2(shape = listOf(x, y)) { _, _ -> random.nextDouble(from, until) }
+
+    override fun d3(
+        input: List<Int>,
+        output: List<Int>,
+        x: Int,
+        y: Int,
+        z: Int,
+    ): IOType.D3 = IOType.d3(shape = listOf(x, y, z)) { _, _, _ -> random.nextDouble(from, until) }
+
+    override fun d4(
+        input: List<Int>,
+        output: List<Int>,
+        i: Int,
+        j: Int,
+        k: Int,
+        n: Int,
+    ): IOType.D4 = IOType.d4(shape = listOf(i, j, k, n)) { _, _, _, _ -> random.nextDouble(from, until) }
+}
