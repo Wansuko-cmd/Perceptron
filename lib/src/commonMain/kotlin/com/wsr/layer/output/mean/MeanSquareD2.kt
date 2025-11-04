8f45841730c5777f6ab9e091dd485110159c782e
Wansuko-cmd <63241922+Wansuko-cmd@users.noreply.github.com>
2025-11-04 23:04:19 +0900

    [add] MeanSquareD2を追加

---
+++ lib/src/commonMain/kotlin/com/wsr/layer/output/mean/MeanSquareD2.kt
@@ -0,0 +1,33 @@
+package com.wsr.layer.output.mean
+
+import com.wsr.IOType
+import com.wsr.NetworkBuilder
+import com.wsr.converter.Converter
+import com.wsr.layer.output.Output
+import com.wsr.operator.minus
+import kotlinx.serialization.Serializable
+
+@Serializable
+internal class MeanSquareD2 internal constructor(
+    val outputX: Int,
+    val outputY: Int,
+) : Output.D2() {
+    override fun expect(input: List<IOType.D2>): List<IOType.D2> = input
+
+    override fun train(input: List<IOType.D2>, label: List<IOType.D2>): List<IOType.D2> = input - label
+}
+
+fun <T> NetworkBuilder.D2<T>.meanSquare() = addOutput(
+    output = MeanSquareD2(
+        outputX = inputX,
+        outputY = inputY,
+    ),
+)
+
+fun <I, O> NetworkBuilder.D2<I>.meanSquare(converter: NetworkBuilder.D2<I>.() -> Converter.D2<O>) = addOutput(
+    output = MeanSquareD2(
+        outputX = inputX,
+        outputY = inputY,
+    ),
+    converter = converter,
+)
