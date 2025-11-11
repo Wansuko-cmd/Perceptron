1212963b8be2111882b812c3c36001676e96ad74
Wansuko-cmd <63241922+Wansuko-cmd@users.noreply.github.com>
2025-11-11 23:32:04 +0900

    [move] BatchAverageを移動

--- io-type/core/src/commonMain/kotlin/com/wsr/collection/AverageExt.kt
+++ io-type/core/src/commonMain/kotlin/com/wsr/collection/AverageExt.kt
@@ -49,33 +49,3 @@ fun IOType.D3.average(axis: Int) = when (axis) {
 
 @JvmName("averageAxisToD3s")
 fun List<IOType.D3>.average(axis: Int) = map { it.average(axis) }
-
-/**
- * batch average
- */
-fun List<IOType.D1>.batchAverage(): IOType.D1 {
-    val result = first().value.copyOf()
-    for (i in 1 until size) {
-        BLAS.daxpy(n = result.size, alpha = 1.0, x = this[i].value, incX = 1, y = result, incY = 1)
-    }
-    BLAS.dscal(n = result.size, alpha = 1.0 / size, x = result, incX = 1)
-    return IOType.D1(result)
-}
-
-fun List<IOType.D2>.batchAverage(): IOType.D2 {
-    val result = first().value.copyOf()
-    for (i in 1 until size) {
-        BLAS.daxpy(n = result.size, alpha = 1.0, x = this[i].value, incX = 1, y = result, incY = 1)
-    }
-    BLAS.dscal(n = result.size, alpha = 1.0 / size, x = result, incX = 1)
-    return IOType.D2(result, first().shape)
-}
-
-fun List<IOType.D3>.batchAverage(): IOType.D3 {
-    val result = first().value.copyOf()
-    for (i in 1 until size) {
-        BLAS.daxpy(n = result.size, alpha = 1.0, x = this[i].value, incX = 1, y = result, incY = 1)
-    }
-    BLAS.dscal(n = result.size, alpha = 1.0 / size, x = result, incX = 1)
-    return IOType.D3(result, first().shape)
-}
