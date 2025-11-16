343ce84be04f2fe9fd42b7c838ae6aa5f3793163
Wansuko-cmd <63241922+Wansuko-cmd@users.noreply.github.com>
2025-11-16 16:32:06 +0900

    [delete] 参考値を削除

--- sample/src/main/kotlin/Main.kt
+++ sample/src/main/kotlin/Main.kt
@@ -2,7 +2,6 @@ import dataset.mnist.createMnistModel
 import kotlin.time.measureTime
 
 fun main() {
-    // 5m 59.059936542s
     measureTime {
         createMnistModel(10, 3)
     }.also(::println)
