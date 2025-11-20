5dc859e6e053056ca838b102f5ce15bb92f739fd
Wansuko-cmd <63241922+Wansuko-cmd@users.noreply.github.com>
2025-11-21 00:19:15 +0900

    [fix] WordD2でPADが入るように修正

--- lib/src/commonMain/kotlin/com/wsr/converter/word/WordD2.kt
+++ lib/src/commonMain/kotlin/com/wsr/converter/word/WordD2.kt
@@ -6,7 +6,6 @@ import com.wsr.collection.maxIndex
 import com.wsr.converter.Converter
 import com.wsr.initializer.WeightInitializer
 import com.wsr.optimizer.Optimizer
-import com.wsr.reshape.toD2
 import kotlinx.serialization.Serializable
 
 @Serializable
@@ -17,10 +16,12 @@ class WordD2(private val words: List<String>, private val length: Int, private v
     private val wordToId = words.mapIndexed { index, word -> word to index }.toMap()
 
     override fun encode(input: List<List<String>>): List<IOType.D2> = input.map { text ->
-        text.map {
-            val id = wordToId[it] ?: unknownIndex
-            IOType.d1(outputY).also { it[id] = 1f }
-        }.toD2()
+        val result = IOType.d2(outputX, outputY)
+        text.forEachIndexed { index, word ->
+            val id = wordToId[word] ?: unknownIndex
+            result[index] = IOType.d1(outputY).also { it[id] = 1f }
+        }
+        result
     }
 
     override fun decode(input: List<IOType.D2>): List<List<String>> = input.map { input ->
