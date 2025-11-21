7208d0cc91e265995c4775b2c2cfe9f5ff852bef
Wansuko-cmd <63241922+Wansuko-cmd@users.noreply.github.com>
2025-11-22 00:54:02 +0900

    [fix] ConverterをBatchに変更

--- lib/src/commonMain/kotlin/com/wsr/converter/Converter.kt
+++ lib/src/commonMain/kotlin/com/wsr/converter/Converter.kt
@@ -1,43 +1,44 @@
 package com.wsr.converter
 
+import com.wsr.Batch
 import com.wsr.IOType
 
 @Suppress("UNCHECKED_CAST")
 sealed interface Converter {
     // Serializer対策で型情報を削除(Network側で型を担保)
     @Suppress("FunctionName")
-    fun _encode(input: List<*>): List<IOType>
+    fun _encode(input: List<*>): Batch<IOType>
 
     @Suppress("FunctionName")
-    fun _decode(input: List<IOType>): List<*>
+    fun _decode(input: Batch<IOType>): List<*>
 
     abstract class D1<T> : Converter {
         abstract val outputSize: Int
-        abstract fun encode(input: List<T>): List<IOType.D1>
-        abstract fun decode(input: List<IOType.D1>): List<T>
+        abstract fun encode(input: List<T>): Batch<IOType.D1>
+        abstract fun decode(input: Batch<IOType.D1>): List<T>
 
-        final override fun _encode(input: List<*>): List<IOType> = encode(input as List<T>)
-        final override fun _decode(input: List<IOType>): List<*> = decode(input as List<IOType.D1>)
+        final override fun _encode(input: List<*>): Batch<IOType> = encode(input as List<T>)
+        final override fun _decode(input: Batch<IOType>): List<*> = decode(input as Batch<IOType.D1>)
     }
 
     abstract class D2<T> : Converter {
         abstract val outputX: Int
         abstract val outputY: Int
-        abstract fun encode(input: List<T>): List<IOType.D2>
-        abstract fun decode(input: List<IOType.D2>): List<T>
+        abstract fun encode(input: List<T>): Batch<IOType.D2>
+        abstract fun decode(input: Batch<IOType.D2>): List<T>
 
-        final override fun _encode(input: List<*>): List<IOType> = encode(input as List<T>)
-        final override fun _decode(input: List<IOType>): List<*> = decode(input as List<IOType.D2>)
+        final override fun _encode(input: List<*>): Batch<IOType> = encode(input as List<T>)
+        final override fun _decode(input: Batch<IOType>): List<*> = decode(input as Batch<IOType.D2>)
     }
 
     abstract class D3<T> : Converter {
         abstract val outputX: Int
         abstract val outputY: Int
         abstract val outputZ: Int
-        abstract fun encode(input: List<T>): List<IOType.D3>
-        abstract fun decode(input: List<IOType.D3>): List<T>
+        abstract fun encode(input: List<T>): Batch<IOType.D3>
+        abstract fun decode(input: Batch<IOType.D3>): List<T>
 
-        final override fun _encode(input: List<*>): List<IOType> = encode(input as List<T>)
-        final override fun _decode(input: List<IOType>): List<*> = decode(input as List<IOType.D3>)
+        final override fun _encode(input: List<*>): Batch<IOType> = encode(input as List<T>)
+        final override fun _decode(input: Batch<IOType>): List<*> = decode(input as Batch<IOType.D3>)
     }
 }
