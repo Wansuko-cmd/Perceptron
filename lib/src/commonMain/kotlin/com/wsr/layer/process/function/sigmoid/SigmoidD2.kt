6b9777168e4d1b571bf106f1bf410eaacd7eb97f
Wansuko-cmd <63241922+Wansuko-cmd@users.noreply.github.com>
2025-11-22 10:27:43 +0900

    [fix] SigmoidD2を修正

--- lib/src/commonMain/kotlin/com/wsr/layer/process/function/sigmoid/SigmoidD2.kt
+++ lib/src/commonMain/kotlin/com/wsr/layer/process/function/sigmoid/SigmoidD2.kt
@@ -3,38 +3,35 @@ package com.wsr.layer.process.function.sigmoid
 import com.wsr.Batch
 import com.wsr.IOType
 import com.wsr.NetworkBuilder
+import com.wsr.batch.collection.map
+import com.wsr.get
 import com.wsr.layer.Context
 import com.wsr.layer.process.Process
-import com.wsr.toBatch
-import com.wsr.toList
-import kotlin.math.exp
 import kotlinx.serialization.Serializable
+import kotlin.math.exp
 
 @Serializable
 class SigmoidD2 internal constructor(override val outputX: Int, override val outputY: Int) : Process.D2() {
     override fun expect(input: Batch<IOType.D2>, context: Context): Batch<IOType.D2> =
-        input.toList().map(::forward).toBatch()
+        input.map(::forward)
 
     override fun train(
         input: Batch<IOType.D2>,
         context: Context,
         calcDelta: (Batch<IOType.D2>) -> Batch<IOType.D2>,
     ): Batch<IOType.D2> {
-        val input = input.toList()
         val output = input.map(::forward)
-        val delta = calcDelta(output.toBatch()).toList()
-        return List(input.size) { i ->
+        val delta = calcDelta(output)
+        return Batch(input.size) { i ->
             IOType.d2(
                 outputX,
                 outputY,
             ) { x, y -> delta[i][x, y] * output[i][x, y] * (1 - output[i][x, y]) }
-        }.toBatch()
+        }
     }
 
     private fun forward(input: IOType.D2): IOType.D2 = IOType.d2(outputX, outputY) { x, y ->
-        1 /
-            (1 + exp(-input[x, y]))
-    }
+        1 / (1 + exp(-input[x, y])) }
 }
 
 fun <T> NetworkBuilder.D2<T>.sigmoid() = addProcess(SigmoidD2(outputX = inputX, outputY = inputY))
