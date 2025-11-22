05a2b7cf923b05146fd83c443a3505e56273ef86
Wansuko-cmd <63241922+Wansuko-cmd@users.noreply.github.com>
2025-11-22 10:21:51 +0900

    [fix] ReLUD1を修正

--- lib/src/commonMain/kotlin/com/wsr/layer/process/function/relu/ReLUD1.kt
+++ lib/src/commonMain/kotlin/com/wsr/layer/process/function/relu/ReLUD1.kt
@@ -3,46 +3,33 @@ package com.wsr.layer.process.function.relu
 import com.wsr.Batch
 import com.wsr.IOType
 import com.wsr.NetworkBuilder
+import com.wsr.batch.collection.map
+import com.wsr.get
 import com.wsr.layer.Context
 import com.wsr.layer.process.Process
-import com.wsr.toBatch
-import com.wsr.toList
 import kotlinx.serialization.Serializable
 
 @Serializable
 class ReLUD1 internal constructor(override val outputSize: Int) : Process.D1() {
     override fun expect(input: Batch<IOType.D1>, context: Context): Batch<IOType.D1> =
-        input.toList().map(::forward).toBatch()
+        input.map(::forward)
 
     override fun train(
         input: Batch<IOType.D1>,
         context: Context,
         calcDelta: (Batch<IOType.D1>) -> Batch<IOType.D1>,
     ): Batch<IOType.D1> {
-        val input = input.toList()
         val output = input.map(::forward)
-        val delta = calcDelta(output.toBatch()).toList()
-        return List(input.size) { i ->
+        val delta = calcDelta(output)
+        return Batch(input.size) { i ->
             IOType.d1(outputSize) {
-                if (input[i][it] >=
-                    0f
-                ) {
-                    delta[i][it]
-                } else {
-                    0f
-                }
+                if (input[i][it] >= 0f) delta[i][it] else 0f
             }
-        }.toBatch()
+        }
     }
 
     private fun forward(input: IOType.D1): IOType.D1 = IOType.d1(outputSize) {
-        if (input[it] >=
-            0f
-        ) {
-            input[it]
-        } else {
-            0f
-        }
+        if (input[it] >= 0f) input[it] else 0f
     }
 }
 
