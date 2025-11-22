bf966bf00433c6655c42e2951a2d1e1c015f98f9
Wansuko-cmd <63241922+Wansuko-cmd@users.noreply.github.com>
2025-11-22 10:25:43 +0900

    [fix] SwishD2を修正

--- lib/src/commonMain/kotlin/com/wsr/layer/process/function/relu/SwishD2.kt
+++ lib/src/commonMain/kotlin/com/wsr/layer/process/function/relu/SwishD2.kt
@@ -3,42 +3,36 @@ package com.wsr.layer.process.function.relu
 import com.wsr.Batch
 import com.wsr.IOType
 import com.wsr.NetworkBuilder
+import com.wsr.batch.collection.map
+import com.wsr.get
 import com.wsr.layer.Context
 import com.wsr.layer.process.Process
-import com.wsr.toBatch
-import com.wsr.toList
-import kotlin.math.exp
 import kotlinx.serialization.Serializable
+import kotlin.math.exp
 
 @Serializable
 class SwishD2 internal constructor(override val outputX: Int, override val outputY: Int) : Process.D2() {
-    override fun expect(input: Batch<IOType.D2>, context: Context): Batch<IOType.D2> = input.toList().map { input ->
+    override fun expect(input: Batch<IOType.D2>, context: Context): Batch<IOType.D2> = input.map { input ->
         IOType.d2(outputX, outputY) { x, y -> input[x, y] / (1 + exp(-input[x, y])) }
-    }.toBatch()
+    }
 
     override fun train(
         input: Batch<IOType.D2>,
         context: Context,
         calcDelta: (Batch<IOType.D2>) -> Batch<IOType.D2>,
     ): Batch<IOType.D2> {
-        val input = input.toList()
-        val sigmoid =
-            input.map { input ->
-                IOType.d2(outputX, outputY) { x, y -> 1 / (1 + exp(-input[x, y])) }
-            }
-        val output =
-            List(input.size) { i ->
-                IOType.d2(
-                    outputX,
-                    outputY,
-                ) { x, y -> input[i][x, y] * sigmoid[i][x, y] }
-            }
-        val delta = calcDelta(output.toBatch()).toList()
-        return List(input.size) { i ->
+        val sigmoid = input.map { input ->
+            IOType.d2(outputX, outputY) { x, y -> 1 / (1 + exp(-input[x, y])) }
+        }
+        val output = Batch(input.size) { i ->
+            IOType.d2(outputX, outputY) { x, y -> input[i][x, y] * sigmoid[i][x, y] }
+        }
+        val delta = calcDelta(output)
+        return Batch(input.size) { i ->
             IOType.d2(outputX, outputY) { x, y ->
                 (output[i][x, y] + sigmoid[i][x, y] * (1 - output[i][x, y])) * delta[i][x, y]
             }
-        }.toBatch()
+        }
     }
 }
 
