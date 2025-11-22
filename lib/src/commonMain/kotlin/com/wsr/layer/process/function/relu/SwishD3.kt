77b3b6ed3d52374da6297cb45503396676678fdf
Wansuko-cmd <63241922+Wansuko-cmd@users.noreply.github.com>
2025-11-22 10:26:20 +0900

    [fix] SwishD3を修正

--- lib/src/commonMain/kotlin/com/wsr/layer/process/function/relu/SwishD3.kt
+++ lib/src/commonMain/kotlin/com/wsr/layer/process/function/relu/SwishD3.kt
@@ -3,30 +3,29 @@ package com.wsr.layer.process.function.relu
 import com.wsr.Batch
 import com.wsr.IOType
 import com.wsr.NetworkBuilder
+import com.wsr.batch.collection.map
+import com.wsr.get
 import com.wsr.layer.Context
 import com.wsr.layer.process.Process
-import com.wsr.toBatch
-import com.wsr.toList
-import kotlin.math.exp
 import kotlinx.serialization.Serializable
+import kotlin.math.exp
 
 @Serializable
 class SwishD3 internal constructor(override val outputX: Int, override val outputY: Int, override val outputZ: Int) :
     Process.D3() {
-    override fun expect(input: Batch<IOType.D3>, context: Context): Batch<IOType.D3> = input.toList().map { input ->
+    override fun expect(input: Batch<IOType.D3>, context: Context): Batch<IOType.D3> = input.map { input ->
         IOType.d3(
             i = outputX,
             j = outputY,
             k = outputZ,
         ) { x, y, z -> input[x, y, z] / (1 + exp(-input[x, y, z])) }
-    }.toBatch()
+    }
 
     override fun train(
         input: Batch<IOType.D3>,
         context: Context,
         calcDelta: (Batch<IOType.D3>) -> Batch<IOType.D3>,
     ): Batch<IOType.D3> {
-        val input = input.toList()
         val sigmoid = input.map { input ->
             IOType.d3(
                 i = outputX,
@@ -34,15 +33,15 @@ class SwishD3 internal constructor(override val outputX: Int, override val outpu
                 k = outputZ,
             ) { x, y, z -> 1 / (1 + exp(-input[x, y, z])) }
         }
-        val output = List(input.size) { i ->
+        val output = Batch(input.size) { i ->
             IOType.d3(
                 i = outputX,
                 j = outputY,
                 k = outputZ,
             ) { x, y, z -> input[i][x, y, z] * sigmoid[i][x, y, z] }
         }
-        val delta = calcDelta(output.toBatch()).toList()
-        return List(input.size) { i ->
+        val delta = calcDelta(output)
+        return Batch(input.size) { i ->
             IOType.d3(
                 i = outputX,
                 j = outputY,
@@ -50,7 +49,7 @@ class SwishD3 internal constructor(override val outputX: Int, override val outpu
             ) { x, y, z ->
                 (output[i][x, y, z] + sigmoid[i][x, y, z] * (1 - output[i][x, y, z])) * delta[i][x, y, z]
             }
-        }.toBatch()
+        }
     }
 }
 
