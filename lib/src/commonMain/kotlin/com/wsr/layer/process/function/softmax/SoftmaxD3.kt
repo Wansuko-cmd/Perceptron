6079025d077253794d9facfd61879001d5e3877d
Wansuko-cmd <63241922+Wansuko-cmd@users.noreply.github.com>
2025-11-22 10:31:02 +0900

    [fix] SoftmaxD3を修正

--- lib/src/commonMain/kotlin/com/wsr/layer/process/function/softmax/SoftmaxD3.kt
+++ lib/src/commonMain/kotlin/com/wsr/layer/process/function/softmax/SoftmaxD3.kt
@@ -3,13 +3,13 @@ package com.wsr.layer.process.function.softmax
 import com.wsr.Batch
 import com.wsr.IOType
 import com.wsr.NetworkBuilder
+import com.wsr.batch.collection.map
 import com.wsr.collection.sum
+import com.wsr.get
 import com.wsr.layer.Context
 import com.wsr.layer.process.Process
-import com.wsr.toBatch
-import com.wsr.toList
-import kotlin.math.exp
 import kotlinx.serialization.Serializable
+import kotlin.math.exp
 
 @Serializable
 class SoftmaxD3 internal constructor(override val outputX: Int, override val outputY: Int, override val outputZ: Int) :
@@ -21,21 +21,21 @@ class SoftmaxD3 internal constructor(override val outputX: Int, override val out
         context: Context,
         calcDelta: (Batch<IOType.D3>) -> Batch<IOType.D3>,
     ): Batch<IOType.D3> {
-        val output = forward(input).toList()
-        val delta = calcDelta(output.toBatch()).toList()
-        return List(input.size) { i ->
+        val output = forward(input)
+        val delta = calcDelta(output)
+        return Batch(input.size) { i ->
             IOType.d3(outputX, outputY, outputZ) { x, y, z ->
                 delta[i][x, y, z] * output[i][x, y, z] * (1 - output[i][x, y, z])
             }
-        }.toBatch()
+        }
     }
 
-    private fun forward(input: Batch<IOType.D3>) = input.toList().map { input ->
+    private fun forward(input: Batch<IOType.D3>) = input.map { input ->
         val max = input.value.max()
         val exp = IOType.d3(shape = input.shape, value = input.value.map { exp(it - max) })
         val sum = exp.sum()
         IOType.d3(outputX, outputY, outputZ) { x, y, z -> exp[x, y, z] / sum }
-    }.toBatch()
+    }
 }
 
 fun <T> NetworkBuilder.D3<T>.softmax() = addProcess(
