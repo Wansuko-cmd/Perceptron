fe800cff062b777a411e60db231702cd6813b2e1
Wansuko-cmd <63241922+Wansuko-cmd@users.noreply.github.com>
2025-11-22 10:24:35 +0900

    [fix] SwishD1を修正

--- lib/src/commonMain/kotlin/com/wsr/layer/process/function/relu/SwishD1.kt
+++ lib/src/commonMain/kotlin/com/wsr/layer/process/function/relu/SwishD1.kt
@@ -3,34 +3,32 @@ package com.wsr.layer.process.function.relu
 import com.wsr.Batch
 import com.wsr.IOType
 import com.wsr.NetworkBuilder
+import com.wsr.batch.collection.map
+import com.wsr.get
 import com.wsr.layer.Context
 import com.wsr.layer.process.Process
-import com.wsr.toBatch
-import com.wsr.toList
-import kotlin.math.exp
 import kotlinx.serialization.Serializable
+import kotlin.math.exp
 
 @Serializable
 class SwishD1 internal constructor(override val outputSize: Int) : Process.D1() {
-    override fun expect(input: Batch<IOType.D1>, context: Context): Batch<IOType.D1> = input.toList().map { input ->
+    override fun expect(input: Batch<IOType.D1>, context: Context): Batch<IOType.D1> = input.map { input ->
         IOType.d1(outputSize) { input[it] / (1 + exp(-input[it])) }
-    }.toBatch()
+    }
 
     override fun train(
         input: Batch<IOType.D1>,
         context: Context,
         calcDelta: (Batch<IOType.D1>) -> Batch<IOType.D1>,
     ): Batch<IOType.D1> {
-        val input = input.toList()
         val sigmoid = input.map { input -> IOType.d1(outputSize) { 1 / (1 + exp(-input[it])) } }
-        val output =
-            List(input.size) { i -> IOType.d1(outputSize) { input[i][it] * sigmoid[i][it] } }
-        val delta = calcDelta(output.toBatch()).toList()
-        return List(input.size) { i ->
+        val output = Batch(input.size) { i -> IOType.d1(outputSize) { input[i][it] * sigmoid[i][it] } }
+        val delta = calcDelta(output)
+        return Batch(input.size) { i ->
             IOType.d1(outputSize) {
                 (output[i][it] + sigmoid[i][it] * (1 - output[i][it])) * delta[i][it]
             }
-        }.toBatch()
+        }
     }
 }
 
