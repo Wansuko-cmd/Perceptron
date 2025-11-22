e7d6bf797a959d60fa3499ed4bdd5108fc2eadb7
Wansuko-cmd <63241922+Wansuko-cmd@users.noreply.github.com>
2025-11-22 10:23:37 +0900

    [fix] ReLUD3を修正

--- lib/src/commonMain/kotlin/com/wsr/layer/process/function/relu/ReLUD3.kt
+++ lib/src/commonMain/kotlin/com/wsr/layer/process/function/relu/ReLUD3.kt
@@ -3,33 +3,32 @@ package com.wsr.layer.process.function.relu
 import com.wsr.Batch
 import com.wsr.IOType
 import com.wsr.NetworkBuilder
+import com.wsr.batch.collection.map
+import com.wsr.get
 import com.wsr.layer.Context
 import com.wsr.layer.process.Process
-import com.wsr.toBatch
-import com.wsr.toList
 import kotlinx.serialization.Serializable
 
 @Serializable
 class ReLUD3 internal constructor(override val outputX: Int, override val outputY: Int, override val outputZ: Int) :
     Process.D3() {
     override fun expect(input: Batch<IOType.D3>, context: Context): Batch<IOType.D3> =
-        input.toList().map(::forward).toBatch()
+        input.map(::forward)
 
     override fun train(
         input: Batch<IOType.D3>,
         context: Context,
         calcDelta: (Batch<IOType.D3>) -> Batch<IOType.D3>,
     ): Batch<IOType.D3> {
-        val input = input.toList()
         val output = input.map(::forward)
-        val delta = calcDelta(output.toBatch()).toList()
-        return List(input.size) { i ->
+        val delta = calcDelta(output)
+        return Batch(input.size) { i ->
             IOType.d3(
                 i = outputX,
                 j = outputY,
                 k = outputZ,
             ) { x, y, z -> if (input[i][x, y, z] >= 0f) delta[i][x, y, z] else 0f }
-        }.toBatch()
+        }
     }
 
     private fun forward(input: IOType.D3): IOType.D3 = IOType.d3(
