272f1f4935220bfe005e5b973374237df32a0985
Wansuko-cmd <63241922+Wansuko-cmd@users.noreply.github.com>
2025-11-22 12:56:36 +0900

    [add] 関数をさらに追加

--- io-type/batch/src/commonMain/kotlin/com/wsr/batch/plus/D2Ext.kt
+++ io-type/batch/src/commonMain/kotlin/com/wsr/batch/plus/D2Ext.kt
@@ -4,7 +4,16 @@ import com.wsr.Batch
 import com.wsr.IOType
 import com.wsr.batch.collection.map
 import com.wsr.batch.collection.mapWith
+import com.wsr.get
 import com.wsr.operator.plus
+import com.wsr.set
+
+@JvmName("batchD2sPlusFloats")
+operator fun Batch<IOType.D2>.plus(other: FloatArray): Batch<IOType.D2> {
+    val result = copy()
+    for (i in result.indices) result[i] += other[i]
+    return result
+}
 
 @JvmName("batchD2sPlusD2")
 operator fun Batch<IOType.D2>.plus(other: IOType.D2) = map { it + other }
--- io-type/batch/src/commonMain/kotlin/com/wsr/batch/plus/D3Ext.kt
+++ io-type/batch/src/commonMain/kotlin/com/wsr/batch/plus/D3Ext.kt
@@ -4,7 +4,16 @@ import com.wsr.Batch
 import com.wsr.IOType
 import com.wsr.batch.collection.map
 import com.wsr.batch.collection.mapWith
+import com.wsr.get
 import com.wsr.operator.plus
+import com.wsr.set
+
+@JvmName("batchD3sPlusFloats")
+operator fun Batch<IOType.D3>.plus(other: FloatArray): Batch<IOType.D3> {
+    val result = copy()
+    for (i in result.indices) result[i] += other[i]
+    return result
+}
 
 @JvmName("batchD3sPlusD3")
 operator fun Batch<IOType.D3>.plus(other: IOType.D3) = map { it + other }
