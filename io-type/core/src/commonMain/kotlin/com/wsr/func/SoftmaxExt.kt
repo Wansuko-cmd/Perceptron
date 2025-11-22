5ac5c4668b438c0f0f7d3aa3bb6946bab5c867ec
Wansuko-cmd <63241922+Wansuko-cmd@users.noreply.github.com>
2025-11-23 00:00:16 +0900

    [fix] func系をvalueを使わないように変更

--- io-type/core/src/commonMain/kotlin/com/wsr/func/SoftmaxExt.kt
+++ io-type/core/src/commonMain/kotlin/com/wsr/func/SoftmaxExt.kt
@@ -5,20 +5,19 @@ import com.wsr.collection.max
 import com.wsr.collection.sum
 import com.wsr.operator.div
 import com.wsr.operator.minus
-import kotlin.math.exp
 
 fun IOType.D1.softmax(): IOType.D1 {
-    val max = value.max()
-    val exp = value.map { exp(it - max) }
+    val max = max()
+    val exp = (this - max).exp()
     val sum = exp.sum()
-    return IOType.d1(shape) { exp[it] / sum }
+    return exp / sum
 }
 
 fun IOType.D2.softmax(): IOType.D2 {
-    val max = value.max()
-    val exp = value.map { exp(it - max) }
+    val max = max()
+    val exp = (this - max).exp()
     val sum = exp.sum()
-    return IOType.d2(shape) { x, y -> exp[x * shape[0] + y] / sum }
+    return exp / sum
 }
 
 fun IOType.D2.softmax(axis: Int): IOType.D2 {
@@ -29,8 +28,8 @@ fun IOType.D2.softmax(axis: Int): IOType.D2 {
 }
 
 fun IOType.D3.softmax(): IOType.D3 {
-    val max = value.max()
-    val exp = value.map { exp(it - max) }
+    val max = max()
+    val exp = (this - max).exp()
     val sum = exp.sum()
-    return IOType.d3(shape) { x, y, z -> exp[x * shape[0] * shape[1] + y * shape[1] + z] / sum }
+    return exp / sum
 }
