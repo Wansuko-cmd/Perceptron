ccbf53fdd0a7422c1bb1d0ebcbab007e5f01b603
Wansuko-cmd <63241922+Wansuko-cmd@users.noreply.github.com>
2025-11-22 10:19:11 +0900

    [fix] LeakyReLUD1を修正

--- io-type/batch/src/commonMain/kotlin/com/wsr/batch/collection/D3Ext.kt
+++ io-type/batch/src/commonMain/kotlin/com/wsr/batch/collection/D3Ext.kt
@@ -9,33 +9,10 @@ inline fun Batch<IOType.D3>.forEach(block: (IOType.D3) -> Unit) {
     for (i in 0 until size) block(this[i])
 }
 
-inline fun Batch<IOType.D3>.map(block: (IOType.D3) -> IOType.D3): Batch<IOType.D3> {
-    val first = block(this[0])
-    val value = FloatArray(first.value.size * size)
-    first.value.copyInto(value, 0)
-    for (i in 1 until size) {
-        block(this[i]).value.copyInto(value, i * first.value.size)
-    }
-    return Batch(
-        value = value,
-        shape = first.shape,
-        size = size,
-    )
-}
+inline fun Batch<IOType.D3>.map(block: (IOType.D3) -> IOType.D3): Batch<IOType.D3> =
+    Batch(size) { block(this[it]) }
 
 inline fun Batch<IOType.D3>.mapWith(
     other: Batch<IOType.D3>,
     block: (IOType.D3, IOType.D3) -> IOType.D3,
-): Batch<IOType.D3> {
-    val first = block(this[0], other[0])
-    val value = FloatArray(first.value.size * size)
-    first.value.copyInto(value, 0)
-    for (i in 1 until size) {
-        block(this[i], other[i]).value.copyInto(value, i * first.value.size)
-    }
-    return Batch(
-        value = value,
-        shape = first.shape,
-        size = size,
-    )
-}
+): Batch<IOType.D3> = Batch(size) { block(this[it], other[it]) }
--- lib/src/commonMain/kotlin/com/wsr/layer/process/function/relu/LeakyReLUD1.kt
+++ lib/src/commonMain/kotlin/com/wsr/layer/process/function/relu/LeakyReLUD1.kt
@@ -3,38 +3,30 @@ package com.wsr.layer.process.function.relu
 import com.wsr.Batch
 import com.wsr.IOType
 import com.wsr.NetworkBuilder
+import com.wsr.batch.collection.map
+import com.wsr.get
 import com.wsr.layer.Context
 import com.wsr.layer.process.Process
-import com.wsr.toBatch
-import com.wsr.toList
 import kotlinx.serialization.Serializable
 
 @Serializable
 class LeakyReLUD1 internal constructor(override val outputSize: Int) : Process.D1() {
-    override fun expect(input: Batch<IOType.D1>, context: Context): Batch<IOType.D1> =
-        input.toList().map(::forward).toBatch()
+    override fun expect(input: Batch<IOType.D1>, context: Context): Batch<IOType.D1> = input.map(::forward)
 
     override fun train(
         input: Batch<IOType.D1>,
         context: Context,
         calcDelta: (Batch<IOType.D1>) -> Batch<IOType.D1>,
     ): Batch<IOType.D1> {
-        val input = input.toList()
         val output = input.map(::forward)
-        val delta = calcDelta(output.toBatch()).toList()
-        return List(input.size) { i ->
+        val delta = calcDelta(output)
+        return Batch(input.size) { i ->
             IOType.d1(outputSize) { if (input[i][it] >= 0f) delta[i][it] else 0.01f * delta[i][it] }
-        }.toBatch()
+        }
     }
 
     private fun forward(input: IOType.D1): IOType.D1 = IOType.d1(outputSize) {
-        if (input[it] >=
-            0f
-        ) {
-            input[it]
-        } else {
-            0.01f
-        }
+        if (input[it] >= 0f) input[it] else 0.01f
     }
 }
 
