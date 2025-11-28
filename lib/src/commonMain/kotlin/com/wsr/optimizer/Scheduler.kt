7b0bf63bc010a294619aa60f6aa26d17dce5afd4
Wansuko-cmd <63241922+Wansuko-cmd@users.noreply.github.com>
2025-11-29 00:57:31 +0900

    [add] Fixを追加

--- lib/src/commonMain/kotlin/com/wsr/optimizer/Scheduler.kt
+++ lib/src/commonMain/kotlin/com/wsr/optimizer/Scheduler.kt
@@ -1,5 +1,12 @@
 package com.wsr.optimizer
 
+import kotlinx.serialization.Serializable
+
 sealed interface Scheduler {
     fun calcRate(): Float
+
+    @Serializable
+    data class Fix(val rate: Float) : Scheduler {
+        override fun calcRate(): Float = rate
+    }
 }
