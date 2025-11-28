412aea75156619cfcb6faca03f5b8045219c4cdf
Wansuko-cmd <63241922+Wansuko-cmd@users.noreply.github.com>
2025-11-29 08:30:54 +0900

    [add] Stepを追加

--- lib/src/commonMain/kotlin/com/wsr/optimizer/Scheduler.kt
+++ lib/src/commonMain/kotlin/com/wsr/optimizer/Scheduler.kt
@@ -1,6 +1,7 @@
 package com.wsr.optimizer
 
 import kotlinx.serialization.Serializable
+import kotlin.math.pow
 
 sealed interface Scheduler {
     fun calcRate(step: Int): Float
@@ -8,5 +9,10 @@ sealed interface Scheduler {
     @Serializable
     data class Fix(val rate: Float) : Scheduler {
         override fun calcRate(step: Int): Float = rate
+    }
+
+    @Serializable
+    data class Step(val rate: Float, val stepSize: Int, val gamma: Float) : Scheduler {
+        override fun calcRate(step: Int): Float = rate * gamma.pow(step / stepSize)
     }
 }
