87903edf9e49b84cc6133bbdbfc624a6c5c31178
Wansuko-cmd <63241922+Wansuko-cmd@users.noreply.github.com>
2025-11-30 00:56:55 +0900

    [add] train関数の実装を追加

--- lib/src/commonMain/kotlin/com/wsr/layer/process/conv/ConvD2.kt
+++ lib/src/commonMain/kotlin/com/wsr/layer/process/conv/ConvD2.kt
@@ -2,12 +2,17 @@ package com.wsr.layer.process.conv
 
 import com.wsr.NetworkBuilder
 import com.wsr.batch.Batch
+import com.wsr.batch.get
 import com.wsr.batch.reshape.convert.toBatch
+import com.wsr.batch.reshape.convert.toD4
+import com.wsr.batch.reshape.fold.fold
 import com.wsr.batch.reshape.fold.unfold
 import com.wsr.core.IOType
+import com.wsr.core.d2
+import com.wsr.core.d4
+import com.wsr.core.get
 import com.wsr.core.operation.matmul.matMul
 import com.wsr.core.reshape.reshape.reshapeToD2
-import com.wsr.core.reshape.reshape.reshapeToD3
 import com.wsr.core.reshape.reshape.reshapeToD4
 import com.wsr.core.reshape.transpose.transpose
 import com.wsr.initializer.WeightInitializer
@@ -25,7 +30,7 @@ class ConvD2 internal constructor(
     private val padding: Int,
     private val inputX: Int,
     private val inputY: Int,
-    private val optimizer: Optimizer.D3,
+    private val optimizer: Optimizer.D4,
     private var weight: IOType.D4,
 ) : Process.D3() {
     override val outputX: Int = filter
@@ -47,7 +52,53 @@ class ConvD2 internal constructor(
         context: Context,
         calcDelta: (Batch<IOType.D3>) -> Batch<IOType.D3>,
     ): Batch<IOType.D3> {
-        TODO("Not yet implemented")
+        val col = input.unfold(windowSize = kernel, stride = stride, padding = padding)
+        val output = (weight.reshapeToD2(i = outputX, j = channel * kernel * kernel) matMul col)
+            .reshapeToD4(i = filter, j = input.size, k = outputY, l = outputZ)
+            .transpose(axisI = 1, axisJ = 0, axisK = 2, axisL = 3)
+            .toBatch()
+
+        val delta = calcDelta(output)
+
+        val reversed = IOType.d2(i = channel * kernel * kernel, j = filter) { i, f ->
+            val c = i / (kernel * kernel)
+            val ky = (i % (kernel * kernel)) / kernel
+            val kx = i % kernel
+            weight[f, c, kernel - ky - 1, kernel - kx - 1]
+        }
+        val deltaCol = delta.toD4()
+            .transpose(axisI = 1, axisJ = 0, axisK = 2, axisL = 3)
+            .reshapeToD2(i = filter, j = input.size * outputY * outputZ)
+        val dx = (reversed matMul deltaCol).fold(
+            batchSize = input.size,
+            channel = channel,
+            inputSizeX = inputX,
+            inputSizeY = inputY,
+            stride = stride,
+            padding = padding,
+        )
+
+        // dw (重み勾配)
+        val dw = Batch(input.size) { b ->
+            val delta = delta[b]
+            val input = input[b]
+            IOType.d4(filter, channel, kernel, kernel) { f, c, ky, kx ->
+                var sum = 0f
+                for (oy in 0 until outputY) {
+                    for (ox in 0 until outputZ) {
+                        val inputIdxX = ox * stride + kx - padding
+                        val inputIdxY = oy * stride + ky - padding
+                        if (inputIdxX in 0 until inputX && inputIdxY in 0 until inputY) {
+                            sum += delta[f, oy, ox] * input[c, inputIdxX, inputIdxY]
+                        }
+                    }
+                }
+                sum
+            }
+        }
+        weight = optimizer.adapt(weight = weight, dw = dw)
+
+        return dx
     }
 }
 
@@ -68,7 +119,7 @@ fun <T> NetworkBuilder.D3<T>.convD2(
             padding = padding,
             inputX = inputY,
             inputY = inputZ,
-            optimizer = optimizer.d3(filter, inputX, kernel),
+            optimizer = optimizer.d4(filter, inputX, kernel, kernel),
             weight = initializer.d4(
                 input = listOf(inputX, kernel, kernel),
                 output = listOf(filter, kernel, kernel),
