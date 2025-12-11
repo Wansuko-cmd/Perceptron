2629ea0b69b32248ddadbbb896e52ed8d317fe89
Wansuko-cmd <63241922+Wansuko-cmd@users.noreply.github.com>
2025-12-11 21:40:55 +0900

    [fix] v2版を使うように書き換え

--- io-type/core/src/commonMain/kotlin/com/wsr/core/operation/inner/D1Ext.kt
+++ io-type/core/src/commonMain/kotlin/com/wsr/core/operation/inner/D1Ext.kt
@@ -3,10 +3,7 @@ package com.wsr.core.operation.inner
 import com.wsr.BLAS
 import com.wsr.core.IOType
 
-infix fun IOType.D1.inner(other: IOType.D1): Float = BLAS.sdot(
-    n = value.size,
+infix fun IOType.D1.inner(other: IOType.D1): Float = BLAS.sdot2(
     x = value,
-    incX = 1,
     y = other.value,
-    incY = 1,
 )
