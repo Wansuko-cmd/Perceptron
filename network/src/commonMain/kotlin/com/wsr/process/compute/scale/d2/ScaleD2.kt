635b37b81b4b3c57bf550663d58e3a616a6bb2ba
Wansuko-cmd <63241922+Wansuko-cmd@users.noreply.github.com>
2025-12-19 17:08:24 +0900

    [add] ScaleAxisD2を追加

--- network/src/commonMain/kotlin/com/wsr/process/compute/scale/d2/ScaleD2.kt
+++ network/src/commonMain/kotlin/com/wsr/process/compute/scale/d2/ScaleD2.kt
@@ -41,21 +41,48 @@ class ScaleD2 internal constructor(
 }
 
 fun <T> NetworkBuilder.D2<T>.scale(
+    axis: Int? = null,
     optimizer: Optimizer = this.optimizer,
     initializer: WeightInitializer = Fixed(1f),
-): NetworkBuilder.D2<T> = addProcess(
-    process = ScaleD2(
-        outputX = inputX,
-        outputY = inputY,
-        optimizer = optimizer.d2(
-            inputX,
-            inputY,
-        ),
-        weight = initializer.d2(
-            input = listOf(inputX, inputY),
-            output = listOf(inputX, inputY),
-            x = inputX,
-            y = inputY,
-        ),
-    ),
-)
+): NetworkBuilder.D2<T> {
+    val process = when (axis) {
+        null -> ScaleD2(
+            outputX = inputX,
+            outputY = inputY,
+            optimizer = optimizer.d2(
+                inputX,
+                inputY,
+            ),
+            weight = initializer.d2(
+                input = listOf(inputX, inputY),
+                output = listOf(inputX, inputY),
+                x = inputX,
+                y = inputY,
+            ),
+        )
+
+        0, 1 -> {
+            val inputT = if (axis == 0) inputX else inputY
+            ScaleAxisD2(
+                outputX = inputX,
+                outputY = inputY,
+                axis = axis,
+                optimizer = optimizer.d1(inputT),
+                weight = initializer.d1(
+                    input = listOf(inputT),
+                    output = listOf(inputT),
+                    size = inputT,
+                ),
+            )
+        }
+
+
+        else -> throw IllegalStateException(
+            """
+            invalid parameter.
+            axis: $axis
+            """.trimIndent(),
+        )
+    }
+    return addProcess(process = process)
+}
