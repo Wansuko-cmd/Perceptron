b987579620815a3a2be6c4ebda94c7ffee40c389
Wansuko-cmd <63241922+Wansuko-cmd@users.noreply.github.com>
2025-12-20 13:55:27 +0900

    [add] BiasAxisD3を追加

--- io-type/src/commonMain/kotlin/com/wsr/core/operation/plus/D3Ext.kt
+++ io-type/src/commonMain/kotlin/com/wsr/core/operation/plus/D3Ext.kt
@@ -9,6 +9,8 @@ operator fun IOType.D3.plus(other: Float): IOType.D3 = IOType.d3(shape) { i, j, 
 
 operator fun IOType.D3.plus(other: IOType.D0): IOType.D3 = IOType.d3(shape) { i, j, k -> this[i, j, k] + other.get() }
 
+fun IOType.D3.plus(other: IOType.D1, axis: Int): IOType.D3 = zipWith(other, axis) { a, b -> a + b }
+
 fun IOType.D3.plus(other: IOType.D2, axis1: Int, axis2: Int) = zipWith(other, axis1, axis2) { a, b -> a + b }
 
 operator fun IOType.D3.plus(other: IOType.D3): IOType.D3 = IOType.d3(shape) { i, j, k ->
