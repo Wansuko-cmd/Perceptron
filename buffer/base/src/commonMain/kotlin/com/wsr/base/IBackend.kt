11477496c65f282f61e273a6d780cff1bea233c3
Wansuko-cmd <63241922+Wansuko-cmd@users.noreply.github.com>
2025-12-23 00:16:51 +0900

    [add] 四則演算を追加

--- buffer/base/src/commonMain/kotlin/com/wsr/base/IBackend.kt
+++ buffer/base/src/commonMain/kotlin/com/wsr/base/IBackend.kt
@@ -1,6 +1,46 @@
 package com.wsr.base
 
 interface IBackend {
+    fun plus(x: Float, y: DataBuffer): DataBuffer
+    fun plus(x: DataBuffer, y: Float): DataBuffer
+    fun plus(x: DataBuffer, y: DataBuffer): DataBuffer
+    fun plus(x: DataBuffer, y: DataBuffer, yi: Int, yj: Int, axis: Int): DataBuffer
+    fun plus(x: DataBuffer, y: DataBuffer, yi: Int, yj: Int, yk: Int, axis: Int): DataBuffer
+    fun plus(x: DataBuffer, xi: Int, xj: Int, y: DataBuffer, axis: Int): DataBuffer
+    fun plus(x: DataBuffer, xi: Int, xj: Int, y: DataBuffer, yi: Int, yj: Int, yk: Int, axis1: Int, axis2: Int): DataBuffer
+    fun plus(x: DataBuffer, xi: Int, xj: Int, xk: Int, y: DataBuffer, axis: Int): DataBuffer
+    fun plus(x: DataBuffer, xi: Int, xj: Int, xk: Int, y: DataBuffer, yi: Int, yj: Int, axis1: Int, axis2: Int): DataBuffer
+
+    fun minus(x: Float, y: DataBuffer): DataBuffer
+    fun minus(x: DataBuffer, y: Float): DataBuffer
+    fun minus(x: DataBuffer, y: DataBuffer): DataBuffer
+    fun minus(x: DataBuffer, y: DataBuffer, yi: Int, yj: Int, axis: Int): DataBuffer
+    fun minus(x: DataBuffer, y: DataBuffer, yi: Int, yj: Int, yk: Int, axis: Int): DataBuffer
+    fun minus(x: DataBuffer, xi: Int, xj: Int, y: DataBuffer, axis: Int): DataBuffer
+    fun minus(x: DataBuffer, xi: Int, xj: Int, y: DataBuffer, yi: Int, yj: Int, yk: Int, axis1: Int, axis2: Int): DataBuffer
+    fun minus(x: DataBuffer, xi: Int, xj: Int, xk: Int, y: DataBuffer, axis: Int): DataBuffer
+    fun minus(x: DataBuffer, xi: Int, xj: Int, xk: Int, y: DataBuffer, yi: Int, yj: Int, axis1: Int, axis2: Int): DataBuffer
+
+    fun times(x: Float, y: DataBuffer): DataBuffer
+    fun times(x: DataBuffer, y: Float): DataBuffer
+    fun times(x: DataBuffer, y: DataBuffer): DataBuffer
+    fun times(x: DataBuffer, y: DataBuffer, yi: Int, yj: Int, axis: Int): DataBuffer
+    fun times(x: DataBuffer, y: DataBuffer, yi: Int, yj: Int, yk: Int, axis: Int): DataBuffer
+    fun times(x: DataBuffer, xi: Int, xj: Int, y: DataBuffer, axis: Int): DataBuffer
+    fun times(x: DataBuffer, xi: Int, xj: Int, y: DataBuffer, yi: Int, yj: Int, yk: Int, axis1: Int, axis2: Int): DataBuffer
+    fun times(x: DataBuffer, xi: Int, xj: Int, xk: Int, y: DataBuffer, axis: Int): DataBuffer
+    fun times(x: DataBuffer, xi: Int, xj: Int, xk: Int, y: DataBuffer, yi: Int, yj: Int, axis1: Int, axis2: Int): DataBuffer
+
+    fun div(x: Float, y: DataBuffer): DataBuffer
+    fun div(x: DataBuffer, y: Float): DataBuffer
+    fun div(x: DataBuffer, y: DataBuffer): DataBuffer
+    fun div(x: DataBuffer, y: DataBuffer, yi: Int, yj: Int, axis: Int): DataBuffer
+    fun div(x: DataBuffer, y: DataBuffer, yi: Int, yj: Int, yk: Int, axis: Int): DataBuffer
+    fun div(x: DataBuffer, xi: Int, xj: Int, y: DataBuffer, axis: Int): DataBuffer
+    fun div(x: DataBuffer, xi: Int, xj: Int, y: DataBuffer, yi: Int, yj: Int, yk: Int, axis1: Int, axis2: Int): DataBuffer
+    fun div(x: DataBuffer, xi: Int, xj: Int, xk: Int, y: DataBuffer, axis: Int): DataBuffer
+    fun div(x: DataBuffer, xi: Int, xj: Int, xk: Int, y: DataBuffer, yi: Int, yj: Int, axis1: Int, axis2: Int): DataBuffer
+
     fun exp(x: DataBuffer): DataBuffer
     fun ln(x: DataBuffer, e: Float): DataBuffer
     fun pow(x: DataBuffer, n: Int): DataBuffer
