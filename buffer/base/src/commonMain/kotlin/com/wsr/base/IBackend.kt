d5e253137b391585308b9f34b6c1c6343de99a1d
Wansuko-cmd <63241922+Wansuko-cmd@users.noreply.github.com>
2025-12-23 00:07:52 +0900

    [add] IBackendにてmath関数群を追加

--- buffer/base/src/commonMain/kotlin/com/wsr/base/IBackend.kt
+++ buffer/base/src/commonMain/kotlin/com/wsr/base/IBackend.kt
@@ -1,4 +1,8 @@
 package com.wsr.base
 
 interface IBackend {
+    fun exp(x: DataBuffer): DataBuffer
+    fun ln(x: DataBuffer, e: Float): DataBuffer
+    fun pow(x: DataBuffer, n: Int): DataBuffer
+    fun sqrt(x: DataBuffer, e: Float): DataBuffer
 }
