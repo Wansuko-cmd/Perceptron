f707d6cc6b4156c4ebd1363e46a0515ce1163e9a
Wansuko-cmd <63241922+Wansuko-cmd@users.noreply.github.com>
2025-12-26 22:42:52 +0900

    [fix] Batchのmath系をBackendに変更

--- io-type/src/commonMain/kotlin/com/wsr/batch/math/ExpExt.kt
+++ io-type/src/commonMain/kotlin/com/wsr/batch/math/ExpExt.kt
@@ -1,15 +1,25 @@
 package com.wsr.batch.math
 
+import com.wsr.Backend
 import com.wsr.batch.Batch
 import com.wsr.batch.collecction.map.mapValue
 import com.wsr.core.IOType
 import kotlin.math.exp
 
 @JvmName("batchD1sExp")
-fun Batch<IOType.D1>.exp(): Batch<IOType.D1> = mapValue { exp(it) }
+fun Batch<IOType.D1>.exp(): Batch<IOType.D1> {
+    val result = Backend.exp(x = value)
+    return Batch(size = size, shape = shape, value = result)
+}
 
 @JvmName("batchD2sExp")
-fun Batch<IOType.D2>.exp(): Batch<IOType.D2> = mapValue { exp(it) }
+fun Batch<IOType.D2>.exp(): Batch<IOType.D2> {
+    val result = Backend.exp(x = value)
+    return Batch(size = size, shape = shape, value = result)
+}
 
 @JvmName("batchD3sExp")
-fun Batch<IOType.D3>.exp(): Batch<IOType.D3> = mapValue { exp(it) }
+fun Batch<IOType.D3>.exp(): Batch<IOType.D3> {
+    val result = Backend.exp(x = value)
+    return Batch(size = size, shape = shape, value = result)
+}
--- io-type/src/commonMain/kotlin/com/wsr/batch/math/LnExt.kt
+++ io-type/src/commonMain/kotlin/com/wsr/batch/math/LnExt.kt
@@ -1,17 +1,30 @@
 package com.wsr.batch.math
 
+import com.wsr.Backend
 import com.wsr.batch.Batch
 import com.wsr.batch.collecction.map.mapValue
 import com.wsr.core.IOType
 
 @JvmName("batchD0sLn")
-fun Batch<IOType.D0>.ln(e: Float = 1e-7f): Batch<IOType.D0> = mapValue { kotlin.math.ln(it + e) }
+fun Batch<IOType.D0>.ln(e: Float = 1e-7f): Batch<IOType.D0> {
+    val result = Backend.ln(x = value, e = e)
+    return Batch(size = size, shape = shape, value = result)
+}
 
 @JvmName("batchD1sLn")
-fun Batch<IOType.D1>.ln(e: Float = 1e-7f): Batch<IOType.D1> = mapValue { kotlin.math.ln(it + e) }
+fun Batch<IOType.D1>.ln(e: Float = 1e-7f): Batch<IOType.D1> {
+    val result = Backend.ln(x = value, e = e)
+    return Batch(size = size, shape = shape, value = result)
+}
 
 @JvmName("batchD2sLn")
-fun Batch<IOType.D2>.ln(e: Float = 1e-7f): Batch<IOType.D2> = mapValue { kotlin.math.ln(it + e) }
+fun Batch<IOType.D2>.ln(e: Float = 1e-7f): Batch<IOType.D2> {
+    val result = Backend.ln(x = value, e = e)
+    return Batch(size = size, shape = shape, value = result)
+}
 
 @JvmName("batchD3sLn")
-fun Batch<IOType.D3>.ln(e: Float = 1e-7f): Batch<IOType.D3> = mapValue { kotlin.math.ln(it + e) }
+fun Batch<IOType.D3>.ln(e: Float = 1e-7f): Batch<IOType.D3> {
+    val result = Backend.ln(x = value, e = e)
+    return Batch(size = size, shape = shape, value = result)
+}
