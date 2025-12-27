e23ae2aea4112fadd8e16d7dfd9563b7d223f372
Wansuko-cmd <63241922+Wansuko-cmd@users.noreply.github.com>
2025-12-27 19:45:27 +0900

    [fix] 一部コードを修正

--- io-type/src/commonMain/kotlin/com/wsr/batch/operation/matmul/D2Ext.kt
+++ io-type/src/commonMain/kotlin/com/wsr/batch/operation/matmul/D2Ext.kt
@@ -5,20 +5,20 @@ import com.wsr.batch.Batch
 import com.wsr.core.IOType
 
 fun IOType.D2.matMul(other: Batch<IOType.D1>, trans: Boolean = false): Batch<IOType.D1> {
-    val m = if (trans) shape[1] else shape[0]
+    val n = if (trans) shape[1] else shape[0]
     val k = if (trans) shape[0] else shape[1]
     val result = Backend.matMul(
-        x = value,
-        transX = trans,
-        y = other.value,
-        transY = false,
-        m = m,
-        n = 1,
+        x = other.value,
+        transX = false,
+        y = value,
+        transY = !trans,
+        m = other.size,
+        n = n,
         k = k,
-        b = other.size,
+        b = 1,
     )
 
-    return Batch(result, other.size, listOf(m))
+    return Batch(result, other.size, listOf(n))
 }
 
 @JvmName("matMulToD2s")
