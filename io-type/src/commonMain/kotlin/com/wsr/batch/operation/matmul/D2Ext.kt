78bdbf0a7e38457bcdd9ee6e7be80f859e409959
Wansuko-cmd <63241922+Wansuko-cmd@users.noreply.github.com>
2025-12-27 17:16:12 +0900

    [fix] IOTypeでの呼び出しをBackendに変更

--- io-type/src/commonMain/kotlin/com/wsr/batch/operation/matmul/D2Ext.kt
+++ io-type/src/commonMain/kotlin/com/wsr/batch/operation/matmul/D2Ext.kt
@@ -1,31 +1,40 @@
 package com.wsr.batch.operation.matmul
 
-import com.wsr.BLAS
-import com.wsr.base.DataBuffer
+import com.wsr.Backend
 import com.wsr.batch.Batch
-import com.wsr.batch.collecction.map.map
 import com.wsr.core.IOType
-import com.wsr.core.operation.matmul.matMul
 
-fun IOType.D2.matMul(other: Batch<IOType.D1>, trans: Boolean = false) = other.map { this.matMul(it, trans) }
+fun IOType.D2.matMul(other: Batch<IOType.D1>, trans: Boolean = false): Batch<IOType.D1> {
+    val m = if (trans) shape[1] else shape[0]
+    val k = if (trans) shape[0] else shape[1]
+    val result = Backend.matMul(
+        x = value,
+        transX = trans,
+        y = other.value,
+        transY = false,
+        m = m,
+        n = 1,
+        k = k,
+        b = other.size
+    )
+
+    return Batch(result, other.size, listOf(m))
+}
 
 @JvmName("matMulToD2s")
 fun Batch<IOType.D2>.matMul(other: IOType.D2, transA: Boolean = false, transB: Boolean = false): Batch<IOType.D2> {
     val m = if (transA) shape[1] else shape[0]
     val n = if (transB) other.shape[0] else other.shape[1]
     val k = if (transA) shape[0] else shape[1]
-    val result = BLAS.sgemm(
+    val result = Backend.matMul(
+        x = value,
+        transX = transA,
+        y = other.value,
+        transY = transB,
         m = size * m,
         n = n,
         k = k,
-        alpha = 1f,
-        a = value,
-        transA = transA,
-        b = other.value,
-        transB = transB,
-        beta = 0f,
-        c = DataBuffer.create(size * m * n),
-        batchSize = 1,
+        b = 1,
     )
     return Batch(value = result, size = size, shape = listOf(m, n))
 }
@@ -39,18 +48,15 @@ fun Batch<IOType.D2>.matMul(
     val m = if (transA) shape[1] else shape[0]
     val n = if (transB) other.shape[0] else other.shape[1]
     val k = if (transA) shape[0] else shape[1]
-    val result = BLAS.sgemm(
+    val result = Backend.matMul(
+        x = value,
+        transX = transA,
+        y = other.value,
+        transY = transB,
         m = m,
         n = n,
         k = k,
-        alpha = 1f,
-        a = value,
-        transA = transA,
-        b = other.value,
-        transB = transB,
-        beta = 0f,
-        c = DataBuffer.create(size * m * n),
-        batchSize = size,
+        b = size,
     )
     return Batch(value = result, size = size, shape = listOf(m, n))
 }
