1e62fe137a1295ef11c004cd211276108f039a71
Wansuko-cmd <63241922+Wansuko-cmd@users.noreply.github.com>
2025-12-28 13:55:04 +0900

    [format] ktlintを実行

--- sample/src/test/kotlin/mnist/MnistTest.kt
+++ sample/src/test/kotlin/mnist/MnistTest.kt
@@ -2,25 +2,13 @@
 
 package mnist
 
-import com.wsr.BLAS
 import dataset.mnist.createMnistModel
 import kotlin.test.Test
-import org.junit.Before
 
 private const val EPOC = 1
 private const val SEED = 0
 
 class MnistTest {
-    @Before
-    fun setup() {
-        println(
-            """
-                設定
-                BLAS is Native: ${BLAS.isNative}
-            """.trimIndent(),
-        )
-    }
-
     @Test
     fun `Mnistモデルの精度が落ちていないか確認`() {
         val actual = createMnistModel(epoc = EPOC, seed = SEED)
