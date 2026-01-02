07798c27d91851afb7bf1f0ddbd9140f677bfb6d
Wansuko-cmd <63241922+Wansuko-cmd@users.noreply.github.com>
2026-01-02 23:22:31 +0900

    [add] CPUBackendに対応を追加

--- buffer/cpu/src/jvmMain/java/com/wsr/cpu/JCollection.java
+++ buffer/cpu/src/jvmMain/java/com/wsr/cpu/JCollection.java
@@ -3,6 +3,11 @@ package com.wsr.cpu;
 import java.nio.ByteBuffer;
 
 class JCollection {
+    public native float averageD1(ByteBuffer x);
+    public native void averageD2(ByteBuffer x, int xi, int xj, int axis, ByteBuffer result);
+    public native void averageD3(ByteBuffer x, int xi, int xj, int xk, int axis, ByteBuffer result);
+    public native void averageD4(ByteBuffer x, int xi, int xj, int xk, int xl, int axis, ByteBuffer result);
+
     public native float maxD1(ByteBuffer x);
     public native void maxD2(ByteBuffer x, int xi, int xj, int axis, ByteBuffer result);
     public native void maxD3(ByteBuffer x, int xi, int xj, int xk, int axis, ByteBuffer result);
