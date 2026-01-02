5cf5ed0b572d0c56cabb8be802df00e40c242618
Wansuko-cmd <63241922+Wansuko-cmd@users.noreply.github.com>
2026-01-02 18:14:07 +0900

    [add] collection_funを追加

---
+++ buffer/cpu/cpp/src/cpu/collection_fun.cpp
@@ -0,0 +1,186 @@
+#include <collection_fun.h>
+
+#define PERFORM_OPERATION(acc, x) \
+    if constexpr (Op == Operation::Max) { \
+        if (acc < x) { \
+            acc = x; \
+        } \
+    } else if constexpr (Op == Operation::Min) { \
+        if (acc > x) { \
+            acc = x; \
+        } \
+    } else if constexpr (Op == Operation::Sum) { \
+        acc += x; \
+    }
+
+enum class Operation {
+    Max,
+    Min,
+    Sum
+};
+
+template<Operation Op>
+inline float reduce_d1(const float* x, size_t size) {
+    float acc = x[0];
+    for (size_t i = 1; i < size; i++) {
+        PERFORM_OPERATION(acc, x[i])
+    }
+    return acc;
+}
+
+template<Operation Op>
+inline void reduce_d2(const float* x, int xi, int xj, int axis, float* result) {
+    if (axis == 0) {
+        for (int j = 0; j < xj; j++) {
+            float acc = x[j];
+            for (size_t i = 1; i < xi; i++) {
+                PERFORM_OPERATION(acc, x[i * xj + j])
+            }
+            result[j] = acc;
+        }
+    } else if (axis == 1) {
+        for (int i = 0; i < xi; i++) {
+            float acc = x[i * xj];
+            for (int j = 1; j < xj; j++) {
+                PERFORM_OPERATION(acc, x[i * xj + j])
+            }
+            result[i] = acc;
+        }
+    }
+}
+
+template<Operation Op>
+inline void reduce_d3(const float* x, int xi, int xj, int xk, int axis, float* result) {
+    if (axis == 0) {
+        for (int j = 0; j < xj; j++) {
+            for (int k = 0; k < xk; k++) {
+                float acc = x[j * xk + k];
+                for (int i = 1; i < xi; i++) {
+                    PERFORM_OPERATION(acc, x[(i * xj + j) * xk + k])
+                }
+                result[j * xk + k] = acc;
+            }
+        }
+    } else if (axis == 1) {
+        for (int i = 0; i < xi; i++) {
+            for (int k = 0; k < xk; k++) {
+                float acc = x[i * xj * xk + k];
+                for (int j = 1; j < xj; j++) {
+                    PERFORM_OPERATION(acc, x[(i * xj + j) * xk + k])
+                }
+                result[i * xk + k] = acc;
+            }
+        }
+    } else if (axis == 2) {
+        for (int i = 0; i < xi; i++) {
+            for (int j = 0; j < xj; j++) {
+                float acc = x[(i * xj + j) * xk];
+                for (int k = 1; k < xk; k++) {
+                    PERFORM_OPERATION(acc, x[(i * xj + j) * xk + k])
+                }
+                result[i * xj + j] = acc;
+            }
+        }
+    }
+}
+
+template<Operation Op>
+inline void reduce_d4(const float* x, int xi, int xj, int xk, int xl, int axis, float* result) {
+    if (axis == 0) {
+        for (int j = 0; j < xj; j++) {
+            for (int k = 0; k < xk; k++) {
+                for (int l = 0; l < xl; l++) {
+                    float acc = x[(j * xk + k) * xl + l];
+                    for (int i = 1; i < xi; i++) {
+                        PERFORM_OPERATION(acc, x[((i * xj + j) * xk + k) * xl + l])
+                    }
+                    result[(j * xk + k) * xl + l] = acc;
+                }
+            }
+        }
+    } else if (axis == 1) {
+        for (int i = 0; i < xi; i++) {
+            for (int k = 0; k < xk; k++) {
+                for (int l = 0; l < xl; l++) {
+                    float acc = x[(i * xj * xk + k) * xl + l];
+                    for (int j = 1; j < xj; j++) {
+                        PERFORM_OPERATION(acc, x[((i * xj + j) * xk + k) * xl + l])
+                    }
+                    result[(i * xk + k) * xl + l] = acc;
+                }
+            }
+        }
+    } else if (axis == 2) {
+        for (int i = 0; i < xi; i++) {
+            for (int j = 0; j < xj; j++) {
+                for (int l = 0; l < xl; l++) {
+                    float acc = x[(i * xj + j) * xk * xl + l];
+                    for (int k = 1; k < xk; k++) {
+                        PERFORM_OPERATION(acc, x[((i * xj + j) * xk + k) * xl + l])
+                    }
+                    result[(i * xj + j) * xl + l] = acc;
+                }
+            }
+        }
+    } else if (axis == 3) {
+        for (int i = 0; i < xi; i++) {
+            for (int j = 0; j < xj; j++) {
+                for (int k = 0; k < xk; k++) {
+                    float acc = x[((i * xj + j) * xk + k) * xl];
+                    for (int l = 1; l < xl; l++) {
+                        PERFORM_OPERATION(acc, x[((i * xj + j) * xk + k) * xl + l])
+                    }
+                    result[(i * xj + j) * xk + k] = acc;
+                }
+            }
+        }
+    }
+}
+
+float max_d1(const float* x, size_t size) {
+    return reduce_d1<Operation::Max>(x, size);
+}
+
+void max_d2(const float* x, int xi, int xj, int axis, float* result) {
+    reduce_d2<Operation::Max>(x, xi, xj, axis, result);
+}
+
+void max_d3(const float* x, int xi, int xj, int xk, int axis, float* result) {
+    reduce_d3<Operation::Max>(x, xi, xj, xk, axis, result);
+}
+
+void max_d4(const float* x, int xi, int xj, int xk, int xl, int axis, float* result) {
+    reduce_d4<Operation::Max>(x, xi, xj, xk, xl, axis, result);
+}
+
+float min_d1(const float* x, size_t size) {
+    return reduce_d1<Operation::Min>(x, size);
+}
+
+void min_d2(const float* x, int xi, int xj, int axis, float* result) {
+    reduce_d2<Operation::Min>(x, xi, xj, axis, result);
+}
+
+void min_d3(const float* x, int xi, int xj, int xk, int axis, float* result) {
+    reduce_d3<Operation::Min>(x, xi, xj, xk, axis, result);
+}
+
+void min_d4(const float* x, int xi, int xj, int xk, int xl, int axis, float* result) {
+    reduce_d4<Operation::Min>(x, xi, xj, xk, xl, axis, result);
+}
+
+float sum_d1(const float* x, size_t size) {
+    return reduce_d1<Operation::Sum>(x, size);
+}
+
+void sum_d2(const float* x, int xi, int xj, int axis, float* result) {
+    reduce_d2<Operation::Sum>(x, xi, xj, axis, result);
+}
+
+void sum_d3(const float* x, int xi, int xj, int xk, int axis, float* result) {
+    reduce_d3<Operation::Sum>(x, xi, xj, xk, axis, result);
+}
+
+void sum_d4(const float* x, int xi, int xj, int xk, int xl, int axis, float* result) {
+    reduce_d4<Operation::Sum>(x, xi, xj, xk, xl, axis, result);
+}
--- buffer/cpu/cpp/src/cpu/jni/com_wsr_cpu_JCollection.cpp
+++ buffer/cpu/cpp/src/cpu/jni/com_wsr_cpu_JCollection.cpp
@@ -1,243 +1,99 @@
 #include "com_wsr_cpu_JCollection.h"
-#include <stdio.h>
-#include <cblas.h>
-
-#define PERFORM_OPERATION(acc, x) \
-    if constexpr (Op == Operation::Max) { \
-        if (acc < x) { \
-            acc = x; \
-        } \
-    } else if constexpr (Op == Operation::Min) { \
-        if (acc > x) { \
-            acc = x; \
-        } \
-    } else if constexpr (Op == Operation::Sum) { \
-        acc += x; \
-    }
-
-enum class Operation {
-    Max,
-    Min,
-    Sum
-};
-
-template<Operation Op>
-inline float reduceD1(
-    JNIEnv *env, jobject obj,
-    jobject x
-) {
-    jfloat *x_ptr = (jfloat*)env->GetDirectBufferAddress(x);
-    jlong size = env->GetDirectBufferCapacity(x) / sizeof(jfloat);
-
-    float acc = x_ptr[0];
-    for (int i = 1; i < size; i++) {
-        PERFORM_OPERATION(acc, x_ptr[i]);
-    }
-    return acc;
-}
-
-template<Operation Op>
-inline void reduceD2(
-    JNIEnv *env, jobject obj,
-    jobject x, jint xi, jint xj,
-    jint axis,
-    jobject result
-) {
-    jfloat *x_ptr = (jfloat*)env->GetDirectBufferAddress(x);
-    jfloat *result_ptr = (jfloat*)env->GetDirectBufferAddress(result);
-
-    if (axis == 0) {
-        for (int j = 0; j < xj; j++) {
-            float acc = x_ptr[j];
-            for (int i = 1; i < xi; i++) {
-                PERFORM_OPERATION(acc, x_ptr[i * xj + j]);
-            }
-            result_ptr[j] = acc;
-        }
-    } else if (axis == 1) {
-        for (int i = 0; i < xi; i++) {
-            float acc = x_ptr[i * xj];
-            for (int j = 1; j < xj; j++) {
-                PERFORM_OPERATION(acc, x_ptr[i * xj + j]);
-            }
-            result_ptr[i] = acc;
-        }
-    }
-}
-
-template<Operation Op>
-inline void reduceD3(
-    JNIEnv *env, jobject obj,
-    jobject x, jint xi, jint xj, jint xk,
-    jint axis,
-    jobject result
-) {
-    jfloat *x_ptr = (jfloat*)env->GetDirectBufferAddress(x);
-    jfloat *result_ptr = (jfloat*)env->GetDirectBufferAddress(result);
-
-    if (axis == 0) {
-        for (int j = 0; j < xj; j++) {
-            for (int k = 0; k < xk; k++) {
-                float acc = x_ptr[j * xk + k];
-                for (int i = 1; i < xi; i++) {
-                    PERFORM_OPERATION(acc, x_ptr[(i * xj + j) * xk + k]);
-                }
-                result_ptr[j * xk + k] = acc;
-            }
-        }
-    } else if (axis == 1) {
-        for (int i = 0; i < xi; i++) {
-            for (int k = 0; k < xk; k++) {
-                float acc = x_ptr[i * xj * xk + k];
-                for (int j = 1; j < xj; j++) {
-                    PERFORM_OPERATION(acc, x_ptr[(i * xj + j) * xk + k]);
-                }
-                result_ptr[i * xk + k] = acc;
-            }
-        }
-    } else if (axis == 2) {
-        for (int i = 0; i < xi; i++) {
-            for (int j = 0; j < xj; j++) {
-                float acc = x_ptr[(i * xj + j) * xk];
-                for (int k = 1; k < xk; k++) {
-                    PERFORM_OPERATION(acc, x_ptr[(i * xj + j) * xk + k]);
-                }
-                result_ptr[i * xj + j] = acc;
-            }
-        }
-    }
-}
-
-template<Operation Op>
-inline void reduceD4(
-    JNIEnv *env, jobject obj,
-    jobject x, jint xi, jint xj, jint xk, jint xl,
-    jint axis,
-    jobject result
-) {
-    jfloat *x_ptr = (jfloat*)env->GetDirectBufferAddress(x);
-    jfloat *result_ptr = (jfloat*)env->GetDirectBufferAddress(result);
-
-    if (axis == 0) {
-        for (int j = 0; j < xj; j++) {
-            for (int k = 0; k < xk; k++) {
-                for (int l = 0; l < xl; l++) {
-                    float acc = x_ptr[(j * xk + k) * xl + l];
-                    for (int i = 1; i < xi; i++) {
-                        PERFORM_OPERATION(acc, x_ptr[((i * xj + j) * xk + k) * xl + l]);
-                    }
-                    result_ptr[(j * xk + k) * xl + l] = acc;
-                }
-            }
-        }
-    } else if (axis == 1) {
-        for (int i = 0; i < xi; i++) {
-            for (int k = 0; k < xk; k++) {
-                for (int l = 0; l < xl; l++) {
-                    float acc = x_ptr[(i * xj * xk + k) * xl + l];
-                    for (int j = 1; j < xj; j++) {
-                        PERFORM_OPERATION(acc, x_ptr[((i * xj + j) * xk + k) * xl + l]);
-                    }
-                    result_ptr[(i * xk + k) * xl + l] = acc;
-                }
-            }
-        }
-    } else if (axis == 2) {
-        for (int i = 0; i < xi; i++) {
-            for (int j = 0; j < xj; j++) {
-                for (int l = 0; l < xl; l++) {
-                    float acc = x_ptr[(i * xj + j) * xk * xl + l];
-                    for (int k = 1; k < xk; k++) {
-                        PERFORM_OPERATION(acc, x_ptr[((i * xj + j) * xk + k) * xl + l]);
-                    }
-                    result_ptr[(i * xj + j) * xl + l] = acc;
-                }
-            }
-        }
-    } else if (axis == 3) {
-        for (int i = 0; i < xi; i++) {
-            for (int j = 0; j < xj; j++) {
-                for (int k = 0; k < xk; k++) {
-                    float acc = x_ptr[((i * xj + j) * xk + k) * xl];
-                    for (int l = 1; l < xl; l++) {
-                        PERFORM_OPERATION(acc, x_ptr[((i * xj + j) * xk + k) * xl + l]);
-                    }
-                    result_ptr[(i * xj + j) * xk + k] = acc;
-                }
-            }
-        }
-    }
-}
+#include <collection_fun.h>
 
 JNIEXPORT jfloat JNICALL Java_com_wsr_cpu_JCollection_maxD1(
         JNIEnv *env, jobject obj, jobject x
 ) {
-    return (jfloat)reduceD1<Operation::Max>(env, obj, x);
+    jfloat *x_ptr = (jfloat*)env->GetDirectBufferAddress(x);
+    size_t size = static_cast<size_t>(env->GetDirectBufferCapacity(x)) / sizeof(jfloat);
+    return (jfloat)max_d1(x_ptr, size);
 }
 
 
 JNIEXPORT void JNICALL Java_com_wsr_cpu_JCollection_maxD2(
         JNIEnv *env, jobject obj, jobject x, jint xi, jint xj, jint axis, jobject result
 ) {
-    reduceD2<Operation::Max>(env, obj, x, xi, xj, axis, result);
+    jfloat *x_ptr = (jfloat*)env->GetDirectBufferAddress(x);
+    jfloat *result_ptr = (jfloat*)env->GetDirectBufferAddress(result);
+    max_d2(x_ptr, xi, xj, axis, result_ptr);
 }
 
 JNIEXPORT void JNICALL Java_com_wsr_cpu_JCollection_maxD3(
         JNIEnv *env, jobject obj, jobject x, jint xi, jint xj, jint xk, jint axis, jobject result
 ) {
-    reduceD3<Operation::Max>(env, obj, x, xi, xj, xk, axis, result);
+    jfloat *x_ptr = (jfloat*)env->GetDirectBufferAddress(x);
+    jfloat *result_ptr = (jfloat*)env->GetDirectBufferAddress(result);
+    max_d3(x_ptr, xi, xj, xk, axis, result_ptr);
 }
 
 JNIEXPORT void JNICALL Java_com_wsr_cpu_JCollection_maxD4(
         JNIEnv *env, jobject obj, jobject x, jint xi, jint xj, jint xk, jint xl, jint axis, jobject result
 ) {
-    reduceD4<Operation::Max>(env, obj, x, xi, xj, xk, xl, axis, result);
+    jfloat *x_ptr = (jfloat*)env->GetDirectBufferAddress(x);
+    jfloat *result_ptr = (jfloat*)env->GetDirectBufferAddress(result);
+    max_d4(x_ptr, xi, xj, xk, xl, axis, result_ptr);
 }
 
 JNIEXPORT jfloat JNICALL Java_com_wsr_cpu_JCollection_minD1(
         JNIEnv *env, jobject obj, jobject x
 ) {
-    return (jfloat)reduceD1<Operation::Min>(env, obj, x);
+    jfloat *x_ptr = (jfloat*)env->GetDirectBufferAddress(x);
+    size_t size = static_cast<size_t>(env->GetDirectBufferCapacity(x)) / sizeof(jfloat);
+    return (jfloat)min_d1(x_ptr, size);
 }
 
 JNIEXPORT void JNICALL Java_com_wsr_cpu_JCollection_minD2(
         JNIEnv *env, jobject obj, jobject x, jint xi, jint xj, jint axis, jobject result
 ) {
-    reduceD2<Operation::Min>(env, obj, x, xi, xj, axis, result);
+    jfloat *x_ptr = (jfloat*)env->GetDirectBufferAddress(x);
+    jfloat *result_ptr = (jfloat*)env->GetDirectBufferAddress(result);
+    min_d2(x_ptr, xi, xj, axis, result_ptr);
 }
 
 JNIEXPORT void JNICALL Java_com_wsr_cpu_JCollection_minD3(
         JNIEnv *env, jobject obj, jobject x, jint xi, jint xj, jint xk, jint axis, jobject result
 ) {
-    reduceD3<Operation::Min>(env, obj, x, xi, xj, xk, axis, result);
+    jfloat *x_ptr = (jfloat*)env->GetDirectBufferAddress(x);
+    jfloat *result_ptr = (jfloat*)env->GetDirectBufferAddress(result);
+    min_d3(x_ptr, xi, xj, xk, axis, result_ptr);
 }
 
 JNIEXPORT void JNICALL Java_com_wsr_cpu_JCollection_minD4(
         JNIEnv *env, jobject obj, jobject x, jint xi, jint xj, jint xk, jint xl, jint axis, jobject result
 ) {
-    reduceD4<Operation::Min>(env, obj, x, xi, xj, xk, xl, axis, result);
+    jfloat *x_ptr = (jfloat*)env->GetDirectBufferAddress(x);
+    jfloat *result_ptr = (jfloat*)env->GetDirectBufferAddress(result);
+    min_d4(x_ptr, xi, xj, xk, xl, axis, result_ptr);
 }
 
 JNIEXPORT jfloat JNICALL Java_com_wsr_cpu_JCollection_sumD1(
         JNIEnv *env, jobject obj, jobject x
 ) {
-    return (jfloat)reduceD1<Operation::Sum>(env, obj, x);
+    jfloat *x_ptr = (jfloat*)env->GetDirectBufferAddress(x);
+    size_t size = static_cast<size_t>(env->GetDirectBufferCapacity(x)) / sizeof(jfloat);
+    return (jfloat)sum_d1(x_ptr, size);
 }
 
 JNIEXPORT void JNICALL Java_com_wsr_cpu_JCollection_sumD2(
         JNIEnv *env, jobject obj, jobject x, jint xi, jint xj, jint axis, jobject result
 ) {
-    reduceD2<Operation::Sum>(env, obj, x, xi, xj, axis, result);
+    jfloat *x_ptr = (jfloat*)env->GetDirectBufferAddress(x);
+    jfloat *result_ptr = (jfloat*)env->GetDirectBufferAddress(result);
+    sum_d2(x_ptr, xi, xj, axis, result_ptr);
 }
 
 JNIEXPORT void JNICALL Java_com_wsr_cpu_JCollection_sumD3(
         JNIEnv *env, jobject obj, jobject x, jint xi, jint xj, jint xk, jint axis, jobject result
 ) {
-    reduceD3<Operation::Sum>(env, obj, x, xi, xj, xk, axis, result);
+    jfloat *x_ptr = (jfloat*)env->GetDirectBufferAddress(x);
+    jfloat *result_ptr = (jfloat*)env->GetDirectBufferAddress(result);
+    sum_d3(x_ptr, xi, xj, xk, axis, result_ptr);
 }
 
 JNIEXPORT void JNICALL Java_com_wsr_cpu_JCollection_sumD4(
         JNIEnv *env, jobject obj, jobject x, jint xi, jint xj, jint xk, jint xl, jint axis, jobject result
 ) {
-    reduceD4<Operation::Sum>(env, obj, x, xi, xj, xk, xl, axis, result);
+    jfloat *x_ptr = (jfloat*)env->GetDirectBufferAddress(x);
+    jfloat *result_ptr = (jfloat*)env->GetDirectBufferAddress(result);
+    sum_d4(x_ptr, xi, xj, xk, xl, axis, result_ptr);
 }
