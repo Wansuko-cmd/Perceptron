dde5ee810bc673cc537ba417d607f3a2eac7ed23
Wansuko-cmd <63241922+Wansuko-cmd@users.noreply.github.com>
2026-01-02 23:00:54 +0900

    [fix] Backendを使う形に変更

--- io-type/src/commonMain/kotlin/com/wsr/batch/collecction/average/D2Ext.kt
+++ io-type/src/commonMain/kotlin/com/wsr/batch/collecction/average/D2Ext.kt
@@ -1,22 +1,30 @@
 package com.wsr.batch.collecction.average
 
+import com.wsr.Backend
 import com.wsr.batch.Batch
-import com.wsr.batch.get
 import com.wsr.core.IOType
-import com.wsr.core.collection.average.average
-import com.wsr.core.d0
-import com.wsr.core.operation.div.div
-import com.wsr.core.operation.plus.plus
 
 @JvmName("batchD2sAverageBatch")
-fun Batch<IOType.D2>.average(): Batch<IOType.D0> = Batch(size) { IOType.d0(this[it].average()) }
+fun Batch<IOType.D2>.average(): Batch<IOType.D0> {
+    val result = Backend.average(x = value, xi = size, xj = step, axis = 1)
+    return Batch(size = size, shape = listOf(1), value = result)
+}
 
 @JvmName("batchD2sAverageWithAxis")
-fun Batch<IOType.D2>.average(axis: Int) = Batch(size) { this[it].average(axis) }
+fun Batch<IOType.D2>.average(axis: Int): Batch<IOType.D1> {
+    val result = Backend.average(x = value, xi = size, xj = shape[0], xk = shape[1], axis = axis + 1)
+    return Batch(
+        size = size,
+        shape = when (axis) {
+            0 -> listOf(shape[1])
+            else -> listOf(shape[0])
+        },
+        value = result,
+    )
+}
 
 @JvmName("batchD2sBatchAverage")
 fun Batch<IOType.D2>.batchAverage(): IOType.D2 {
-    var sum = this[0]
-    for (i in 1 until size) sum += this[i]
-    return sum / size.toFloat()
+    val result = Backend.average(x = value, xi = size, xj = step, axis = 0)
+    return IOType.D2(shape = shape, value = result)
 }
