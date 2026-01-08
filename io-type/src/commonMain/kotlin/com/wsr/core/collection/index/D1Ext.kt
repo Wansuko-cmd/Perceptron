73ab251b6176b17c37f174c9aa70db0dc8760312
Wansuko-cmd <63241922+Wansuko-cmd@users.noreply.github.com>
2026-01-08 21:29:30 +0900

    [fix] IOTypeでの実装を置き換え

--- io-type/src/commonMain/kotlin/com/wsr/core/collection/index/D1Ext.kt
+++ io-type/src/commonMain/kotlin/com/wsr/core/collection/index/D1Ext.kt
@@ -1,64 +1,9 @@
 package com.wsr.core.collection.index
 
+import com.wsr.Backend
 import com.wsr.core.IOType
-import com.wsr.core.get
-import com.wsr.core.set
 import kotlin.random.Random
 
-fun IOType.D1.topK(k: Int, random: Random = Random): Int {
-    val target = maxWithIndex()
-        .takeWhile { it.first >= 0 }
-        .take(k)
-        .toList()
+fun IOType.D1.topK(k: Int, random: Random = Random): Int = Backend.topK(x = value, k = k, random = random)
 
-    return target.randomIndex(random)
-}
-
-fun IOType.D1.topP(p: Float, random: Random = Random): Int {
-    val total = maxWithIndex()
-        .takeWhile { it.first >= 0 }
-        .sumOf { it.first.toDouble() }
-
-    var sum = 0.0
-    val threshold = total * p
-
-    val target = maxWithIndex()
-        .takeWhile { it.first >= 0 }
-        .takeWhile { (value, _) ->
-            val shouldTake = sum < threshold
-            sum += value
-            shouldTake
-        }
-        .toList()
-        .ifEmpty { listOf(maxWithIndex().first()) }
-
-    return target.randomIndex(random)
-}
-
-private fun IOType.D1.maxWithIndex(): Sequence<Pair<Float, Int>> = sequence {
-    val target = value.toFloatArray().copyOf()
-    repeat(target.size) {
-        var index = 0
-        var max = Float.MIN_VALUE
-        for (i in 0 until shape[0]) {
-            if (max < target[i]) {
-                index = i
-                max = target[i]
-            }
-        }
-        target[index] = Float.MIN_VALUE
-        yield(max to index)
-    }
-}
-
-private fun List<Pair<Float, Int>>.randomIndex(random: Random): Int {
-    val sum = sumOf { (value, _) -> value.toDouble() }
-    if (sum == 0.0) return random(random).second
-
-    var p = random.nextDouble(from = 0.0, until = sum)
-    for ((value, index) in this) {
-        p -= value
-        if (p <= 0) return index
-    }
-    return last().second
-}
+fun IOType.D1.topP(p: Float, random: Random = Random): Int = Backend.topP(x = value, p = p, random = random)
