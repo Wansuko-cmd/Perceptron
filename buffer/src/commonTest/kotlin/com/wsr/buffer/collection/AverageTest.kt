503e0d073e6328ec8fc6229104c05a76745f45ae
Wansuko-cmd <63241922+Wansuko-cmd@users.noreply.github.com>
2026-01-11 18:25:36 +0900

    [add] collection系のテストを追加

---
+++ buffer/src/commonTest/kotlin/com/wsr/buffer/collection/AverageTest.kt
@@ -0,0 +1,121 @@
+@file:Suppress("NonAsciiCharacters")
+
+package com.wsr.buffer.collection
+
+import com.wsr.Backend
+import com.wsr.base.data.DataBuffer
+import com.wsr.buffer.BufferTestRule
+import com.wsr.create
+import kotlin.test.Test
+import kotlin.test.assertEquals
+import org.junit.Rule
+
+class AverageTest {
+    @get:Rule
+    val bufferTestRule = BufferTestRule()
+
+    @Test
+    fun `1次元平均`() {
+        val x = DataBuffer.create(FloatArray(24) { it.toFloat() })
+
+        val actual = Backend.average(x = x)
+
+        assertEquals(expected = 11.5f, actual = actual)
+    }
+
+    @Test
+    fun `2次元平均(axis=0)`() {
+        val x = DataBuffer.create(FloatArray(24) { it.toFloat() })
+
+        val actual = Backend.average(x = x, xi = 4, xj = 6, axis = 0)
+
+        assertEquals(expected = DataBuffer.create(9f, 10f, 11f, 12f, 13f, 14f), actual = actual)
+    }
+
+    @Test
+    fun `2次元平均(axis=1)`() {
+        val x = DataBuffer.create(FloatArray(24) { it.toFloat() })
+
+        val actual = Backend.average(x = x, xi = 4, xj = 6, axis = 1)
+
+        assertEquals(expected = DataBuffer.create(2.5f, 8.5f, 14.5f, 20.5f), actual = actual)
+    }
+
+    @Test
+    fun `3次元平均(axis=0)`() {
+        val x = DataBuffer.create(FloatArray(24) { it.toFloat() })
+
+        val actual = Backend.average(x = x, xi = 4, xj = 3, xk = 2, axis = 0)
+
+        assertEquals(expected = DataBuffer.create(9f, 10f, 11f, 12f, 13f, 14f), actual = actual)
+    }
+
+    @Test
+    fun `3次元平均(axis=1)`() {
+        val x = DataBuffer.create(FloatArray(24) { it.toFloat() })
+
+        val actual = Backend.average(x = x, xi = 4, xj = 3, xk = 2, axis = 1)
+
+        assertEquals(expected = DataBuffer.create(2f, 3f, 8f, 9f, 14f, 15f, 20f, 21f), actual = actual)
+    }
+
+    @Test
+    fun `3次元平均(axis=2)`() {
+        val x = DataBuffer.create(FloatArray(24) { it.toFloat() })
+
+        val actual = Backend.average(x = x, xi = 4, xj = 3, xk = 2, axis = 2)
+
+        assertEquals(
+            expected = DataBuffer.create(0.5f, 2.5f, 4.5f, 6.5f, 8.5f, 10.5f, 12.5f, 14.5f, 16.5f, 18.5f, 20.5f, 22.5f),
+            actual = actual,
+        )
+    }
+
+    @Test
+    fun `4次元平均(axis=0)`() {
+        val x = DataBuffer.create(FloatArray(24) { it.toFloat() })
+
+        val actual = Backend.average(x = x, xi = 2, xj = 2, xk = 2, xl = 3, axis = 0)
+
+        assertEquals(
+            expected = DataBuffer.create(6f, 7f, 8f, 9f, 10f, 11f, 12f, 13f, 14f, 15f, 16f, 17f),
+            actual = actual,
+        )
+    }
+
+    @Test
+    fun `4次元平均(axis=1)`() {
+        val x = DataBuffer.create(FloatArray(24) { it.toFloat() })
+
+        val actual = Backend.average(x = x, xi = 2, xj = 2, xk = 2, xl = 3, axis = 1)
+
+        assertEquals(
+            expected = DataBuffer.create(3f, 4f, 5f, 6f, 7f, 8f, 15f, 16f, 17f, 18f, 19f, 20f),
+            actual = actual,
+        )
+    }
+
+    @Test
+    fun `4次元平均(axis=2)`() {
+        val x = DataBuffer.create(FloatArray(24) { it.toFloat() })
+
+        val actual = Backend.average(x = x, xi = 2, xj = 2, xk = 2, xl = 3, axis = 2)
+
+        assertEquals(
+            expected = DataBuffer.create(1.5f, 2.5f, 3.5f, 7.5f, 8.5f, 9.5f, 13.5f, 14.5f, 15.5f, 19.5f, 20.5f, 21.5f),
+            actual = actual,
+        )
+    }
+
+    @Test
+    fun `4次元平均(axis=3)`() {
+        val x = DataBuffer.create(FloatArray(24) { it.toFloat() })
+
+        val actual = Backend.average(x = x, xi = 2, xj = 2, xk = 2, xl = 3, axis = 3)
+
+        assertEquals(
+            expected = DataBuffer.create(1f, 4f, 7f, 10f, 13f, 16f, 19f, 22f),
+            actual = actual,
+        )
+    }
+}
---
+++ buffer/src/commonTest/kotlin/com/wsr/buffer/collection/SumTest.kt
@@ -0,0 +1,121 @@
+@file:Suppress("NonAsciiCharacters")
+
+package com.wsr.buffer.collection
+
+import com.wsr.Backend
+import com.wsr.base.data.DataBuffer
+import com.wsr.buffer.BufferTestRule
+import com.wsr.create
+import kotlin.test.Test
+import kotlin.test.assertEquals
+import org.junit.Rule
+
+class SumTest {
+    @get:Rule
+    val bufferTestRule = BufferTestRule()
+
+    @Test
+    fun `1次元内累積値`() {
+        val x = DataBuffer.create(FloatArray(24) { it.toFloat() })
+
+        val actual = Backend.sum(x = x)
+
+        assertEquals(expected = 276f, actual = actual)
+    }
+
+    @Test
+    fun `2次元累積値(axis=0)`() {
+        val x = DataBuffer.create(FloatArray(24) { it.toFloat() })
+
+        val actual = Backend.sum(x = x, xi = 4, xj = 6, axis = 0)
+
+        assertEquals(expected = DataBuffer.create(36f, 40f, 44f, 48f, 52f, 56f), actual = actual)
+    }
+
+    @Test
+    fun `2次元累積値(axis=1)`() {
+        val x = DataBuffer.create(FloatArray(24) { it.toFloat() })
+
+        val actual = Backend.sum(x = x, xi = 4, xj = 6, axis = 1)
+
+        assertEquals(expected = DataBuffer.create(15f, 51f, 87f, 123f), actual = actual)
+    }
+
+    @Test
+    fun `3次元累積値(axis=0)`() {
+        val x = DataBuffer.create(FloatArray(24) { it.toFloat() })
+
+        val actual = Backend.sum(x = x, xi = 4, xj = 3, xk = 2, axis = 0)
+
+        assertEquals(expected = DataBuffer.create(36f, 40f, 44f, 48f, 52f, 56f), actual = actual)
+    }
+
+    @Test
+    fun `3次元累積値(axis=1)`() {
+        val x = DataBuffer.create(FloatArray(24) { it.toFloat() })
+
+        val actual = Backend.sum(x = x, xi = 4, xj = 3, xk = 2, axis = 1)
+
+        assertEquals(expected = DataBuffer.create(6f, 9f, 24f, 27f, 42f, 45f, 60f, 63f), actual = actual)
+    }
+
+    @Test
+    fun `3次元累積値(axis=2)`() {
+        val x = DataBuffer.create(FloatArray(24) { it.toFloat() })
+
+        val actual = Backend.sum(x = x, xi = 4, xj = 3, xk = 2, axis = 2)
+
+        assertEquals(
+            expected = DataBuffer.create(1f, 5f, 9f, 13f, 17f, 21f, 25f, 29f, 33f, 37f, 41f, 45f),
+            actual = actual,
+        )
+    }
+
+    @Test
+    fun `4次元累積値(axis=0)`() {
+        val x = DataBuffer.create(FloatArray(24) { it.toFloat() })
+
+        val actual = Backend.sum(x = x, xi = 2, xj = 2, xk = 2, xl = 3, axis = 0)
+
+        assertEquals(
+            expected = DataBuffer.create(12f, 14f, 16f, 18f, 20f, 22f, 24f, 26f, 28f, 30f, 32f, 34f),
+            actual = actual,
+        )
+    }
+
+    @Test
+    fun `4次元累積値(axis=1)`() {
+        val x = DataBuffer.create(FloatArray(24) { it.toFloat() })
+
+        val actual = Backend.sum(x = x, xi = 2, xj = 2, xk = 2, xl = 3, axis = 1)
+
+        assertEquals(
+            expected = DataBuffer.create(6f, 8f, 10f, 12f, 14f, 16f, 30f, 32f, 34f, 36f, 38f, 40f),
+            actual = actual,
+        )
+    }
+
+    @Test
+    fun `4次元累積値(axis=2)`() {
+        val x = DataBuffer.create(FloatArray(24) { it.toFloat() })
+
+        val actual = Backend.sum(x = x, xi = 2, xj = 2, xk = 2, xl = 3, axis = 2)
+
+        assertEquals(
+            expected = DataBuffer.create(3f, 5f, 7f, 15f, 17f, 19f, 27f, 29f, 31f, 39f, 41f, 43f),
+            actual = actual,
+        )
+    }
+
+    @Test
+    fun `4次元累積値(axis=3)`() {
+        val x = DataBuffer.create(FloatArray(24) { it.toFloat() })
+
+        val actual = Backend.sum(x = x, xi = 2, xj = 2, xk = 2, xl = 3, axis = 3)
+
+        assertEquals(
+            expected = DataBuffer.create(3f, 12f, 21f, 30f, 39f, 48f, 57f, 66f),
+            actual = actual,
+        )
+    }
+}
