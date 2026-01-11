1997cc5457534c9c3bb125e0d1cab80af9209979
Wansuko-cmd <63241922+Wansuko-cmd@users.noreply.github.com>
2026-01-11 14:08:47 +0900

    [add] BufferTestRuleを追加

---
+++ buffer/src/commonTest/kotlin/com/wsr/buffer/BufferTestRule.kt
@@ -0,0 +1,26 @@
+package com.wsr.buffer
+
+import com.wsr.Backend
+import com.wsr.base.KotlinBackend
+import com.wsr.cpu.cpu
+import com.wsr.gpu.gpu
+import org.junit.rules.TestRule
+import org.junit.runner.Description
+import org.junit.runners.model.Statement
+import kotlin.time.measureTime
+
+private val targets = listOf(KotlinBackend, cpu, gpu)
+
+class BufferTestRule : TestRule {
+    override fun apply(base: Statement?, description: Description?): Statement = object : Statement() {
+        override fun evaluate() {
+            targets.forEach { target ->
+                Backend.set(target)
+                measureTime { base?.evaluate() }
+                    .also { time ->
+                        println("${description?.className}::${description?.methodName}(${target::class.simpleName}): $time")
+                    }
+            }
+        }
+    }
+}
