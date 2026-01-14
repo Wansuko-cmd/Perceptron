5a3ce408fd49e1a71bb5dce3de3319d73a2a8ba7
Wansuko-cmd <63241922+Wansuko-cmd@users.noreply.github.com>
2026-01-14 19:20:25 +0900

    [fix] Mnistのテストの内容を一部移植

--- sample/src/test/kotlin/mnist/MnistTest.kt
+++ sample/src/test/kotlin/mnist/MnistTest.kt
@@ -2,16 +2,87 @@
 
 package mnist
 
-import dataset.mnist.createMnistModel
+import com.wsr.network.Network
+import com.wsr.network.NetworkBuilder
+import com.wsr.network.initializer.He
+import com.wsr.network.optimizer.Scheduler
+import com.wsr.network.optimizer.adam.AdamW
+import com.wsr.network.output.softmax.softmaxWithLoss
+import com.wsr.network.process.compute.affine.affine
+import com.wsr.network.process.compute.bias.d1.bias
+import com.wsr.network.process.compute.function.relu.reLU
+import com.wsr.network.process.compute.function.relu.swish
+import com.wsr.network.process.compute.norm.layer.d1.layerNorm
+import com.wsr.network.process.compute.skip.skip
+import com.wsr.network.process.reshape.reshape.reshapeToD1
+import dataset.mnist.LabelConverter
+import dataset.mnist.MnistDataset
+import dataset.mnist.inputPx
 import kotlin.test.Test
 
-private const val EPOC = 1
-private const val SEED = 0
+private const val TRAIN_IMAGE_PATH = "mnist/train-images-idx3-ubyte.gz"
+private const val TRAIN_LABEL_PATH = "mnist/train-labels-idx1-ubyte.gz"
+
+private const val TEST_IMAGE_PATH = "mnist/t10k-images-idx3-ubyte.gz"
+private const val TEST_LABEL_PATH = "mnist/t10k-labels-idx1-ubyte.gz"
 
 class MnistTest {
     @Test
     fun `Mnistモデルの精度が落ちていないか確認`() {
-        val actual = createMnistModel(epoc = EPOC, seed = SEED)
-        assert(actual > 0.9f) { "精度が90%を割っています" }
+        println("ネットワーク構築")
+        val network = createNetwork()
+
+        println("Json変換")
+        network.toJson()
+            .also { println(it) }
+            .also { println(Network.fromJson<List<Float>, Int>(it)) }
+
+        println("訓練開始")
+        val train = MnistDataset.read(imagePath = TRAIN_IMAGE_PATH, labelPath = TRAIN_LABEL_PATH)
+        train.chunked(240).mapIndexed { i, data ->
+            if (i % 10 == 0) println("train: $i")
+            network.train(
+                input = data.map { it.pixels },
+                label = data.map { it.label },
+            )
+        }
+
+        println("評価開始")
+        val test = MnistDataset
+            .read(imagePath = TEST_IMAGE_PATH, labelPath = TEST_LABEL_PATH)
+            .take(100)
+
+        val accuracy = test
+            .count { data -> network.expect(input = data.pixels) == data.label }
+            .let { it.toFloat() / test.size.toFloat() }
+
+        println("${accuracy * 100}%")
+
+        assert(accuracy > 0.9f) { "精度が90%を割っています" }
     }
+
+    private fun createNetwork(): Network<List<Float>, Int> = NetworkBuilder
+        .inputPx(
+            x = 28,
+            y = 28,
+            optimizer = AdamW(scheduler = Scheduler.Fix(0.001f)),
+            initializer = He(seed = 0),
+        )
+        .reshapeToD1()
+        .affine(neuron = 512).bias().reLU()
+        .repeat(5) {
+            skip {
+                this
+                    .layerNorm().affine(neuron = 512).bias().swish()
+                    .layerNorm().affine(neuron = 512).bias().swish()
+            }
+        }
+        .skip {
+            this
+                .layerNorm().affine(neuron = 512).bias().swish()
+                .layerNorm().affine(neuron = 128).bias().swish()
+        }
+        .affine(neuron = 10)
+        .softmaxWithLoss(converter = { LabelConverter(inputSize) })
+
 }
