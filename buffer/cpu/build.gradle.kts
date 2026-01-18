55abaffb4406fdf661074e5e278bbf37c7deec92
Wansuko-cmd <63241922+Wansuko-cmd@users.noreply.github.com>
2026-01-19 01:38:52 +0900

    [fix] ビルドが通るように設定を変更

--- buffer/cpu/build.gradle.kts
+++ buffer/cpu/build.gradle.kts
@@ -6,13 +6,31 @@ kotlin {
     applyDefaultHierarchyTemplate()
     jvm()
 
-    mingwX64()
-
-    linuxX64()
-    linuxArm64()
-
-    macosX64()
-    macosArm64()
+    listOf(
+        mingwX64(),
+        linuxX64(),
+        linuxArm64(),
+        macosX64(),
+        macosArm64(),
+    ).forEach { arch ->
+        arch.compilations.getByName("main") {
+            cinterops {
+                val lib by creating {
+                    val headersDir = "$projectDir/cpp/src/cpu"
+                    headers(
+                        "$headersDir/collection_fun.h",
+                        "$headersDir/collection_fun.h",
+                        "$headersDir/mat_mul_fun.h",
+                        "$headersDir/math_fun.h",
+                        "$headersDir/operation_fun.h",
+                        "$headersDir/transpose_fun.h",
+                    )
+                    defFile(project.file("src/nativeMain/lib.def"))
+                    extraOpts("-libraryPath", "$projectDir/src/nativeMain/resources/cpu/${target.name}")
+                }
+            }
+        }
+    }
 
     iosX64()
     iosArm64()
--- buffer/cpu/cpp/src/cpu/math_fun.cpp
+++ buffer/cpu/cpp/src/cpu/math_fun.cpp
@@ -3,26 +3,26 @@
 #include <cmath>
 #include <algorithm>
 
-void exp_d1(const float* x, float* result, size_t size) {
-    for (size_t i = 0; i < size; i++) {
+void exp_d1(const float* x, float* result, int size) {
+    for (int i = 0; i < size; i++) {
         result[i] = std::exp(x[i]);
     }
 }
 
-void ln_d1(const float* x, float e, float* result, size_t size) {
-    for (size_t i = 0; i < size; i++) {
+void ln_d1(const float* x, float e, float* result, int size) {
+    for (int i = 0; i < size; i++) {
         result[i] = std::log(x[i] + e);
     }
 }
 
-void pow_d1(const float* x, int n, float* result, size_t size) {
-    for (size_t i = 0; i < size; i++) {
+void pow_d1(const float* x, int n, float* result, int size) {
+    for (int i = 0; i < size; i++) {
         result[i] = std::pow(x[i], n);
     }
 }
 
-void sqrt_d1(const float* x, float e, float* result, size_t size) {
-    for (size_t i = 0; i < size; i++) {
+void sqrt_d1(const float* x, float e, float* result, int size) {
+    for (int i = 0; i < size; i++) {
         result[i] = std::sqrt(x[i] + e);
     }
 }
