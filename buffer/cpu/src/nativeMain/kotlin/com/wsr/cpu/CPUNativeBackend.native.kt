5f4f3d73d2c0b9cf08b82b4fb4ee3f6288167ecd
Wansuko-cmd <63241922+Wansuko-cmd@users.noreply.github.com>
2026-01-21 09:51:14 +0900

    [fix] デフォルトのgeneratorに変更

--- buffer/cpu/src/nativeMain/kotlin/com/wsr/cpu/CPUNativeBackend.native.kt
+++ buffer/cpu/src/nativeMain/kotlin/com/wsr/cpu/CPUNativeBackend.native.kt
@@ -2,9 +2,10 @@ package com.wsr.cpu
 
 import com.wsr.base.IBackend
 import com.wsr.base.KotlinBackend
+import com.wsr.base.data.Default
 
 actual fun loadCPUBackend(): IBackend? = CPUNativeBackend()
 
 class CPUNativeBackend : IBackend by KotlinBackend {
-    override val generator = CPUNativeBuffer.generator
+    override val generator = Default.generator
 }
