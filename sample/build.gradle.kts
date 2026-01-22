1ad99f568858416b90f1f1380d774e6db8e13a1c
Wansuko-cmd <63241922+Wansuko-cmd@users.noreply.github.com>
2026-01-22 20:56:46 +0900

    wip

--- sample/build.gradle.kts
+++ sample/build.gradle.kts
@@ -35,7 +35,7 @@ kotlin {
         all {
             if (hostOs.isWindows || hostOs.isLinux) {
                 val path = System.getenv("OPENBLAS_HOME")
-                linkerOpts("-L$path")
+                linkerOpts("-l$path")
             }
         }
         executable {
