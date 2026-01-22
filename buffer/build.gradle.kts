15331639dc00a421171d67d14cec34f8a78b06c1
Wansuko-cmd <63241922+Wansuko-cmd@users.noreply.github.com>
2026-01-22 13:21:47 +0900

    wip

--- buffer/build.gradle.kts
+++ buffer/build.gradle.kts
@@ -21,7 +21,7 @@ kotlin {
     hostTarget.binaries.all {
         if (hostOs.isWindows || hostOs.isLinux) {
             val path = System.getenv("OPENBLAS_HOME")
-            linkerOpts("$path")
+            linkerOpts("-L$path")
         }
     }
 
