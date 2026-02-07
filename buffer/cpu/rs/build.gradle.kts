26fdbcc996e77bedc67f250334e4e932acc4fb94
Wansuko-cmd <63241922+Wansuko-cmd@users.noreply.github.com>
2026-02-08 02:51:08 +0900

    [add] 最適化フラグを追加

--- buffer/cpu/rs/build.gradle.kts
+++ buffer/cpu/rs/build.gradle.kts
@@ -1,3 +1,5 @@
+import org.gradle.nativeplatform.platform.internal.DefaultNativePlatform
+
 val sourceDir = projectDir
 val targetDir = projectDir.resolve("target")
 
@@ -8,6 +10,17 @@ val cargoJvmBuild by tasks.registering(Exec::class) {
     group = "build"
     description = "JVM環境向けにビルド"
     workingDir = sourceDir
+
+    val hostOs = DefaultNativePlatform.getCurrentOperatingSystem()
+    val hostArch = DefaultNativePlatform.getCurrentArchitecture()
+    val target = when {
+        hostArch.isAmd64 -> "x86-64-v3"
+        hostOs.isMacOsX && hostArch.isArm64 -> "apple-m1"
+        hostArch.isArm64 -> "generic -C target-feature=+neon"
+        else -> "native"
+    }
+    environment("RUSTFLAGS", "-C target-cpu=$target")
+
     commandLine = listOf("cargo", "build", "--release")
 }
 
