319ce97dcd532b8e8b63e096bd4fd3e802ba5b78
Wansuko-cmd <63241922+Wansuko-cmd@users.noreply.github.com>
2026-02-08 10:55:37 +0900

    [revert] フラグだけ元に戻す

--- buffer/cpu/rs/build.gradle.kts
+++ buffer/cpu/rs/build.gradle.kts
@@ -1,5 +1,3 @@
-import org.gradle.nativeplatform.platform.internal.DefaultNativePlatform
-
 val sourceDir = projectDir
 val targetDir = projectDir.resolve("target")
 
@@ -10,17 +8,6 @@ val cargoJvmBuild by tasks.registering(Exec::class) {
     group = "build"
     description = "JVM環境向けにビルド"
     workingDir = sourceDir
-
-    val hostOs = DefaultNativePlatform.getCurrentOperatingSystem()
-    val hostArch = DefaultNativePlatform.getCurrentArchitecture()
-    val target = when {
-        hostArch.isAmd64 -> "x86-64-v3"
-        hostOs.isMacOsX && hostArch.isArm64 -> "apple-m1"
-        hostArch.isArm64 -> "generic -C target-feature=+neon"
-        else -> "native"
-    }
-    environment("RUSTFLAGS", "-C target-cpu=$target")
-
     commandLine = listOf("cargo", "build", "--release")
 }
 
