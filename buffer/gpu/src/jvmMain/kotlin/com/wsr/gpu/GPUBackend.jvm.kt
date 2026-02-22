a5d1783714f17dfb0a77c20bd3ae03840d23172a
Wansuko-cmd <63241922+Wansuko-cmd@users.noreply.github.com>
2026-02-23 01:36:41 +0900

    [add] JVM側で初期化を呼び出すコードを追加

--- buffer/gpu/src/jvmMain/kotlin/com/wsr/gpu/GPUBackend.jvm.kt
+++ buffer/gpu/src/jvmMain/kotlin/com/wsr/gpu/GPUBackend.jvm.kt
@@ -4,10 +4,25 @@ import com.wsr.base.IBackend
 import com.wsr.base.KotlinBackend
 import com.wsr.base.data.DataBuffer
 import com.wsr.base.loadNativeLibrary
+import java.lang.ref.Cleaner
 
 private const val LIB_PATH = "gpu"
 private const val LIB_NAME = "gpu"
 
-actual fun loadGPUBackend(): IBackend? = GPUBackend()
+actual fun loadGPUBackend(): IBackend? {
+    val isSuccess = loadNativeLibrary(path = LIB_PATH, name = LIB_NAME)
+    return if (isSuccess) GPUBackend() else null
+}
 
-class GPUBackend : IBackend by KotlinBackend
+class GPUBackend : IBackend by KotlinBackend {
+    val context = JContext().allocate()
+
+    init {
+        val ptr = context
+        cleaner.register(this) {JContext().release(ptr) }
+    }
+
+    companion object {
+        private val cleaner = Cleaner.create()
+    }
+}
