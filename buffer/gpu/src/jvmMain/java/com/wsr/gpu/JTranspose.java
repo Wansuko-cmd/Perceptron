8bef28b36b2df9f3eb2315381bbc12d0d5c8407c
Wansuko-cmd <63241922+Wansuko-cmd@users.noreply.github.com>
2026-02-24 00:21:05 +0900

    [add] JTransposeの定義を追加

---
+++ buffer/gpu/src/jvmMain/java/com/wsr/gpu/JTranspose.java
@@ -0,0 +1,34 @@
+package com.wsr.gpu;
+
+class JTranspose {
+    public native void transposeD2(
+            long x,
+            int xi,
+            int xj,
+            long result
+    );
+
+    public native void transposeD3(
+            long x,
+            int xi,
+            int xj,
+            int xk,
+            int axisI,
+            int axisJ,
+            int axisK,
+            long result
+    );
+
+    public native void transposeD4(
+            long x,
+            int xi,
+            int xj,
+            int xk,
+            int xl,
+            int axisI,
+            int axisJ,
+            int axisK,
+            int axisL,
+            long result
+    );
+}
