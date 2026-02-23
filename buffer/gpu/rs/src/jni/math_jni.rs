9694eb5b2bfbadc97cafab1cb67929310e50fcc1
Wansuko-cmd <63241922+Wansuko-cmd@users.noreply.github.com>
2026-02-23 23:00:25 +0900

    [add] 残りの関数の定義を追加

--- buffer/gpu/rs/src/jni/math_jni.rs
+++ buffer/gpu/rs/src/jni/math_jni.rs
@@ -1,6 +1,6 @@
 use jni::{JNIEnv};
 use jni::objects::{JClass};
-use jni::sys::{jlong};
+use jni::sys::{jfloat, jint, jlong};
 
 use crate::core::buffer::GPUBuffer;
 use crate::core::context::Context;
@@ -19,4 +19,52 @@ pub extern "system" fn Java_com_wsr_gpu_JMath_exp(
     let context = unsafe { &*(context as *const Context) };
 
     math::exp_d1(x, result, context);
+}
+
+#[unsafe(no_mangle)]
+pub extern "system" fn Java_com_wsr_gpu_JMath_ln(
+    _: JNIEnv,
+    _class: JClass,
+    x: jlong,
+    e: jfloat,
+    result: jlong,
+    context: jlong,
+) {
+    let x = unsafe { &*(x as *const GPUBuffer) };
+    let result = unsafe { &*(result as *const GPUBuffer) };
+    let context = unsafe { &*(context as *const Context) };
+
+    math::ln_d1(x, e as f32, result, context);
+}
+
+#[unsafe(no_mangle)]
+pub extern "system" fn Java_com_wsr_gpu_JMath_pow(
+    _: JNIEnv,
+    _class: JClass,
+    x: jlong,
+    n: jint,
+    result: jlong,
+    context: jlong,
+) {
+    let x = unsafe { &*(x as *const GPUBuffer) };
+    let result = unsafe { &*(result as *const GPUBuffer) };
+    let context = unsafe { &*(context as *const Context) };
+
+    math::pow_d1(x, n as i32, result, context);
+}
+
+#[unsafe(no_mangle)]
+pub extern "system" fn Java_com_wsr_gpu_JMath_sqrt(
+    _: JNIEnv,
+    _class: JClass,
+    x: jlong,
+    e: jfloat,
+    result: jlong,
+    context: jlong,
+) {
+    let x = unsafe { &*(x as *const GPUBuffer) };
+    let result = unsafe { &*(result as *const GPUBuffer) };
+    let context = unsafe { &*(context as *const Context) };
+
+    math::sqrt_d1(x, e as f32, result, context);
 }
