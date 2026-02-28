a89fccc736bb6a74ccfef6a959e8e620eabcf317
Wansuko-cmd <63241922+Wansuko-cmd@users.noreply.github.com>
2026-03-01 02:08:49 +0900

    [add] io-typeに対応を追加

---
+++ io-type/src/commonMain/kotlin/com/wsr/batch/compare/equals/D0Ext.kt
@@ -0,0 +1,51 @@
+package com.wsr.batch.compare.equals
+
+import com.wsr.Backend
+import com.wsr.batch.Batch
+import com.wsr.core.IOType
+import com.wsr.core.compare.equals.EQUALS_ABSOLUTE_TOLERANCE
+import com.wsr.core.compare.equals.EQUALS_RELATIVE_TOLERANCE
+import kotlin.jvm.JvmName
+
+@JvmName("infixBatchD0sEqFloat")
+infix fun Batch<IOType.D0>.eq(other: Float): Batch<IOType.D0> = eq(
+    other = other,
+    absoluteTolerance = EQUALS_ABSOLUTE_TOLERANCE,
+    relativeTolerance = EQUALS_RELATIVE_TOLERANCE,
+)
+
+fun Batch<IOType.D0>.eq(
+    other: Float,
+    absoluteTolerance: Float = EQUALS_ABSOLUTE_TOLERANCE,
+    relativeTolerance: Float = EQUALS_RELATIVE_TOLERANCE,
+): Batch<IOType.D0> {
+    val result = Backend.equals(
+        x = value,
+        y = other,
+        absoluteTolerance = absoluteTolerance,
+        relativeTolerance = relativeTolerance,
+    )
+    return Batch(size = size, shape = shape, value = result)
+}
+
+@JvmName("infixBatchD0sEqD0s")
+infix fun Batch<IOType.D0>.eq(other: Batch<IOType.D0>): Batch<IOType.D0> = eq(
+    other = other,
+    absoluteTolerance = EQUALS_ABSOLUTE_TOLERANCE,
+    relativeTolerance = EQUALS_RELATIVE_TOLERANCE,
+)
+
+@JvmName("batchD0sEqD0s")
+fun Batch<IOType.D0>.eq(
+    other: Batch<IOType.D0>,
+    absoluteTolerance: Float = EQUALS_ABSOLUTE_TOLERANCE,
+    relativeTolerance: Float = EQUALS_RELATIVE_TOLERANCE,
+): Batch<IOType.D0> {
+    val result = Backend.equals(
+        x = value,
+        y = other.value,
+        absoluteTolerance = absoluteTolerance,
+        relativeTolerance = relativeTolerance,
+    )
+    return Batch(size = size, shape = shape, value = result)
+}
---
+++ io-type/src/commonMain/kotlin/com/wsr/core/compare/equals/EqualsExt.kt
@@ -0,0 +1,207 @@
+package com.wsr.core.compare.equals
+
+import com.wsr.Backend
+import com.wsr.core.IOType
+
+internal const val EQUALS_ABSOLUTE_TOLERANCE = 1e-4f
+internal const val EQUALS_RELATIVE_TOLERANCE = 1e-4f
+
+infix fun IOType.D0.eq(other: Float) = eq(
+    other = other,
+    absoluteTolerance = EQUALS_ABSOLUTE_TOLERANCE,
+    relativeTolerance = EQUALS_RELATIVE_TOLERANCE,
+)
+
+fun IOType.D0.eq(
+    other: Float,
+    absoluteTolerance: Float = EQUALS_ABSOLUTE_TOLERANCE,
+    relativeTolerance: Float = EQUALS_RELATIVE_TOLERANCE,
+): IOType.D0 {
+    val result = Backend.equals(
+        x = value,
+        y = other,
+        absoluteTolerance = absoluteTolerance,
+        relativeTolerance = relativeTolerance,
+    )
+    return IOType.D0(result)
+}
+
+infix fun IOType.D0.eq(other: IOType.D0) = eq(
+    other = other,
+    absoluteTolerance = EQUALS_ABSOLUTE_TOLERANCE,
+    relativeTolerance = EQUALS_RELATIVE_TOLERANCE,
+)
+
+fun IOType.D0.eq(
+    other: IOType.D0,
+    absoluteTolerance: Float = EQUALS_ABSOLUTE_TOLERANCE,
+    relativeTolerance: Float = EQUALS_RELATIVE_TOLERANCE,
+): IOType.D0 {
+    val result = Backend.equals(
+        x = value,
+        y = other.value,
+        absoluteTolerance = absoluteTolerance,
+        relativeTolerance = relativeTolerance,
+    )
+    return IOType.D0(result)
+}
+
+infix fun IOType.D1.eq(other: Float) = eq(
+    other = other,
+    absoluteTolerance = EQUALS_ABSOLUTE_TOLERANCE,
+    relativeTolerance = EQUALS_RELATIVE_TOLERANCE,
+)
+
+fun IOType.D1.eq(
+    other: Float,
+    absoluteTolerance: Float = EQUALS_ABSOLUTE_TOLERANCE,
+    relativeTolerance: Float = EQUALS_RELATIVE_TOLERANCE,
+): IOType.D1 {
+    val result = Backend.equals(
+        x = value,
+        y = other,
+        absoluteTolerance = absoluteTolerance,
+        relativeTolerance = relativeTolerance,
+    )
+    return IOType.D1(result)
+}
+
+infix fun IOType.D1.eq(other: IOType.D1) = eq(
+    other = other,
+    absoluteTolerance = EQUALS_ABSOLUTE_TOLERANCE,
+    relativeTolerance = EQUALS_RELATIVE_TOLERANCE,
+)
+
+fun IOType.D1.eq(
+    other: IOType.D1,
+    absoluteTolerance: Float = EQUALS_ABSOLUTE_TOLERANCE,
+    relativeTolerance: Float = EQUALS_RELATIVE_TOLERANCE,
+): IOType.D1 {
+    val result = Backend.equals(
+        x = value,
+        y = other.value,
+        absoluteTolerance = absoluteTolerance,
+        relativeTolerance = relativeTolerance,
+    )
+    return IOType.D1(result)
+}
+
+infix fun IOType.D2.eq(other: Float) = eq(
+    other = other,
+    absoluteTolerance = EQUALS_ABSOLUTE_TOLERANCE,
+    relativeTolerance = EQUALS_RELATIVE_TOLERANCE,
+)
+
+fun IOType.D2.eq(
+    other: Float,
+    absoluteTolerance: Float = EQUALS_ABSOLUTE_TOLERANCE,
+    relativeTolerance: Float = EQUALS_RELATIVE_TOLERANCE,
+): IOType.D2 {
+    val result = Backend.equals(
+        x = value,
+        y = other,
+        absoluteTolerance = absoluteTolerance,
+        relativeTolerance = relativeTolerance,
+    )
+    return IOType.D2(shape = shape, value = result)
+}
+
+infix fun IOType.D2.eq(other: IOType.D2) = eq(
+    other = other,
+    absoluteTolerance = EQUALS_ABSOLUTE_TOLERANCE,
+    relativeTolerance = EQUALS_RELATIVE_TOLERANCE,
+)
+
+fun IOType.D2.eq(
+    other: IOType.D2,
+    absoluteTolerance: Float = EQUALS_ABSOLUTE_TOLERANCE,
+    relativeTolerance: Float = EQUALS_RELATIVE_TOLERANCE,
+): IOType.D2 {
+    val result = Backend.equals(
+        x = value,
+        y = other.value,
+        absoluteTolerance = absoluteTolerance,
+        relativeTolerance = relativeTolerance,
+    )
+    return IOType.D2(shape = shape, value = result)
+}
+
+infix fun IOType.D3.eq(other: Float) = eq(
+    other = other,
+    absoluteTolerance = EQUALS_ABSOLUTE_TOLERANCE,
+    relativeTolerance = EQUALS_RELATIVE_TOLERANCE,
+)
+
+fun IOType.D3.eq(
+    other: Float,
+    absoluteTolerance: Float = EQUALS_ABSOLUTE_TOLERANCE,
+    relativeTolerance: Float = EQUALS_RELATIVE_TOLERANCE,
+): IOType.D3 {
+    val result = Backend.equals(
+        x = value,
+        y = other,
+        absoluteTolerance = absoluteTolerance,
+        relativeTolerance = relativeTolerance,
+    )
+    return IOType.D3(shape = shape, value = result)
+}
+
+infix fun IOType.D3.eq(other: IOType.D3) = eq(
+    other = other,
+    absoluteTolerance = EQUALS_ABSOLUTE_TOLERANCE,
+    relativeTolerance = EQUALS_RELATIVE_TOLERANCE,
+)
+
+fun IOType.D3.eq(
+    other: IOType.D3,
+    absoluteTolerance: Float = EQUALS_ABSOLUTE_TOLERANCE,
+    relativeTolerance: Float = EQUALS_RELATIVE_TOLERANCE,
+): IOType.D3 {
+    val result = Backend.equals(
+        x = value,
+        y = other.value,
+        absoluteTolerance = absoluteTolerance,
+        relativeTolerance = relativeTolerance,
+    )
+    return IOType.D3(shape = shape, value = result)
+}
+
+infix fun IOType.D4.eq(other: Float) = eq(
+    other = other,
+    absoluteTolerance = EQUALS_ABSOLUTE_TOLERANCE,
+    relativeTolerance = EQUALS_RELATIVE_TOLERANCE,
+)
+
+fun IOType.D4.eq(
+    other: Float,
+    absoluteTolerance: Float = EQUALS_ABSOLUTE_TOLERANCE,
+    relativeTolerance: Float = EQUALS_RELATIVE_TOLERANCE,
+): IOType.D4 {
+    val result = Backend.equals(
+        x = value,
+        y = other,
+        absoluteTolerance = absoluteTolerance,
+        relativeTolerance = relativeTolerance,
+    )
+    return IOType.D4(shape = shape, value = result)
+}
+
+infix fun IOType.D4.eq(other: IOType.D4) = eq(
+    other = other,
+    absoluteTolerance = EQUALS_ABSOLUTE_TOLERANCE,
+    relativeTolerance = EQUALS_RELATIVE_TOLERANCE,
+)
+
+fun IOType.D4.eq(
+    other: IOType.D4,
+    absoluteTolerance: Float = EQUALS_ABSOLUTE_TOLERANCE,
+    relativeTolerance: Float = EQUALS_RELATIVE_TOLERANCE,
+): IOType.D4 {
+    val result = Backend.equals(
+        x = value,
+        y = other.value,
+        absoluteTolerance = absoluteTolerance,
+        relativeTolerance = relativeTolerance,
+    )
+    return IOType.D4(shape = shape, value = result)
+}
