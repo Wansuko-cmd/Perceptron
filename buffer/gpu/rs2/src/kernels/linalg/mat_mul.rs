5fab21fd15352a4f3f461d77b98996f7bd8fc7d6
Wansuko-cmd <63241922+Wansuko-cmd@users.noreply.github.com>
2026-03-04 00:54:29 +0900

    [add] mat_mul系を追加

---
+++ buffer/gpu/rs2/src/kernels/linalg/mat_mul.rs
@@ -0,0 +1,248 @@
+use wgpu::{Device, util::DeviceExt};
+
+use crate::{kernels::task::ComputeTask, resource::buffer::GPUBuffer};
+
+pub struct MatMul {
+    mat_mul_nn: wgpu::ComputePipeline,
+    mat_mul_nt: wgpu::ComputePipeline,
+    mat_mul_tn: wgpu::ComputePipeline,
+    mat_mul_tt: wgpu::ComputePipeline,
+
+    bind_group_layout: wgpu::BindGroupLayout,
+}
+
+impl MatMul {
+    pub fn new(device: &Device) -> Self {
+        let shader = device.create_shader_module(wgpu::ShaderModuleDescriptor {
+            label: Some("MatMul::new"),
+            source: wgpu::ShaderSource::Wgsl(include_str!("mat_mul.wgsl").into()),
+        });
+
+        let bind_group_layout = device.create_bind_group_layout(&wgpu::BindGroupLayoutDescriptor {
+            label: Some("MatMul::new"),
+            entries: &[
+                wgpu::BindGroupLayoutEntry {
+                    binding: 0,
+                    visibility: wgpu::ShaderStages::COMPUTE,
+                    ty: wgpu::BindingType::Buffer {
+                        ty: wgpu::BufferBindingType::Storage { read_only: true },
+                        has_dynamic_offset: false,
+                        min_binding_size: None,
+                    },
+                    count: None,
+                },
+                wgpu::BindGroupLayoutEntry {
+                    binding: 1,
+                    visibility: wgpu::ShaderStages::COMPUTE,
+                    ty: wgpu::BindingType::Buffer {
+                        ty: wgpu::BufferBindingType::Storage { read_only: true },
+                        has_dynamic_offset: false,
+                        min_binding_size: None,
+                    },
+                    count: None,
+                },
+                wgpu::BindGroupLayoutEntry {
+                    binding: 2,
+                    visibility: wgpu::ShaderStages::COMPUTE,
+                    ty: wgpu::BindingType::Buffer {
+                        ty: wgpu::BufferBindingType::Storage { read_only: false },
+                        has_dynamic_offset: false,
+                        min_binding_size: None,
+                    },
+                    count: None,
+                },
+                wgpu::BindGroupLayoutEntry {
+                    binding: 3,
+                    visibility: wgpu::ShaderStages::COMPUTE,
+                    ty: wgpu::BindingType::Buffer {
+                        ty: wgpu::BufferBindingType::Uniform,
+                        has_dynamic_offset: false,
+                        min_binding_size: None,
+                    },
+                    count: None,
+                },
+            ],
+        });
+
+        let pipeline_layout = device.create_pipeline_layout(&wgpu::PipelineLayoutDescriptor {
+            label: Some("MatMul::new"),
+            bind_group_layouts: &[&bind_group_layout],
+            immediate_size: 0,
+        });
+
+        MatMul {
+            mat_mul_nn: device.create_compute_pipeline(&wgpu::ComputePipelineDescriptor {
+                label: Some("mat_mul_nn"),
+                layout: Some(&pipeline_layout),
+                module: &shader,
+                entry_point: Some("mat_mul_nn"),
+                compilation_options: Default::default(),
+                cache: None,
+            }),
+            mat_mul_nt: device.create_compute_pipeline(&wgpu::ComputePipelineDescriptor {
+                label: Some("mat_mul_nt"),
+                layout: Some(&pipeline_layout),
+                module: &shader,
+                entry_point: Some("mat_mul_nt"),
+                compilation_options: Default::default(),
+                cache: None,
+            }),
+            mat_mul_tn: device.create_compute_pipeline(&wgpu::ComputePipelineDescriptor {
+                label: Some("mat_mul_tn"),
+                layout: Some(&pipeline_layout),
+                module: &shader,
+                entry_point: Some("mat_mul_tn"),
+                compilation_options: Default::default(),
+                cache: None,
+            }),
+            mat_mul_tt: device.create_compute_pipeline(&wgpu::ComputePipelineDescriptor {
+                label: Some("mat_mul_tt"),
+                layout: Some(&pipeline_layout),
+                module: &shader,
+                entry_point: Some("mat_mul_tt"),
+                compilation_options: Default::default(),
+                cache: None,
+            }),
+            bind_group_layout: bind_group_layout,
+        }
+    }
+}
+
+#[repr(C)]
+#[derive(Copy, Clone, bytemuck::Pod, bytemuck::Zeroable)]
+struct Params {
+    m: u32,
+    n: u32,
+    k: u32,
+    b: u32,
+}
+
+impl MatMul {
+    const WORKGROUP_SIZE: u32 = 256;
+
+    pub fn mat_mul_nn<'a>(
+        &'a self,
+        x: &GPUBuffer,
+        y: &GPUBuffer,
+        m: usize, n: usize, k: usize, b: usize,
+        result: &GPUBuffer,
+        device: &Device,
+    ) -> ComputeTask<'a> {
+        self.create_task(
+            "mat_mul_nn",
+            &self.mat_mul_nn,
+            x,
+            y,
+            m, n, k, b,
+            result,
+            device,
+        )
+    }
+
+    pub fn mat_mul_nt<'a>(
+        &'a self,
+        x: &GPUBuffer,
+        y: &GPUBuffer,
+        m: usize, n: usize, k: usize, b: usize,
+        result: &GPUBuffer,
+        device: &Device,
+    ) -> ComputeTask<'a> {
+        self.create_task(
+            "mat_mul_nt",
+            &self.mat_mul_nt,
+            x,
+            y,
+            m, n, k, b,
+            result,
+            device,
+        )
+    }
+
+    pub fn mat_mul_tn<'a>(
+        &'a self,
+        x: &GPUBuffer,
+        y: &GPUBuffer,
+        m: usize, n: usize, k: usize, b: usize,
+        result: &GPUBuffer,
+        device: &Device,
+    ) -> ComputeTask<'a> {
+        self.create_task(
+            "mat_mul_tn",
+            &self.mat_mul_tn,
+            x,
+            y,
+            m, n, k, b,
+            result,
+            device,
+        )
+    }
+
+    pub fn mat_mul_tt<'a>(
+        &'a self,
+        x: &GPUBuffer,
+        y: &GPUBuffer,
+        m: usize, n: usize, k: usize, b: usize,
+        result: &GPUBuffer,
+        device: &Device,
+    ) -> ComputeTask<'a> {
+        self.create_task(
+            "mat_mul_tt",
+            &self.mat_mul_tt,
+            x,
+            y,
+            m, n, k, b,
+            result,
+            device,
+        )
+    }
+    
+    fn create_task<'a>(
+        &'a self,
+        label: &'static str,
+        pipeline: &'a wgpu::ComputePipeline,
+        x: &GPUBuffer,
+        y: &GPUBuffer,
+        m: usize, n: usize, k: usize, b: usize,
+        result: &GPUBuffer,
+        device: &Device,
+    ) -> ComputeTask<'a> {
+        let params = Params { m: m as u32, n: n as u32, k: k as u32, b: b as u32 };
+        let params_buffer = device.create_buffer_init(&wgpu::util::BufferInitDescriptor {
+            label: Some(label),
+            contents: bytemuck::bytes_of(&params),
+            usage: wgpu::BufferUsages::UNIFORM,
+        });
+
+        let bind_group = device.create_bind_group(&wgpu::BindGroupDescriptor {
+            label: Some(label),
+            layout: &self.bind_group_layout,
+            entries: &[
+                wgpu::BindGroupEntry {
+                    binding: 0,
+                    resource: x.buffer.as_entire_binding(),
+                },
+                wgpu::BindGroupEntry {
+                    binding: 1,
+                    resource: y.buffer.as_entire_binding(),
+                },
+                wgpu::BindGroupEntry {
+                    binding: 2,
+                    resource: result.buffer.as_entire_binding(),
+                },
+                wgpu::BindGroupEntry {
+                    binding: 3,
+                    resource: params_buffer.as_entire_binding(),
+                },
+            ]
+        });
+
+        let workgroups = result.workgroup_count(MatMul::WORKGROUP_SIZE);
+
+        ComputeTask {
+            label: label,
+            pipeline: pipeline,
+            bind_group: bind_group,
+            workgroups: workgroups,
+        }
+    }
+}
