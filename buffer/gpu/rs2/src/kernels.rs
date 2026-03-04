bf2555f5598e1299c5507924f4284c077bb403ed
Wansuko-cmd <63241922+Wansuko-cmd@users.noreply.github.com>
2026-03-04 23:54:30 +0900

    [add] kernelsを追加

--- buffer/gpu/rs2/src/kernels.rs
+++ buffer/gpu/rs2/src/kernels.rs
@@ -1,4 +1,34 @@
+use wgpu::Device;
+
+use crate::kernels::{
+    elementwise::{compare::Compare, math::Math, operation::Operation},
+    linalg::{mat_mul::MatMul, transpose::Transpose},
+    reduction::collection::Collection,
+};
+
 pub mod elementwise;
 pub mod linalg;
 pub mod reduction;
 pub mod task;
+
+pub struct Kernels {
+    pub collection: Collection,
+    pub compare: Compare,
+    pub mat_mul: MatMul,
+    pub math: Math,
+    pub operation: Operation,
+    pub transpose: Transpose,
+}
+
+impl Kernels {
+    pub fn new(device: &Device) -> Self {
+        Kernels {
+            collection: Collection::new(device),
+            compare: Compare::new(device),
+            mat_mul: MatMul::new(device),
+            math: Math::new(device),
+            operation: Operation::new(device),
+            transpose: Transpose::new(device),
+        }
+    }
+}
