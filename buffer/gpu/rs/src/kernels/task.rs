40ef3af4c87bafa088658c5f18801f174665a539
Wansuko-cmd <63241922+Wansuko-cmd@users.noreply.github.com>
2026-03-07 00:37:39 +0900

    [move] ComputeTaskに実装を移動

--- buffer/gpu/rs/src/kernels/task.rs
+++ buffer/gpu/rs/src/kernels/task.rs
@@ -1,6 +1,24 @@
+pub trait Task {
+    fn recode(self, encoder: &mut wgpu::CommandEncoder);
+}
+
 pub struct ComputeTask<'a> {
     pub label: &'static str,
     pub pipeline: &'a wgpu::ComputePipeline,
     pub bind_group: wgpu::BindGroup,
     pub workgroups: [u32; 3],
+}
+
+impl Task for ComputeTask<'_> {
+    fn recode(self, encoder: &mut wgpu::CommandEncoder) {
+        let mut compute_pass = encoder.begin_compute_pass(&wgpu::ComputePassDescriptor {
+            label: Some(self.label),
+            timestamp_writes: None,
+        });
+
+        compute_pass.set_pipeline(self.pipeline);
+        compute_pass.set_bind_group(0, &self.bind_group, &[]);
+
+        compute_pass.dispatch_workgroups(self.workgroups[0], self.workgroups[1], self.workgroups[2]);
+    }
 }
