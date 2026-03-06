10bbf51db62d56edd45016e69c8504f7e2606a8a
Wansuko-cmd <63241922+Wansuko-cmd@users.noreply.github.com>
2026-03-07 01:11:04 +0900

    [move] read_allをopssに移動

--- buffer/gpu/rs/src/ops/buffer.rs
+++ buffer/gpu/rs/src/ops/buffer.rs
@@ -9,11 +9,20 @@ pub fn init(value: &[f32], runtime: &Runtime) -> GPUBuffer {
 }
 
 pub fn read_all(buffer: &GPUBuffer, runtime: &mut Runtime) -> Vec<f32> {
+    let map_buffer = GPUBuffer::create_map_read(buffer.count(), &runtime.device);
+    runtime.dispatch(
+        CopyTask {
+            src: buffer,
+            src_offset: 0,
+            dest: &map_buffer,
+            dest_offset: 0,
+            size: buffer.buffer.size(),
+        },
+    );
     runtime.submit();
 
     let mut dest = vec![0.0f32; buffer.count()];
-    buffer.read_all(&mut dest, &runtime.device, &runtime.queue);
-
+    map_buffer.copy_into(&mut dest, &runtime.device);
     return dest;
 }
 
@@ -56,6 +65,8 @@ pub fn copy_into(src: &GPUBuffer, dest: &GPUBuffer, dest_offset: usize, runtime:
 }
 
 pub fn content_equals(x: &GPUBuffer, y: &GPUBuffer, runtime: &mut Runtime) -> bool {
-    runtime.submit();
-    x.content_equals(y, &runtime.device, &runtime.queue)
+    if x == y { return true; }
+    if x.count() != y.count() { return false; }
+
+    return read_all(x, runtime) == read_all(y, runtime);
 }
--- buffer/gpu/rs/src/resource/buffer.rs
+++ buffer/gpu/rs/src/resource/buffer.rs
@@ -1,6 +1,7 @@
+use std::sync::mpsc;
+
 use wgpu::{Device, Queue, util::DeviceExt};
 use bytemuck;
-use std::sync::mpsc;
 
 pub struct GPUBuffer {
     pub buffer: wgpu::Buffer,
@@ -12,7 +13,7 @@ impl GPUBuffer {
     pub fn create(size: usize, device: &Device) -> GPUBuffer {
         let byte_size = (size * GPUBuffer::SIZE_BYTES) as u64;
         let buffer = device.create_buffer(&wgpu::BufferDescriptor {
-            label: Some("GPUBuffer::create_buffer"),
+            label: Some("GPUBuffer::create"),
             size: byte_size,
             usage: wgpu::BufferUsages::STORAGE | wgpu::BufferUsages::COPY_SRC | wgpu::BufferUsages::COPY_DST,
             mapped_at_creation: false,
@@ -20,59 +21,44 @@ impl GPUBuffer {
         GPUBuffer { buffer }
     }
 
+    pub fn create_map_read(size: usize, device: &Device) -> GPUBuffer {
+        let byte_size = (size * GPUBuffer::SIZE_BYTES) as u64;
+        let buffer = device.create_buffer(&wgpu::BufferDescriptor {
+            label: Some("GPUBufer::create_map_read"),
+            size: byte_size,
+            usage: wgpu::BufferUsages::MAP_READ | wgpu::BufferUsages::COPY_DST,
+            mapped_at_creation: false,
+        });
+        GPUBuffer { buffer }
+    }
+
     pub fn init(value: &[f32], device: &Device) -> GPUBuffer {
         let buffer = device.create_buffer_init(&wgpu::util::BufferInitDescriptor {
-            label: Some("GPUBuffer::init_buffer"),
+            label: Some("GPUBuffer::init"),
             contents: bytemuck::cast_slice(&value),
             usage: wgpu::BufferUsages::STORAGE | wgpu::BufferUsages::COPY_SRC | wgpu::BufferUsages::COPY_DST,
         });
         GPUBuffer { buffer }
     }
-}
 
-impl GPUBuffer {
-    pub fn read_all(self: &Self, dest: &mut [f32], device: &Device, queue: &Queue) {
-        let size = self.buffer.size();
-        // GPU上の共有バッファを用意
-        let read_buffer = device.create_buffer(&wgpu::BufferDescriptor {
-            label: Some("GPUBufer::read_all"),
-            size: size,
-            usage: wgpu::BufferUsages::MAP_READ | wgpu::BufferUsages::COPY_DST,
-            mapped_at_creation: false,
-        });
+    pub fn copy_into(self, dest: &mut Vec<f32>, device: &Device) {
+        let map_buffer_slice = self.buffer.slice(..);
 
-        // 対象バッファの値を共有バッファにコピー
-        let mut encoder = device.create_command_encoder(&wgpu::CommandEncoderDescriptor{
-            label: Some("GPUBuffer:read_all")
-        });
-        encoder.copy_buffer_to_buffer(&self.buffer, 0, &read_buffer, 0, size);
-        queue.submit(Some(encoder.finish()));
+        let (sender, receiver) = mpsc::sync_channel(1);
+        map_buffer_slice.map_async(wgpu::MapMode::Read, move |v| sender.send(v).unwrap());
+        let _ = device.poll(wgpu::PollType::wait_indefinitely());
+        receiver.recv().unwrap().unwrap();
 
-        {
-            let read_buffer_slice = read_buffer.slice(..);
-
-            // コピー待機処理
-            let (sender, receiver) = mpsc::sync_channel(1);
-            read_buffer_slice.map_async(wgpu::MapMode::Read, move |v| sender.send(v).unwrap());
-            let _ = device.poll(wgpu::PollType::wait_indefinitely());
-            receiver.recv().unwrap().unwrap();
-
-            let data = read_buffer_slice.get_mapped_range();
-            dest.copy_from_slice( bytemuck::cast_slice(&data));
-        }
-
-        read_buffer.unmap();
+        let data = map_buffer_slice.get_mapped_range();
+        dest.copy_from_slice( bytemuck::cast_slice(&data));
     }
-}
 
-impl GPUBuffer {
+
     pub fn write(self: &Self, index: usize, value: f32, queue: &Queue) {
         let offset = (index * GPUBuffer::SIZE_BYTES) as u64;
         queue.write_buffer(&self.buffer, offset, bytemuck::bytes_of(&value));
     }
-}
 
-impl GPUBuffer {
     pub fn count(self: &Self) -> usize {
         let byte_size = self.buffer.size() as usize;
         byte_size / GPUBuffer::SIZE_BYTES
@@ -82,17 +68,10 @@ impl GPUBuffer {
         let count = self.count() as u32;
         [(count + workgroup_size - 1) / workgroup_size, 1u32, 1u32]
     }
+}
 
-    pub fn content_equals(&self, other: &GPUBuffer, device: &Device, queue: &Queue) -> bool {
-        if self.buffer == other.buffer { return true; }
-        if self.buffer.size() != other.buffer.size() { return false; }
-
-        let mut self_data = vec![0.0f32; self.count()];
-        let mut other_data = vec![0.0f32; other.count()];
-        
-        self.read_all(&mut self_data, device, queue);
-        other.read_all(&mut other_data, device, queue);
-
-        return self_data == other_data;
+impl PartialEq for GPUBuffer {
+    fn eq(&self, other: &Self) -> bool {
+        self.buffer == other.buffer
     }
 }
