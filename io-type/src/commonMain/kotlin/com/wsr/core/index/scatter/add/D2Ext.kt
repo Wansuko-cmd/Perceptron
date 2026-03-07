355d8575d712ea96baff706a274b32490e2b2167
Wansuko-cmd <63241922+Wansuko-cmd@users.noreply.github.com>
2026-03-08 02:21:45 +0900

    [add] IOTypeに実装を追加

---
+++ io-type/src/commonMain/kotlin/com/wsr/core/index/scatter/add/D2Ext.kt
@@ -0,0 +1,9 @@
+package com.wsr.core.index.scatter.add
+
+import com.wsr.Backend
+import com.wsr.core.IOType
+
+fun IOType.D2.scatterAdd(other: IOType.D1, n: Int): IOType.D2 {
+    val result = Backend.scatterAdd(x = value, y = other.value, i = 1, j = n, k = j)
+    return IOType.D2(shape = listOf(n, j), value = result)
+}
