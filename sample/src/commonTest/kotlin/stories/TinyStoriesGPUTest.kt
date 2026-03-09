354ef18eebed3196505a84c2a3f54c0533250051
Wansuko-cmd <63241922+Wansuko-cmd@users.noreply.github.com>
2026-03-09 22:00:02 +0900

    [add] TinyStoriesのテストのバリエーションを追加

---
+++ sample/src/commonTest/kotlin/stories/TinyStoriesGPUTest.kt
@@ -0,0 +1,16 @@
+@file:Suppress("NonAsciiCharacters", "RemoveRedundantBackticks")
+
+package stories
+
+import com.wsr.Backend
+import com.wsr.gpu.gpu
+import kotlin.test.Test
+
+
+class TinyStoriesGPUTest {
+    @Test
+    fun run() {
+        Backend.set(gpu)
+        evaluateModel()
+    }
+}
