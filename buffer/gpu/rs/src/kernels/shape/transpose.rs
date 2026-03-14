a7f8bd5b1350ad9f357b7379b9d36aebfedef8a2
Wansuko-cmd <63241922+Wansuko-cmd@users.noreply.github.com>
2026-03-15 00:34:56 +0900

    [fix] Kernel側のtranspose_d2の呼び出しを修正

--- buffer/gpu/rs/src/kernels/shape/transpose.rs
+++ buffer/gpu/rs/src/kernels/shape/transpose.rs
@@ -4,13 +4,18 @@ use crate::{kernels::task::ComputeTask, resource::buffer::GPUBuffer};
 
 pub struct Transpose {
     device: Device,
+    transpose_d2: wgpu::ComputePipeline,
     transpose_d4: wgpu::ComputePipeline,
     bind_group_layout: wgpu::BindGroupLayout,
 }
 
 impl Transpose {
     pub fn new(device: &Device) -> Self {
-        let shader = device.create_shader_module(wgpu::ShaderModuleDescriptor {
+        let shader_d2 = device.create_shader_module(wgpu::ShaderModuleDescriptor {
+            label: Some("Transpose::new"),
+            source: wgpu::ShaderSource::Wgsl(include_str!("transpose_d2.wgsl").into()),
+        });
+        let shader_d4 = device.create_shader_module(wgpu::ShaderModuleDescriptor {
             label: Some("Transpose::new"),
             source: wgpu::ShaderSource::Wgsl(include_str!("transpose_d4.wgsl").into()),
         });
@@ -59,10 +64,18 @@ impl Transpose {
 
         Transpose {
             device: device.clone(),
+            transpose_d2: device.create_compute_pipeline(&wgpu::ComputePipelineDescriptor {
+                label: Some("transpose_d2"),
+                layout: Some(&pipeline_layout),
+                module: &shader_d2,
+                entry_point: Some("transpose_d2"),
+                compilation_options: Default::default(),
+                cache: None,
+            }),
             transpose_d4: device.create_compute_pipeline(&wgpu::ComputePipelineDescriptor {
                 label: Some("transpose_d4"),
                 layout: Some(&pipeline_layout),
-                module: &shader,
+                module: &shader_d4,
                 entry_point: Some("transpose_d4"),
                 compilation_options: Default::default(),
                 cache: None,
@@ -72,25 +85,76 @@ impl Transpose {
     }
 }
 
+#[repr(C)]
+#[derive(Copy, Clone, bytemuck::Pod, bytemuck::Zeroable)]
+struct D2Params {
+    oi: u32,
+    oj: u32,
+    _pad1: u32,
+    _pad2: u32,
+}
+
+impl Transpose {
+    pub fn transpose_d2<'a>(
+        &'a self,
+        x: &GPUBuffer,
+        xi: usize, xj: usize,
+        result: &GPUBuffer,
+    ) -> ComputeTask<'a> {
+        let label = "transpose_d2";
+        let device = &self.device;
+        let params = D2Params {
+            oi: xi as u32,
+            oj: xj as u32,
+            _pad1: 0u32,
+            _pad2: 0u32,
+        };
+
+        let params_buffer = device.create_buffer_init(&wgpu::util::BufferInitDescriptor {
+            label: Some(label),
+            contents: bytemuck::bytes_of(&params),
+            usage: wgpu::BufferUsages::UNIFORM,
+        });
+
+        let bind_group = device.create_bind_group(&wgpu::BindGroupDescriptor {
+            label: Some(label),
+            layout: &self.bind_group_layout,
+            entries: &[
+                wgpu::BindGroupEntry {
+                    binding: 0,
+                    resource: x.buffer.as_entire_binding(),
+                },
+                wgpu::BindGroupEntry {
+                    binding: 1,
+                    resource: result.buffer.as_entire_binding(),
+                },
+                wgpu::BindGroupEntry {
+                    binding: 2,
+                    resource: params_buffer.as_entire_binding(),
+                },
+            ]
+        });
+
+        let workgroups = [((xj + 15) / 16) as u32, ((xi + 15) / 16) as u32, 1];
+
+        ComputeTask {
+            label: label,
+            pipeline: &self.transpose_d2,
+            bind_group: bind_group,
+            workgroups: workgroups,
+        }
+    }
+}
 
 #[repr(C)]
 #[derive(Copy, Clone, bytemuck::Pod, bytemuck::Zeroable)]
-struct Params {
+struct D4Params {
     new_stride: [u32; 4],
     permuted_stride: [u32; 4],
 }
 
 impl Transpose {
     const WORKGROUP_SIZE: u32 = 256;
-
-    pub fn transpose_d2<'a>(&'a self, x: &GPUBuffer, xi: usize, xj: usize, result: &GPUBuffer) -> ComputeTask<'a> {
-        self.create_task(
-            "transpose_d2",
-            x, 1, 1,  xi, xj,
-            0, 1,  3, 2,
-            result,
-        )
-    }
 
     pub fn transpose_d3<'a>(
         &'a self,
@@ -137,7 +201,7 @@ impl Transpose {
         let old_stride = [old_shape[1] * old_shape[2] * old_shape[3], old_shape[2] * old_shape[3], old_shape[3], 1];
         let new_stride = [new_shape[1] * new_shape[2] * new_shape[3], new_shape[2] * new_shape[3], new_shape[3], 1];
         let permuted_stride = [old_stride[axis_i], old_stride[axis_j], old_stride[axis_k], old_stride[axis_l]];
-        let params = Params {
+        let params = D4Params {
             new_stride: new_stride.map(|v| v as u32),
             permuted_stride: permuted_stride.map(|v| v as u32),
         };
