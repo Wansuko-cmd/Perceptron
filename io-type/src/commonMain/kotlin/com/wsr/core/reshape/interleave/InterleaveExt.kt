54c37e507ff56fc00c7a0e8b7b63f3ca0461fe1c
Wansuko-cmd <63241922+Wansuko-cmd@users.noreply.github.com>
2026-03-17 01:45:34 +0900

    [add] interleaveを追加

---
+++ io-type/src/commonMain/kotlin/com/wsr/core/reshape/interleave/InterleaveExt.kt
@@ -0,0 +1,60 @@
+package com.wsr.core.reshape.interleave
+
+import com.wsr.base.data.DataBuffer
+import com.wsr.base.data.size
+import com.wsr.core.IOType
+import com.wsr.create
+import kotlin.math.min
+
+fun IOType.D1.interleave(other: IOType.D1): IOType.D1 {
+    check(size == other.size)
+
+    val result = DataBuffer.create(size + other.size)
+    value.copyInto(dest = result, destIndices = result.indices step 2)
+    other.value.copyInto(dest = result, destIndices = 1 until result.size step 2)
+    
+    return IOType.D1(result)
+}
+
+fun IOType.D2.interleave(other: IOType.D2, axis: Int): IOType.D2 {
+    check(i == other.i && j == other.j)
+    val result = DataBuffer.create(size + other.size)
+    return when (axis) {
+        0 -> {
+            repeat(i) {
+                val start = it * j
+                val end = start + j
+                value
+                    .slice(start until end)
+                    .run {
+                        val destStart = it * j * 2
+                        val destEnd = destStart + j
+                        copyInto(result, destStart until destEnd)
+                    }
+                other.value
+                    .slice(start until end)
+                    .run {
+                        val destStart = it * j * 2 + j
+                        val destEnd = destStart + j
+                        copyInto(result, destStart until destEnd)
+                    }
+            }
+            IOType.D2(shape = listOf(i * 2, j), value = result)
+        }
+        else -> {
+            repeat(i) {
+                val start = it * j
+                val end = start + j
+                val destStart = it * j * 2
+                val destEnd = destStart + j * 2
+                value
+                    .slice(start until end)
+                    .copyInto(result, destStart until destEnd step 2)
+                other.value
+                    .slice(start until end)
+                    .copyInto(result, destStart + 1 until destEnd step 2)
+            }
+            IOType.D2(shape = listOf(i, j * 2), value = result)
+        }
+    }
+}
