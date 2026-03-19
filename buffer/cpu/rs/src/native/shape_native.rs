4710928283ef8b98c8fc28ded10a31b3ce245b28
Wansuko-cmd <63241922+Wansuko-cmd@users.noreply.github.com>
2026-03-19 23:13:52 +0900

    [add] Native側との連結を追加

--- buffer/cpu/rs/src/native/shape_native.rs
+++ buffer/cpu/rs/src/native/shape_native.rs
@@ -70,3 +70,40 @@ pub extern "C" fn com_wsr_cpu_slice_d3(
     let result = unsafe { std::slice::from_raw_parts_mut(result, result_size as usize) };
     shape::slice_d3(x, xi as usize, xj as usize, xk as usize, axis as usize, start as usize, end as usize, step as isize, result);
 }
+
+#[unsafe(no_mangle)]
+pub extern "C" fn com_wsr_cpu_copy_into_d1(
+    x: *const f32, x_size: i32,
+    result: *mut f32, result_size: i32,
+    start: i32, end: i32, step: i32,
+) {
+    let x = unsafe { std::slice::from_raw_parts(x, x_size as usize) };
+    let result = unsafe { std::slice::from_raw_parts_mut(result, result_size as usize) };
+    shape::copy_into_d1(x, result, start as usize, end as usize, step as isize);
+}
+
+#[unsafe(no_mangle)]
+pub extern "C" fn com_wsr_cpu_copy_into_d2(
+    x: *const f32, x_size: i32,
+    result: *mut f32,
+    ri: i32, rj: i32, axis: i32,
+    start: i32, end: i32, step: i32,
+) {
+    let x = unsafe { std::slice::from_raw_parts(x, x_size as usize) };
+    let result_size = ri * rj;
+    let result = unsafe { std::slice::from_raw_parts_mut(result, result_size as usize) };
+    shape::copy_into_d2(x, result, ri as usize, rj as usize, axis as usize, start as usize, end as usize, step as isize);
+}
+
+#[unsafe(no_mangle)]
+pub extern "C" fn com_wsr_cpu_copy_into_d3(
+    x: *const f32, x_size: i32,
+    result: *mut f32,
+    ri: i32, rj: i32, rk: i32, axis: i32,
+    start: i32, end: i32, step: i32,
+) {
+    let x = unsafe { std::slice::from_raw_parts(x, x_size as usize) };
+    let result_size = ri * rj * rk;
+    let result = unsafe { std::slice::from_raw_parts_mut(result, result_size as usize) };
+    shape::copy_into_d3(x, result, ri as usize, rj as usize, rk as usize, axis as usize, start as usize, end as usize, step as isize);
+}
