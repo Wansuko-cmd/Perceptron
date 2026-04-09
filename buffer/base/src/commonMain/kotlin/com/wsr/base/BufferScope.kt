e8e0cdb0a4f3575785b2808d19483b2542bcbfca
Wansuko-cmd <63241922+Wansuko-cmd@users.noreply.github.com>
2026-04-09 23:59:03 +0900

    [add] GlobalとLocalを追加

--- buffer/base/src/commonMain/kotlin/com/wsr/base/BufferScope.kt
+++ buffer/base/src/commonMain/kotlin/com/wsr/base/BufferScope.kt
@@ -2,30 +2,49 @@ package com.wsr.base
 
 import com.wsr.base.data.DataBuffer
 
-class BufferScope(val buffers: ArrayList<DataBuffer> = arrayListOf()) : AutoCloseable {
-    fun register(buffer: DataBuffer) {
-        buffers.add(buffer)
+sealed interface BufferScope {
+    fun register(buffer: DataBuffer)
+
+    fun remove(buffer: DataBuffer)
+
+    fun close()
+
+    object Global : BufferScope {
+        override fun register(buffer: DataBuffer) {
+        }
+
+        override fun remove(buffer: DataBuffer) {
+        }
+
+        override fun close() {
+        }
     }
 
-    fun remove(buffer: DataBuffer) {
-        buffers.add(buffer)
-    }
+    class Local(val buffers: ArrayList<DataBuffer> = arrayListOf()) : BufferScope, AutoCloseable {
+        override fun register(buffer: DataBuffer) {
+            buffers.add(buffer)
+        }
 
-    override fun close() {
-        for (i in buffers.indices) buffers[i].release()
+        override fun remove(buffer: DataBuffer) {
+            buffers.add(buffer)
+        }
+
+        override fun close() {
+            for (i in buffers.indices) buffers[i].release()
+        }
     }
 
     companion object {
         inline fun launch(block: BufferScope.() -> Unit) {
-            BufferScope().use { scope -> scope.block() }
+            Local().use { scope -> scope.block() }
         }
 
-        inline fun launch(block: BufferScope.() -> DataBuffer): DataBuffer = BufferScope()
+        inline fun launch(block: BufferScope.() -> DataBuffer): DataBuffer = Local()
             .use { scope ->
                 scope.block().also { scope.remove(it) }
             }
 
-        inline fun BufferScope.launch(block: BufferScope.() -> DataBuffer): DataBuffer = BufferScope()
+        inline fun BufferScope.launch(block: BufferScope.() -> DataBuffer): DataBuffer = Local()
             .use { scope ->
                 scope.block()
                     .also { scope.remove(it) }
