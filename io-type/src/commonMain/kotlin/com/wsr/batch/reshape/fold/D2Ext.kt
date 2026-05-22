5a63985e31349eac4876b165a962e796c78ec4e3
Wansuko-cmd <63241922+Wansuko-cmd@users.noreply.github.com>
2026-05-23 00:27:42 +0900

    [add] IOTypeに呼び出し実装を追加

--- io-type/src/commonMain/kotlin/com/wsr/batch/reshape/fold/D2Ext.kt
+++ io-type/src/commonMain/kotlin/com/wsr/batch/reshape/fold/D2Ext.kt
@@ -1,11 +1,53 @@
 package com.wsr.batch.reshape.fold
 
+import com.wsr.Backend
 import com.wsr.batch.Batch
 import com.wsr.batch.get
 import com.wsr.core.IOType
 import com.wsr.core.d2
 import com.wsr.core.get
 import com.wsr.core.set
+
+fun Batch<IOType.D2>.unfold2(windowSize: Int, stride: Int, padding: Int): Batch<IOType.D3> {
+    val result = Backend.unfold(
+        x = value,
+        xi = shape[0],
+        xj = shape[1],
+        b = size,
+        window = windowSize,
+        stride = stride,
+        padding = padding,
+    )
+    return Batch(
+        value = result,
+        size = size,
+        shape = listOf(
+            shape[0],
+            (shape[1] - windowSize + padding * 2) / stride + 1,
+            windowSize,
+        ),
+    )
+}
+
+fun Batch<IOType.D3>.fold2(stride: Int, padding: Int): Batch<IOType.D2> {
+    val result = Backend.fold(
+        x = value,
+        xi = shape[0],
+        xj = shape[1],
+        xk = shape[2],
+        b = size,
+        stride = stride,
+        padding = padding,
+    )
+    return Batch(
+        value = result,
+        size = size,
+        shape = listOf(
+            shape[0],
+            shape[2] + (shape[1] - 1) * stride - padding * 2,
+        )
+    )
+}
 
 /**
  * Unfold: Batch<IOType.D2>を列形式に展開 (im2col)
