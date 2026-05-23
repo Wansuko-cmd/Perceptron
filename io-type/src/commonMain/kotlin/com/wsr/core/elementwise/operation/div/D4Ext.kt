2d2d3894de4469141295af90ad66d3a2fccb9156
Wansuko-cmd <63241922+Wansuko-cmd@users.noreply.github.com>
2026-05-23 11:56:49 +0900

    [add] D4-D0の演算を追加

--- io-type/src/commonMain/kotlin/com/wsr/core/elementwise/operation/div/D4Ext.kt
+++ io-type/src/commonMain/kotlin/com/wsr/core/elementwise/operation/div/D4Ext.kt
@@ -2,9 +2,62 @@
 
 import com.wsr.Backend
 import com.wsr.core.IOType
+import com.wsr.core.get
 
 operator fun IOType.D4.div(other: Float): IOType.D4 {
     val result = Backend.div(x = value, y = other)
+    return IOType.D4(shape = shape, value = result)
+}
+
+operator fun IOType.D4.div(other: IOType.D0): IOType.D4 {
+    val result = Backend.div(x = value, y = other.get())
+    return IOType.D4(shape = shape, value = result)
+}
+
+fun IOType.D4.div(other: IOType.D1, axis: Int): IOType.D4 {
+    val result = Backend.div(
+        x = value,
+        xi = i,
+        xj = j,
+        xk = k,
+        xl = l,
+        y = other.value,
+        axis = axis,
+    )
+    return IOType.D4(shape = shape, value = result)
+}
+
+fun IOType.D4.div(other: IOType.D2, axis1: Int, axis2: Int): IOType.D4 {
+    val result = Backend.div(
+        x = value,
+        xi = i,
+        xj = j,
+        xk = k,
+        xl = l,
+        y = other.value,
+        yi = other.i,
+        yj = other.j,
+        axis1 = axis1,
+        axis2 = axis2,
+    )
+    return IOType.D4(shape = shape, value = result)
+}
+
+fun IOType.D4.div(other: IOType.D3, axis1: Int, axis2: Int, axis3: Int): IOType.D4 {
+    val result = Backend.div(
+        x = value,
+        xi = i,
+        xj = j,
+        xk = k,
+        xl = l,
+        y = other.value,
+        yi = other.i,
+        yj = other.j,
+        yk = other.k,
+        axis1 = axis1,
+        axis2 = axis2,
+        axis3 = axis3,
+    )
     return IOType.D4(shape = shape, value = result)
 }
 
