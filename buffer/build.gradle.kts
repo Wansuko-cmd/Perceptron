a5e95e766ab866315a82fc5b5ba6cc8e385fae2f
Wansuko-cmd <63241922+Wansuko-cmd@users.noreply.github.com>
2026-05-29 01:01:50 +0900

    [add] Androidテストを追加

--- buffer/build.gradle.kts
+++ buffer/build.gradle.kts
@@ -2,11 +2,19 @@ import org.gradle.nativeplatform.platform.internal.DefaultNativePlatform
 
 plugins {
     kotlin("multiplatform")
+    alias(libs.plugins.android.kmp.library)
 }
 
 kotlin {
     applyDefaultHierarchyTemplate()
     jvm()
+
+    android {
+        compileSdk = 35
+        minSdk = 28
+        namespace = "com.wsr.buffer"
+        withHostTestBuilder { sourceSetTreeName = "test" }
+    }
 
     val hostOs = DefaultNativePlatform.getCurrentOperatingSystem()
     val hostArch = DefaultNativePlatform.getCurrentArchitecture()
