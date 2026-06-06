9774e1a4c49386e2207fc3371ddbb36d0e3b2239
Wansuko-cmd <63241922+Wansuko-cmd@users.noreply.github.com>
2026-06-07 00:42:53 +0900

    [add] opsに定義を追加

--- buffer/gpu/rs/src/ops/shape.rs
+++ buffer/gpu/rs/src/ops/shape.rs
@@ -118,3 +118,8 @@ pub fn flip_d3(x: &GPUBuffer, xi: usize, xj: usize, xk: usize, axis: usize, resu
     };
     runtime.dispatch(task);
 }
+
+pub fn unfold_d1(x: &GPUBuffer, xi: usize, xj: usize, b: usize, window: usize, stride: usize, padding: usize, result: &GPUBuffer, runtime: &mut Runtime) {
+    let task = runtime.kernels.shape.unfold_d1(x, xi, xj, b, window, stride, padding, result);
+    runtime.dispatch(task);
+}
