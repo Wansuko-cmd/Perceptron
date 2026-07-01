b150e9e8834b597c6609a118b86fd0e8151cb6f4
bugdog24 <63241922+Wansuko-cmd@users.noreply.github.com>
2026-07-02 00:17:39 +0900

    Fix/softmax mask (#252)
    * [delete] JBuffer系を削除
    
    * [fix] softmaxにてlabelのフラグが経っていない場合をmaskと判定
    
    * [fix] 利用箇所を修正
    
    * [fix] docs系を修正
    
    * [format] ktlintを実行

--- buffer/gpu/src/jvmAndroidMain/kotlin/com/wsr/knist/gpu/GPUJvmBuffer.kt
+++ buffer/gpu/src/jvmAndroidMain/kotlin/com/wsr/knist/gpu/GPUJvmBuffer.kt
@@ -13,47 +13,41 @@ import kotlin.concurrent.atomics.minusAssign
 private val allocateSize = AtomicInt(0)
 private const val MAX_ALLOCATE_SIZE = 1_500_000_000
 
-internal fun DataBuffer.toGPUBuffer(runtime: Long, native: JBuffer): GPUJvmBuffer = when (this) {
+internal fun DataBuffer.toGPUBuffer(runtime: Long): GPUJvmBuffer = when (this) {
     is GPUJvmBuffer -> this
-    else -> GPUJvmBuffer.create(this.toFloatArray(), runtime, native)
+    else -> GPUJvmBuffer.create(this.toFloatArray(), runtime)
 }
 
-class GPUJvmBuffer(
-    override val size: Int,
-    internal val ptr: Long,
-    private val runtime: Long,
-    private val native: JBuffer,
-) : DataBuffer {
+class GPUJvmBuffer(override val size: Int, internal val ptr: Long, private val runtime: Long) : DataBuffer {
     private val isReleased = AtomicBoolean(false)
 
     init {
         val size = size
         val ptr = ptr
         val runtime = runtime
-        val native = native
         val isReleased = isReleased
         if (allocateSize.addAndFetch(size) >= MAX_ALLOCATE_SIZE) System.gc()
         cleaner.register(this) {
             if (!isReleased.getAndSet(true)) {
-                native.release(ptr, runtime)
+                JBuffer.release(ptr, runtime)
                 allocateSize.minusAssign(size)
             }
         }
     }
 
-    override fun get(i: Int): Float = native.readAll(ptr, runtime)[i]
+    override fun get(i: Int): Float = JBuffer.readAll(ptr, runtime)[i]
 
     override fun set(i: Int, value: Float) {
-        native.write(ptr, i, value, runtime)
+        JBuffer.write(ptr, i, value, runtime)
     }
 
-    override fun toFloatArray(): FloatArray = native.readAll(ptr, runtime)
+    override fun toFloatArray(): FloatArray = JBuffer.readAll(ptr, runtime)
 
     override fun toString(): String = toFloatArray().joinToString(prefix = "GPUJvmBuffer[", postfix = "]")
 
     override fun release() {
         if (!isReleased.getAndSet(true)) {
-            native.release(ptr, runtime)
+            JBuffer.release(ptr, runtime)
             allocateSize.minusAssign(size)
         }
     }
@@ -61,27 +55,25 @@ class GPUJvmBuffer(
     companion object {
         private val cleaner = Cleaner.create()
 
-        fun create(size: Int, runtime: Long, native: JBuffer): GPUJvmBuffer {
-            val ptr = native.allocate(size, runtime)
-            return GPUJvmBuffer(size = size, ptr = ptr, runtime = runtime, native = native)
+        fun create(size: Int, runtime: Long): GPUJvmBuffer {
+            val ptr = JBuffer.allocate(size, runtime)
+            return GPUJvmBuffer(size = size, ptr = ptr, runtime = runtime)
         }
 
-        fun create(value: FloatArray, runtime: Long, native: JBuffer): GPUJvmBuffer {
-            val ptr = native.init(value, runtime)
-            return GPUJvmBuffer(size = value.size, ptr = ptr, runtime = runtime, native = native)
+        fun create(value: FloatArray, runtime: Long): GPUJvmBuffer {
+            val ptr = JBuffer.init(value, runtime)
+            return GPUJvmBuffer(size = value.size, ptr = ptr, runtime = runtime)
         }
 
-        fun createGenerator(runtime: Long, native: JBuffer) = object : IDataBufferGenerator {
+        fun createGenerator(runtime: Long) = object : IDataBufferGenerator {
             override fun create(size: Int): DataBuffer = create(
                 size = size,
                 runtime = runtime,
-                native = native,
             )
 
             override fun create(value: FloatArray): DataBuffer = create(
                 value = value,
                 runtime = runtime,
-                native = native,
             )
         }
     }
