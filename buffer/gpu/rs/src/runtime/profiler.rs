f816f56169d399242e1de3370acc1250942fe510
Wansuko-cmd <63241922+Wansuko-cmd@users.noreply.github.com>
2026-07-06 22:08:36 +0900

    [add] GPUProfiler::startを追加

--- buffer/gpu/rs/src/runtime/profiler.rs
+++ buffer/gpu/rs/src/runtime/profiler.rs
@@ -1,5 +1,5 @@
 use std::cell::Cell;
-use std::sync::atomic::{AtomicU64, Ordering};
+use std::sync::atomic::{AtomicU32, AtomicU64, Ordering};
 use std::time::Instant;
 
 thread_local! {
@@ -40,5 +40,49 @@ impl Drop for OpTimer<'_> {
             let time = t0.elapsed().as_nanos() as u64;
             self.profiler.acc.fetch_add(time, Ordering::Relaxed);
         }
+    }
+}
+
+pub struct GpuProfiler {
+    enabled: bool,
+    query_set: Option<wgpu::QuerySet>,
+    query_set_index: AtomicU32,
+}
+
+impl GpuProfiler {
+    const TIMESTAMP_CAPACITY: u32 = 4096;
+
+    pub fn new(device: &wgpu::Device, enabled: bool) -> Self {
+        if !enabled {
+            return GpuProfiler { enabled, query_set: None, query_set_index: AtomicU32::new(0) };
+        }
+        let query_set = device.create_query_set(&wgpu::QuerySetDescriptor {
+            label: Some("Profiler::new"),
+            ty: wgpu::QueryType::Timestamp,
+            count: Self::TIMESTAMP_CAPACITY,
+        });
+        GpuProfiler {
+            enabled: enabled,
+            query_set: Some(query_set),
+            query_set_index: AtomicU32::new(0),
+        }
+    }
+
+    pub fn start(&self) -> Option<wgpu::ComputePassTimestampWrites<'_>> {
+        if !self.enabled {
+            return None;
+        }
+        let begin = self.query_set_index.fetch_add(2, Ordering::Relaxed);
+        let end = begin + 1;
+        if end >= Self::TIMESTAMP_CAPACITY {
+            return None;
+        }
+
+        let query_set = self.query_set.as_ref().unwrap();
+        Some(wgpu::ComputePassTimestampWrites {
+            query_set,
+            beginning_of_pass_write_index: Some(begin),
+            end_of_pass_write_index: Some(end),
+        })
     }
 }
