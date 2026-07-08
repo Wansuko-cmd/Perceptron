f1124e83c5f3ecb2d88bd577c51d16b18fbc5084
Wansuko-cmd <63241922+Wansuko-cmd@users.noreply.github.com>
2026-07-09 00:28:47 +0900

    [delete] MinMaxNormからweightを削除

--- network/src/commonMain/kotlin/com/wsr/knist/network/process/compute/norm/minmax/MinMaxNormD1.kt
+++ network/src/commonMain/kotlin/com/wsr/knist/network/process/compute/norm/minmax/MinMaxNormD1.kt
@@ -4,9 +4,6 @@ import com.wsr.knist.batch.Batch
 import com.wsr.knist.core.IOScope
 import com.wsr.knist.core.IOType
 import com.wsr.knist.network.NetworkBuilder
-import com.wsr.knist.network.initializer.Fixed
-import com.wsr.knist.network.initializer.WeightInitializer
-import com.wsr.knist.network.optimizer.Optimizer
 import com.wsr.knist.network.process.Context
 import com.wsr.knist.network.process.compute.Compute
 import kotlin.uuid.Uuid
@@ -15,15 +12,13 @@ import kotlinx.serialization.Serializable
 @Serializable
 class MinMaxNormD1 internal constructor(
     override val outputI: Int,
-    private val optimizer: Optimizer.D1,
-    private var weight: IOType.D1.Global,
     override val id: String = Uuid.random().toString(),
 ) : Compute.D1() {
     override fun IOScope.expect(input: Batch<IOType.D1>, context: Context): Batch<IOType.D1> {
         val min = input.min()
         val max = input.max()
         val denominator = max - min
-        return weight * (input - min) / denominator
+        return (input - min) / denominator
     }
 
     override fun IOScope.train(
@@ -37,25 +32,14 @@ class MinMaxNormD1 internal constructor(
         val numerator = input - min
         val denominator = 1f / (max - min)
 
-        val mean = denominator * numerator
-        val output = weight * mean
-
+        val output = denominator * numerator
         val delta = calcDelta(output)
 
-        val dOutput = delta * weight
-
-        weight = optimizer.adapt(
-            weight = weight,
-            dw = mean * delta,
-        ).toGlobal()
-
-        numerator.inner(other = dOutput)
-
         // 分母側(dy/d[max(x) - min(x)])
-        val dDenominator = denominator.pow(2) * numerator.inner(other = dOutput)
+        val dDenominator = denominator.pow(2) * numerator.inner(other = delta)
 
         // 分子側(dy/d[x - min(x)])
-        val dNumerator = denominator * dOutput
+        val dNumerator = denominator * delta
 
         val dMin = dDenominator.broadcastToD1(input.step)
         val dMax = dNumerator - dDenominator
@@ -69,25 +53,11 @@ class MinMaxNormD1 internal constructor(
             ),
         )
     }
-
-    override fun freeze(isFrozen: Boolean) {
-        optimizer.isFrozen = isFrozen
-    }
 }
 
-fun <T> NetworkBuilder.D1<T>.minMaxNorm(
-    optimizer: Optimizer = this.optimizer,
-    initializer: WeightInitializer = Fixed(1f),
-    id: String = Uuid.random().toString(),
-) = addProcess(
+fun <T> NetworkBuilder.D1<T>.minMaxNorm(id: String = Uuid.random().toString()) = addProcess(
     process = MinMaxNormD1(
         outputI = inputI,
-        optimizer = optimizer.d1(inputI),
-        weight = initializer.d1(
-            input = listOf(inputI),
-            output = listOf(inputI),
-            size = inputI,
-        ),
         id = id,
     ),
 )
