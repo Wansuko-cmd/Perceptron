71cff07a264d9c4b937a4052ce90c74652d977f4
Wansuko-cmd <63241922+Wansuko-cmd@users.noreply.github.com>
2026-07-13 01:55:01 +0900

    [add] tanhの定義を追加

--- io-type/src/commonMain/kotlin/com/wsr/knist/batch/elementwise/math/D1Ext.kt
+++ io-type/src/commonMain/kotlin/com/wsr/knist/batch/elementwise/math/D1Ext.kt
@@ -3,10 +3,14 @@
 import com.wsr.knist.Backend
 import com.wsr.knist.batch.Batch
 import com.wsr.knist.batch.d1
+import com.wsr.knist.batch.elementwise.operation.minus.minus
+import com.wsr.knist.batch.elementwise.operation.times.times
 import com.wsr.knist.batch.shape.toBatch
 import com.wsr.knist.batch.shape.toD2
 import com.wsr.knist.core.IOType
+import com.wsr.knist.core.elementwise.math.exp
 import com.wsr.knist.core.elementwise.math.softmax
+import com.wsr.knist.core.elementwise.operation.minus.minus
 import com.wsr.knist.scope.ScopeOp
 import com.wsr.knist.scope.ScopeOpDefault
 import kotlin.jvm.JvmName
@@ -46,3 +50,7 @@ fun Batch<IOType.D1>.sqrt(@ScopeOpDefault("1e-7f") e: Float = 1e-7f): Batch<IOTy
     val result = Backend.sqrt(x = value, e = e)
     return Batch.d1(size, shape, result)
 }
+
+@JvmName("batchD1sTanh")
+@ScopeOp
+fun Batch<IOType.D1>.tanh(): Batch<IOType.D1.Global> = 2f * (this * 2f).sigmoid() - 1f
