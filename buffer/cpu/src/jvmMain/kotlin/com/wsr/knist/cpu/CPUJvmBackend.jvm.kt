2a67370dfc1a5845eda5defe494e039a08576fb7
Wansuko-cmd <63241922+Wansuko-cmd@users.noreply.github.com>
2026-08-10 00:11:26 +0900

    [fix] Backendでの定義を追加

--- buffer/cpu/src/jvmMain/kotlin/com/wsr/knist/cpu/CPUJvmBackend.jvm.kt
+++ buffer/cpu/src/jvmMain/kotlin/com/wsr/knist/cpu/CPUJvmBackend.jvm.kt
@@ -6,8 +6,8 @@ import com.wsr.knist.base.loadNativeLibrary
 private const val LIB_PATH = "cpu"
 private const val LIB_NAME = "cpu"
 
-actual fun loadCPUBackend(fallback: IBackend, maxReservedBytes: Long): IBackend {
+actual fun loadCPUBackend(fallback: IBackend, maxReservedBytes: Long, maxPoolBytes: Long): IBackend {
     val isSuccess = loadNativeLibrary(path = LIB_PATH, name = LIB_NAME)
     if (isSuccess) cpuMaxReservedBytes = maxReservedBytes
-    return if (isSuccess) CPUJvmBackend(fallback) else fallback
+    return if (isSuccess) CPUJvmBackend(fallback, maxPoolBytes) else fallback
 }
